2d6d22176409463a8a3d18e71af00930f446252e
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-10-03 04:58:32 +0800

    Implement ByeCommand

---
+++ src/main/java/expiryeliminator/commands/ByeCommand.java
@@ -0,0 +1,26 @@
+package expiryeliminator.commands;
+
+//@@author bernardboey-reused
+// Reused from https://github.com/bernardboey/ip/blob/master/src/main/java/duke/commands/ByeCommand.java
+// with minor modifications
+
+/**
+ * Terminates the program.
+ */
+public class ByeCommand extends Command {
+    /** Unique word associated with the command. */
+    public static final String COMMAND_WORD = "bye";
+
+    private static final String MESSAGE_FAREWELL = "Bye. Hope to see you again soon!";
+
+    @Override
+    public String execute() {
+        return MESSAGE_FAREWELL;
+    }
+
+    @Override
+    public boolean isExit() {
+        return true;
+    }
+}
+//@@author
