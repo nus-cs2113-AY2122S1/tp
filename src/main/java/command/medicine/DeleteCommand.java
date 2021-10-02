7d780e9b563308217396002ad7a0200df291e1fd
RemusTeo <remustck@gmail.com>
2021-10-03 01:08:48 +0800

    Add delete feature
    Updated MedicineValidator.isValidStockId as well to handle more edge cases

---
+++ src/main/java/command/medicine/DeleteCommand.java
@@ -0,0 +1,45 @@
+package command.medicine;
+
+import command.Command;
+import command.CommandParameters;
+import command.CommandSyntax;
+import inventory.Medicine;
+import inventory.Stock;
+import parser.MedicineValidator;
+import ui.Ui;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Delete medication based on user input given stock id.
+ */
+
+public class DeleteCommand extends Command {
+
+    @Override
+    public void execute(Ui ui, HashMap<String, String> parameters, ArrayList<Stock> stocks) {
+        String [] requiredParameters = {CommandParameters.STOCK_ID};
+        if (!CommandSyntax.checkRequiredParameters(parameters, requiredParameters)) {
+            if (!parameters.isEmpty()) {
+                ui.printInvalidParameter(parameters.keySet().toArray()[0].toString(), CommandSyntax.DELETECOMMAND);
+            } else {
+                ui.printInvalidParameter("", CommandSyntax.DELETECOMMAND);
+            }
+            return;
+        };
+        String stockIdToDelete = parameters.get(CommandParameters.STOCK_ID);
+        if (!MedicineValidator.isValidStockId(ui, stockIdToDelete, stocks)) {
+            return;
+        }
+        int stockId = Integer.parseInt(stockIdToDelete);
+        for (Stock stock: stocks) {
+            Medicine medicine = (Medicine) stock;
+            if (medicine.getStockID() == stockId) {
+                stocks.remove(stock);
+                break;
+            }
+        }
+        ui.print("Medication deleted: Stock_Id=" + stockId);
+    }
+}
