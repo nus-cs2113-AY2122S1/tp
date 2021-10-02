2b49fda66396bdd2945113d5a3278c9e01b84558
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-10-03 04:58:05 +0800

    Implement Command class

---
+++ src/main/java/expiryeliminator/commands/Command.java
@@ -0,0 +1,27 @@
+package expiryeliminator.commands;
+
+//@@author bernardboey-reused
+// Reused from https://github.com/bernardboey/ip/blob/master/src/main/java/duke/commands/Command.java
+// with minor modifications
+
+/**
+ * Represents an executable command.
+ */
+public abstract class Command {
+    /**
+     * Executes the appropriate actions.
+     *
+     * @return Feedback about what was executed.
+     */
+    public abstract String execute();
+
+    /**
+     * Indicates whether the command is an exit command.
+     *
+     * @return {@code true} if and only if the command is an exit command; {@code false} otherwise.
+     */
+    public boolean isExit() {
+        return false;
+    }
+}
+//@@author
