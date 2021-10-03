85818975c3286c2014eec8bf1b10d56101625f5f
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-10-03 22:22:40 +0800

    Implement IngredientList class and DuplicateDataException

---
+++ src/main/java/expiryeliminator/data/IngredientList.java
@@ -0,0 +1,50 @@
+package expiryeliminator.data;
+
+import java.util.HashMap;
+
+import expiryeliminator.data.exception.DuplicateDataException;
+
+/**
+ * Represents the ingredient list and contains methods to add and remove ingredients.
+ */
+public class IngredientList {
+    private final HashMap<String, Ingredient> ingredients;
+
+    /**
+     * Constructs IngredientList object with no tasks.
+     */
+    public IngredientList() {
+        ingredients = new HashMap<>();
+    }
+
+    /**
+     * Adds ingredient to the ingredient list.
+     *
+     * @param ingredient Ingredient to be added;
+     */
+    public void add(Ingredient ingredient) throws DuplicateDataException {
+        if (contains(ingredient)) {
+            throw new DuplicateDataException();
+        }
+        ingredients.put(ingredient.getName(), ingredient);
+    }
+
+    /**
+     * Returns {@code true} if the ingredient list contains the specified ingredient.
+     *
+     * @param ingredient Ingredient whose presence in this list is to be tested.
+     * @return {@code true} if the ingredient list contains the specified ingredient.
+     */
+    public boolean contains(Ingredient ingredient) {
+        return ingredients.containsKey(ingredient.getName());
+    }
+
+    /**
+     * Returns the number of ingredients in the ingredient list.
+     *
+     * @return The number of ingredients in the ingredient list.
+     */
+    public int size() {
+        return ingredients.size();
+    }
+}
---
+++ src/main/java/expiryeliminator/data/exception/DuplicateDataException.java
@@ -0,0 +1,11 @@
+package expiryeliminator.data.exception;
+
+//@@author bernardboey-reused
+// Reused from
+// https://github.com/se-edu/addressbook-level2/blob/master/src/seedu/addressbook/data/exception/DuplicateDataException.java
+// with minor modifications
+
+/** Signals an error cause by duplicate data where there should be none. */
+public class DuplicateDataException extends Exception {
+}
+//@@author
