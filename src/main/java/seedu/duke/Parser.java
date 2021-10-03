a11f91ffc39523c8610a7dfca07264deea41003c
AnShengLee <anshengl@yahoo.com.sg>
2021-10-03 15:53:27 +0800

    Add skeleton code for Parser class

---
+++ src/main/java/seedu/duke/Parser.java
@@ -0,0 +1,141 @@
+package seedu.duke;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+
+    /**
+     * Used for initial separation of command word and args.
+     * This was adapted from addressbook-level2 source code here:
+     * https://github.com/se-edu/addressbook-level2/blob/master/src/seedu/addressbook/parser/Parser.java
+     */
+    private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
+
+    /**
+     * This was adapted from addressbook-level2 source code here:
+     * https://github.com/se-edu/addressbook-level2/blob/master/src/seedu/addressbook/parser/Parser.java
+     */
+    private static final Pattern ADD_EXPENSE_ARGUMENT_FORMAT =
+            Pattern.compile("d/(?<description>[^/]+)"
+                    + " a/(?<amount>[^/]+)");
+
+    /**
+     * This was adapted from addressbook-level2 source code here:
+     * https://github.com/se-edu/addressbook-level2/blob/master/src/seedu/addressbook/parser/Parser.java
+     */
+    private static final Pattern ADD_INCOME_ARGUMENT_FORMAT =
+            Pattern.compile("d/(?<description>[^/]+)"
+                    + " a/(?<amount>[^/]+)");
+
+    /**
+     * This was adapted from addressbook-level2 source code here:
+     * https://github.com/se-edu/addressbook-level2/blob/master/src/seedu/addressbook/parser/Parser.java
+     */
+    private static final Pattern DELETE_EXPENSE_ARGUMENT_FORMAT =
+            Pattern.compile("i/(?<index>[^/]+)");
+
+    /**
+     * This was adapted from addressbook-level2 source code here:
+     * https://github.com/se-edu/addressbook-level2/blob/master/src/seedu/addressbook/parser/Parser.java
+     */
+    private static final Pattern DELETE_INCOME_ARGUMENT_FORMAT =
+            Pattern.compile("i/(?<index>[^/]+)");
+
+    /**
+     * Parses user input into command for execution.
+     * This was adapted from addressbook-level2 source code here:
+     * https://github.com/se-edu/addressbook-level2/blob/master/src/seedu/addressbook/parser/Parser.java
+     *
+     * @param userInput full user input string
+     * @return the command based on the user input
+     */
+    public Command parseCommand(String userInput) {
+        final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
+        if (!matcher.matches()) {
+            return new InvalidCommand();
+        }
+
+        final String commandWord = matcher.group("commandWord");
+        final String arguments = matcher.group("arguments");
+
+        switch (commandWord) {
+        case "help":
+            return new HelpCommand();
+        case "add_ex":
+            return prepareAddExpense(arguments);
+        case "add_in":
+            return prepareAddIncome(arguments);
+        case "del_ex":
+            return prepareDeleteExpense(arguments);
+        case "del_in":
+            return prepareDeleteIncome(arguments);
+        case "list_ex":
+            return new ListExpenseCommand();
+        case "list_in":
+            return new ListIncomeCommand();
+        case "total_ex":
+            return new TotalExpenseCommand();
+        case "total_in":
+            return new TotalIncomeCommand();
+        default:
+            return new InvalidCommand();
+        }
+    }
+
+    /**
+     * This was adapted from addressbook-level2 source code here:
+     * https://github.com/se-edu/addressbook-level2/blob/master/src/seedu/addressbook/parser/Parser.java
+     */
+    private Command prepareAddExpense(String arguments) {
+        final Matcher matcher = ADD_EXPENSE_ARGUMENT_FORMAT.matcher(arguments.trim());
+
+        if (!matcher.matches()) {
+            return new InvalidCommand();
+        }
+
+        return new AddExpenseCommand();
+    }
+
+    /**
+     * This was adapted from addressbook-level2 source code here:
+     * https://github.com/se-edu/addressbook-level2/blob/master/src/seedu/addressbook/parser/Parser.java
+     */
+    private Command prepareAddIncome(String arguments) {
+        final Matcher matcher = ADD_INCOME_ARGUMENT_FORMAT.matcher(arguments.trim());
+
+        if (!matcher.matches()) {
+            return new InvalidCommand();
+        }
+
+        return new AddIncomeCommand();
+    }
+
+    /**
+     * This was adapted from addressbook-level2 source code here:
+     * https://github.com/se-edu/addressbook-level2/blob/master/src/seedu/addressbook/parser/Parser.java
+     */
+    private Command prepareDeleteExpense(String arguments) {
+        final Matcher matcher = DELETE_EXPENSE_ARGUMENT_FORMAT.matcher(arguments.trim());
+
+        if (!matcher.matches()) {
+            return new InvalidCommand();
+        }
+
+        return new DeleteExpenseCommand();
+    }
+
+    /**
+     * This was adapted from addressbook-level2 source code here:
+     * https://github.com/se-edu/addressbook-level2/blob/master/src/seedu/addressbook/parser/Parser.java
+     */
+    private Command prepareDeleteIncome(String arguments) {
+        final Matcher matcher = DELETE_INCOME_ARGUMENT_FORMAT.matcher(arguments.trim());
+
+        if (!matcher.matches()) {
+            return new InvalidCommand();
+        }
+
+        return new DeleteIncomeCommand();
+    }
+}
