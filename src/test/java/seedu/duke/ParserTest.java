08f1697c5c5903031b7560b188baa82372901ffc
AnShengLee <anshengl@yahoo.com.sg>
2021-10-03 15:53:54 +0800

    Add simple JUnit tests for Parser class

---
+++ src/test/java/seedu/duke/ParserTest.java
@@ -0,0 +1,35 @@
+package seedu.duke;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ParserTest {
+    @Test
+    public void testParseCommandForHelp() {
+        Parser testParser = new Parser();
+        Command underTest = testParser.parseCommand("help");
+        assertTrue(underTest.getClass() == HelpCommand.class);
+    }
+
+    @Test
+    public void testParseCommandForIncomeList() {
+        Parser testParser = new Parser();
+        Command underTest = testParser.parseCommand("list_in dshdtfshsdfh");
+        assertTrue(underTest.getClass() == ListIncomeCommand.class);
+    }
+
+    @Test
+    public void testParseCommandForInvalid() {
+        Parser testParser = new Parser();
+        Command underTest = testParser.parseCommand("tfshsdfh");
+        assertTrue(underTest.getClass() == InvalidCommand.class);
+    }
+
+    @Test
+    public void testParseCommandForInvalidAddExpense() {
+        Parser testParser = new Parser();
+        Command underTest = testParser.parseCommand("add_ex d/tfshsdfh");
+        assertTrue(underTest.getClass() == InvalidCommand.class);
+    }
+}
