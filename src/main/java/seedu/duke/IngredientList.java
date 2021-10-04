d1e09a7169139341f86c35f7292e63508605b66c
Renzo Canare <renzo_canare@hotmail.com>
2021-10-04 18:39:09 +0800

    Implement List Ingredients

---
+++ src/main/java/seedu/duke/IngredientList.java
@@ -0,0 +1,48 @@
+package seedu.duke;
+
+import java.util.ArrayList;
+
+public class IngredientList {
+    public static ArrayList<Ingredient> ingredientList = new ArrayList<>();
+
+    public static void add(String ingredientName) {
+        if (IngredientList.find(ingredientName) == -1) {
+            Ingredient ingredientToAdd = new Ingredient(ingredientName.toLowerCase());
+            ingredientList.add(ingredientToAdd);
+            System.out.println("Added:" + ingredientToAdd.getIngredientName());
+        } else {
+            System.out.println("Dish already exists");
+        }
+    }
+
+    //Returns -1 if not present, index if present
+    private static int find(String ingredientName) {
+        for (Ingredient ingredient: ingredientList) {
+            if (ingredient.getIngredientName().equals(ingredientName.toLowerCase())) {
+                return ingredientList.indexOf(ingredient);
+            }
+        }
+        return -1;
+    }
+
+    private static void list() {
+        System.out.println("Here are the ingredients you have: ");
+        for (int i=0; i<ingredientList.size(); i++) {
+            String currentIngredient = ingredientList.get(i).getIngredientName();
+            System.out.println((i+1) + ". " + currentIngredient);
+        }
+        System.out.println("You can use commands 'add' or 'find' to add new ingredients or find existing ones!");
+    }
+
+    private static void delete(String ingredientName) {
+        int ingredientIndex = IngredientList.find(ingredientName);
+        if (ingredientIndex == -1) {
+            System.out.println("Ingredient does not exist");
+        } else {
+            ingredientList.remove(ingredientIndex);
+            System.out.println("Ingredient, " + ingredientName + " has been removed!");
+        }
+    }
+}
+
+}
--- src/main/java/seedu/duke/command/Command.java
+++ src/main/java/seedu/duke/command/Command.java
@@ -4,4 +4,5 @@ import java.util.ArrayList;
 
 public abstract class Command {
     public abstract void execute(ArrayList<String> parameters);
+
 }
