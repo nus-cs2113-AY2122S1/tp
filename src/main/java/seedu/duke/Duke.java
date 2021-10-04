276675b374f20a3ed9a62f85acdda4a9dc03adc8
Sridharan Arvind Srinivasan <arv1920@gmail.com>
2021-10-04 15:38:14 +0800

    Modify io redirection to include new feature

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -15,6 +15,14 @@ public class Duke {
         InputParser parser = new InputParser();
         Scanner input = new Scanner(System.in);
 
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("How may I help you?");
+
         String userInput = input.nextLine();
         while (!userInput.equals("bye")) {
             try {
