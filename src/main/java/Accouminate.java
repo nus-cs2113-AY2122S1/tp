27f000de82b7d27c633c2ae40a83f381f8fa6d0c
Tng Jun Wei <tngjunwei@gmail.com>
2021-10-05 00:40:29 +0800

    Add Main app skeleton

---
+++ src/main/java/Accouminate.java
@@ -0,0 +1,34 @@
+import command.MainCommand;
+import picocli.CommandLine;
+import terminal.Ui;
+import java.util.concurrent.TimeUnit;
+
+public class Accouminate {
+
+    public static void main(String[] args) {
+        new Accouminate().run();
+    }
+
+    public void run() {
+        String[] splitUserInput;
+        String userInput;
+
+        Ui ui = new Ui();
+        ui.printGreeting();
+
+        CommandLine cmd = new CommandLine(new MainCommand());
+
+        while(true) {
+            userInput = ui.getUserInput();
+            splitUserInput = userInput.trim().split(" +"); //remove all excessive spaces in input
+            Integer returnCode = cmd.execute(splitUserInput);
+
+            //Allows error message to be printed out before the trying to get next user input
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
