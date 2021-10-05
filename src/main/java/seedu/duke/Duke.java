e5e038da8c220dd25276173ad9f64733330562cd
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2021-10-06 01:08:11 +0800

    Implement method calls to test Storage unit

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,20 +1,31 @@
 package seedu.duke;
 
+import seedu.contact.ContactList;
+import seedu.exception.FileErrorException;
+import seedu.storage.Storage;
 import seedu.ui.TextUi;
-
-import java.util.Scanner;
 
 public class Duke {
     private TextUi textUi;
+    private String contactFilePath;
+    private Storage storage;
+    private ContactList contactList;
 
-    public Duke() {
+    public Duke(String contactFilePath) {
         this.textUi = new TextUi();
+        this.contactFilePath = contactFilePath;
+        this.storage = new Storage(contactFilePath);
+        try {
+            this.contactList = storage.loadExistingContacts();
+        } catch (FileErrorException e) {
+            TextUi.fileErrorMessage(this.contactFilePath);
+        }
     }
 
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        new Duke();
+        new Duke("data/contacts.txt");
     }
 }
