e6c29b0da7320b6530e3413835d8db3a92666e53
Rebecca Chua <e0564171@u.nus.edu>
2021-10-05 21:35:20 +0800

    Add methods for Task and TaskList classes to manage tasks

--- src/main/java/seedu/duke/task/TaskList.java
+++ src/main/java/seedu/duke/task/TaskList.java
@@ -1,5 +1,8 @@
 package seedu.duke.task;
 
+import seedu.duke.exception.DukeException;
+import seedu.duke.ui.Message;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -32,7 +35,43 @@ public class TaskList {
      *
      * @param index the index of the task to be removed
      */
-    public void deleteTask(int index) {
-        taskList.remove(index);
+    public void deleteTask(int index) throws DukeException {
+        try {
+            taskList.remove(index);
+        } catch (IndexOutOfBoundsException exception) {
+            throw new DukeException(Message.ERROR_INDEX_OUT_OF_BOUNDS);
+        } catch (NumberFormatException exception) {
+            throw new DukeException(Message.ERROR_NUMBER_FORMAT);
+        }
+    }
+
+    public Task getTask(int index) throws DukeException {
+        try {
+            return taskList.get(index);
+        } catch (IndexOutOfBoundsException exception) {
+            throw new DukeException(Message.ERROR_INDEX_OUT_OF_BOUNDS);
+        } catch (NumberFormatException exception) {
+            throw new DukeException(Message.ERROR_NUMBER_FORMAT);
+        }
+    }
+
+    public void markTaskAsDone(int index) throws DukeException {
+        try {
+            taskList.get(index).setDone();
+        } catch (IndexOutOfBoundsException exception) {
+            throw new DukeException(Message.ERROR_INDEX_OUT_OF_BOUNDS);
+        } catch (NumberFormatException exception) {
+            throw new DukeException(Message.ERROR_NUMBER_FORMAT);
+        }
+    }
+
+    public TaskList findTasks(String keyword) {
+        TaskList matchingTaskList = new TaskList();
+        for (Task task: taskList) {
+            if (task.getInformation().contains(keyword)) {
+                matchingTaskList.addTask(task);
+            }
+        }
+        return matchingTaskList;
     }
 }
