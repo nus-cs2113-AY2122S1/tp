89e033848ee76c971b9c996343e399768d4d02ec
Jonathan Mui <jonathanmui4@gmail.com>
2021-10-05 17:48:53 +0800

    Update gitignore to ignore data file

--- src/main/java/seedu/duke/storage/Storage.java
+++ src/main/java/seedu/duke/storage/Storage.java
@@ -1,7 +1,24 @@
 package seedu.duke.storage;
 
+import seedu.duke.exception.GetJackDException;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * To deal with loading tasks from the json file and saving tasks in the json file.
  */
 public class Storage {
+    private final String STORAGE_PATH = "data/workouts.json";
+    private final File file = new File(STORAGE_PATH);
+    private final String FILE_PATH = file.getAbsolutePath();
+
+    public Storage() throws GetJackDException {
+        try {
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new GetJackDException("☹ OOPS!!! Data file can't be found.");
+        }
+    }
 }
