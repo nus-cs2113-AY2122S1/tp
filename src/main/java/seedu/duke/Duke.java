346fd2f4d55c37b1eb8ee283a0c1c13df542d792
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2021-10-05 22:58:20 +0800

    Replace Duke logo with ConTech logo

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -7,11 +7,14 @@ public class Duke {
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "   _____         _______        _     \n" +
+                "  / ____|       |__   __|      | |    \n" +
+                " | |     ___  _ __ | | ___  ___| |__  \n" +
+                " | |    / _ \\| '_ \\| |/ _ \\/ __| '_ \\ \n" +
+                " | |___| (_) | | | | |  __/ (__| | | |\n" +
+                "  \\_____\\___/|_| |_|_|\\___|\\___|_| |_|\n" +
+                "                                      \n" +
+                "                                      ";
         System.out.println("Hello from\n" + logo);
         System.out.println("What is your name?");
 
