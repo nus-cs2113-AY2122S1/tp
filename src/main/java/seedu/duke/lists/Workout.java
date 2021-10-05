35aeba9b337990c8a859d3e34e6b2e211728b8a6
DESKTOP-TO5H2A5\Ishaa <ishaan.vyas@hotmail.com>
2021-10-05 21:44:09 +0800

    Added decription for Workout class and a getter for it

--- src/main/java/seedu/duke/lists/Workout.java
+++ src/main/java/seedu/duke/lists/Workout.java
@@ -9,10 +9,17 @@ import java.util.ArrayList;
  * Adding, deleting and other operations related to the exercises will be executed from here.
  */
 public class Workout {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
     private ArrayList<Exercise> exercises;
 
-    public Workout() {
+    public Workout(String description) {
         exercises = new ArrayList<>();
+        this.description = description;
     }
 
     public void addExercise(Exercise exercise) {
