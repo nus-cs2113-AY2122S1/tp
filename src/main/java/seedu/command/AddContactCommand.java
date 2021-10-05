0cf1a91e030089536910ab4d72e68f560da937b6
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2021-10-06 01:33:40 +0800

    Implement Add command for adding contacts

---
+++ src/main/java/seedu/command/AddContactCommand.java
@@ -0,0 +1,20 @@
+package seedu.command;
+
+import seedu.contact.Contact;
+import seedu.ui.TextUi;
+
+public class AddContactCommand extends Command {
+    private final String name;
+    private final String github;
+
+    public AddContactCommand(String name, String github) {
+        this.name = name;
+        this.github = github;
+    }
+
+    public void execute() {
+        Contact addedContact = new Contact(name, github);
+        contactList.addContact(addedContact);
+        TextUi.addContactMessage(name, contactList.getListSize());
+    }
+}
