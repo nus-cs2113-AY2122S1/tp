b78a57c801bb25d9bc92f81594f0f5f094b797a5
Rebecca Chua <e0564171@u.nus.edu>
2021-10-05 21:37:50 +0800

    Add exit command class

---
+++ src/main/java/seedu/duke/command/ExitCommand.java
@@ -0,0 +1,15 @@
+package seedu.duke.command;
+
+import seedu.duke.lesson.LessonList;
+import seedu.duke.task.TaskList;
+import seedu.duke.ui.Ui;
+
+public class ExitCommand extends Command{
+    @Override
+    public boolean isExit() {
+        return true;
+    }
+
+    @Override
+    public void execute(Ui ui, TaskList taskList, LessonList lessonList) {}
+}
