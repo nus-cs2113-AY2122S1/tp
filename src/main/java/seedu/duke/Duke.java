c05585e02ae94561a4f7b0e2d961a5bb76204485
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2021-10-06 02:48:05 +0800

    Introduce parser into main to implement add contact

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,7 +1,10 @@
 package seedu.duke;
 
+import seedu.command.Command;
 import seedu.contact.ContactList;
 import seedu.exception.FileErrorException;
+import seedu.parser.Parser;
+import seedu.storage.ContactsEncoder;
 import seedu.storage.Storage;
 import seedu.ui.TextUi;
 
@@ -9,12 +12,14 @@ public class Duke {
     private TextUi textUi;
     private String contactFilePath;
     private Storage storage;
+    private Parser parser;
     private ContactList contactList;
 
     public Duke(String contactFilePath) {
         this.textUi = new TextUi();
         this.contactFilePath = contactFilePath;
         this.storage = new Storage(contactFilePath);
+        this.parser = new Parser();
         try {
             this.contactList = storage.loadExistingContacts();
         } catch (FileErrorException e) {
@@ -22,10 +27,29 @@ public class Duke {
         }
     }
 
+    private void runConTech() {
+        Command command;
+        do {
+            String userInput = textUi.getUserInput();
+            command = parser.parseCommand(userInput);
+            runCommandProcedure(command);
+        } while (!command.isExit());
+    }
+
+    private void runCommandProcedure(Command command) {
+        try {
+            command.setContactList(contactList);
+            command.execute();
+            ContactsEncoder.saveContacts(contactFilePath, contactList);
+        } catch (FileErrorException e) {
+            TextUi.fileErrorMessage(contactFilePath);
+        }
+    }
+
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        new Duke("data/contacts.txt");
+        new Duke("data/contacts.txt").runConTech();
     }
 }
