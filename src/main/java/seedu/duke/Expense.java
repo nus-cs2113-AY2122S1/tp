8767a0f16f301a750491095135d18a1a70244c48
leeyikai <e0544550@u.nus.edu>
2021-10-06 17:18:53 +0800

    Added attributes and method to edit location of expense (#20)

--- src/main/java/seedu/duke/Expense.java
+++ src/main/java/seedu/duke/Expense.java
@@ -1,4 +1,14 @@
 package seedu.duke;
 
+import java.util.ArrayList;
+
 public class Expense {
+    private String location;
+    
+    public String getLocation() { return this.location; }
+
+    public void setLocation(String location) { this.location = location; }
+
+
+
 }
