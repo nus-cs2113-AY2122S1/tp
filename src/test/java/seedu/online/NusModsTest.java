52a6d8ad10564a8b5ce44797e0ee92e5471b2b62
ChewyChair <69298017+ChewyChair@users.noreply.github.com>
2021-10-06 18:52:26 +0800

    Minor change to JUnit tests for NusMods and ModStorage.

--- src/test/java/seedu/online/NusModsTest.java
+++ src/test/java/seedu/online/NusModsTest.java
@@ -7,19 +7,18 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static seedu.online.NusMods.fetchModOnline;
 
 public class NusModsTest {
 
     @Test
     public void load_Invalid_Mod_Exception_Thrown() throws Exception {
         assertThrows(IOException.class, () ->
-                fetchModOnline("This mod does not exist"));
+                NusMods.fetchModOnline("This mod does not exist"));
     }
 
     @Test
     public void load_Valid_Mod() throws Exception {
-        Module modCS2113T = fetchModOnline("CS2113T");
+        Module modCS2113T = NusMods.fetchModOnline("CS2113T");
         Module mod = new Module("Dummy mod");
         assertEquals(modCS2113T.getClass(), mod.getClass());
     }
--- src/test/java/seedu/storage/ModStorageTest.java
+++ src/test/java/seedu/storage/ModStorageTest.java
@@ -2,7 +2,6 @@ package seedu.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static seedu.storage.ModStorage.loadModInfo;
 
 import org.junit.jupiter.api.Test;
 import seedu.module.Module;
@@ -15,12 +14,12 @@ public class ModStorageTest {
     @Test
     public void load_Invalid_File_Exception_Thrown() throws Exception {
         assertThrows(IOException.class, () ->
-                loadModInfo("CS2113S"));
+                ModStorage.loadModInfo("CS2113S"));
     }
 
     @Test
     public void load_Valid_File() throws Exception {
-        Module modCS2113T = loadModInfo("CS2113T");
+        Module modCS2113T = ModStorage.loadModInfo("CS2113T");
         Module mod = new Module("Dummy mod");
         assertEquals(modCS2113T.getClass(), mod.getClass());
     }
