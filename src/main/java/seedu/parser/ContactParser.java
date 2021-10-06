f4a91feeb49002b93ee7b098d3d3fcca1e7ec627
Ng Andre <ng.andr3@gmail.com>
2021-10-06 20:34:35 +0800

    Update constants in ContactParser

--- src/main/java/seedu/parser/ContactParser.java
+++ src/main/java/seedu/parser/ContactParser.java
@@ -5,6 +5,7 @@ import seedu.exception.InvalidFlagException;
 public abstract class ContactParser {
     public static final int CONTACT_PARAMS_START_INDEX = 1;
     public static final int NUMBER_OF_DETAILS = 2;
+    public static final int NUMBER_OF_EDIT_DETAILS = 3;
 
     public static final String DETAIL_SEPARATOR = " -";
     public static final int DETAILS_TO_SPLIT = 2;
