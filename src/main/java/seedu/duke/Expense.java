52db7972946d056fe47c4e2fa94aa63a406ab83f
leeyikai <e0544550@u.nus.edu>
2021-10-06 17:19:39 +0800

    Added function to enter names of other people (#11)

--- src/main/java/seedu/duke/Expense.java
+++ src/main/java/seedu/duke/Expense.java
@@ -4,11 +4,23 @@ import java.util.ArrayList;
 
 public class Expense {
     private String location;
-    
+    private ArrayList<Person> listOfPersons;
+
     public String getLocation() { return this.location; }
 
     public void setLocation(String location) { this.location = location; }
 
-
+    public void addPersons(ArrayList<Person> newListOfPersons) {
+        listOfPersons.clear();
+        Person personToAdd;
+        Person personToChange;
+        String newName;
+        for(int i = 0; i < newListOfPersons.size(); i++) {
+            personToAdd = newListOfPersons.get(i);
+            personToChange = listOfPersons.get(i);
+            newName = personToAdd.getName();
+            personToChange.setName(newName);
+        }
+    }
 
 }
