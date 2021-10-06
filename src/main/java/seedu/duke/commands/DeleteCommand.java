832c5584d6b3b1a2d9b01c06fb7ff9cfb8dd13de
alwinangys <alwinang99@gmail.com>
2021-10-06 11:49:52 +0800

    Convert command array to string

--- src/main/java/seedu/duke/commands/DeleteCommand.java
+++ src/main/java/seedu/duke/commands/DeleteCommand.java
@@ -1,16 +1,19 @@
 package seedu.duke.commands;
 
+import java.util.Arrays;
+
 import static seedu.duke.Duke.itemList;
 
 public class DeleteCommand extends Command {
 
     // input from user
-    public String[] taskToDelete;
+    public String taskToDelete;
 
     // v1.0: deleteCommand deletes purely based on index, i.e. delete [TASK_INDEX]
-
+    // converted command array to string for future uses, where input may have extra spaces
+    // such as 'delete foo bar'
     public DeleteCommand(String[] command) {
-        taskToDelete = command;
+        taskToDelete = Arrays.toString(command);
     }
 
     public CommandResult execute() {
@@ -23,7 +26,7 @@ public class DeleteCommand extends Command {
     }
 
     // did not throw a custom exception if taskIndex > itemList.size(), already caught by ArrayIndexOutOfBoundsException
-    public static String deleteTask(String[] input) {
+    public static String deleteTask(String input) {
         try {
             int taskIndex = getTaskIndex(input);
             String taskTitle = itemList.get(taskIndex).getTitle();
@@ -36,7 +39,8 @@ public class DeleteCommand extends Command {
         }
     }
 
-    public static int getTaskIndex(String[] command) {
-        return Integer.parseInt(command[1]) - 1;
+    public static int getTaskIndex(String command) {
+        int taskIndexPosition = command.trim().indexOf(" ") + 1;
+        return Integer.parseInt(command.trim().substring(taskIndexPosition)) - 1;
     }
 }
