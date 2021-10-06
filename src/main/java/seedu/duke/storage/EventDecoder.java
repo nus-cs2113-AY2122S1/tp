ad115b2ed53699a35826be3b502c41ca4d5d63a1
Alvin.Avocados <alvinpang321@gmail.com>
2021-10-06 22:53:58 +0800

    Add methods for decoding of event data

--- src/main/java/seedu/duke/storage/EventDecoder.java
+++ src/main/java/seedu/duke/storage/EventDecoder.java
@@ -6,24 +6,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EventDecoder {
-    public static final int INDEX_OF_TITLE = 0;
-    public static final int INDEX_OF_DATETIME = 1;
-    public static final int INDEX_OF_VENUE = 2;
-    public static final int INDEX_OF_DESCRIPTION = 3;
-    public static final int INDEX_OF_BUDGET = 4;
+
+    private static final String EVENT_DATA_ARGS_DELIMITER = "\\s*\\|\\s*";
+
+    private static final int INDEX_OF_TITLE = 0;
+    private static final int INDEX_OF_DATETIME = 1;
+    private static final int INDEX_OF_VENUE = 2;
+    private static final int INDEX_OF_DESCRIPTION = 3;
+    private static final int INDEX_OF_BUDGET = 4;
 
     public static ArrayList<Event> decodeEventList(List<String> encodedEventList) {
         ArrayList<Event> decodedEvents = new ArrayList<>();
-
         for (String encodedEvent : encodedEventList) {
-
+            decodedEvents.add(decodeEventFromString(encodedEvent));
         }
-
         return decodedEvents;
     }
 
     public static Event decodeEventFromString(String encodedEvent) {
-        String[] eventDetails = encodedEvent.trim().split("\\s*\\|\\s*");
+        String[] eventDetails = encodedEvent.trim().split(EVENT_DATA_ARGS_DELIMITER);
         String eventTitle = eventDetails[INDEX_OF_TITLE];
         String eventDateTime = eventDetails[INDEX_OF_DATETIME];
         String eventVenue = eventDetails[INDEX_OF_VENUE];
