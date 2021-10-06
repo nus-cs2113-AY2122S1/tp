419d894533f384eae4994c227bc1e5efaac2da38
Alvin.Avocados <alvinpang321@gmail.com>
2021-10-06 22:54:46 +0800

    Event.java: Add placeholder constructor for even EventDecoder

--- src/main/java/seedu/duke/items/Event.java
+++ src/main/java/seedu/duke/items/Event.java
@@ -3,6 +3,11 @@ package seedu.duke.items;
 import java.time.LocalDateTime;
 
 public class Event extends Item {
+
+    public Event(String eventTitle, String eventDateTime, String eventVenue, String eventDescription, String eventBudget) {
+        super();
+    }
+
     public static String getItemType() {
         return "event";
     }
