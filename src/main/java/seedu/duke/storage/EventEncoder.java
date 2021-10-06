fea1082931d148e34d1670fbc311f81d49439998
Alvin.Avocados <alvinpang321@gmail.com>
2021-10-06 23:39:31 +0800

    EventEncoder.java: Add skeleton code for methods

--- src/main/java/seedu/duke/storage/EventEncoder.java
+++ src/main/java/seedu/duke/storage/EventEncoder.java
@@ -1,4 +1,21 @@
 package seedu.duke.storage;
 
+import seedu.duke.items.Event;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EventEncoder {
+
+    public static List<String> encodeEventsList(ArrayList<Event> eventsToSave) {
+        List<String> encodedEvents = new ArrayList<>();
+
+        return encodedEvents;
+    }
+
+    public static String encodeEventToString(Event event) {
+        StringBuilder encodedEventBuilder = new StringBuilder();
+
+        return encodedEventBuilder.toString();
+    }
 }
