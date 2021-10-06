402d5cd88517a80aae8018c36c3e3a0cf2a6c9ff
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2021-10-06 22:44:23 +0800

    Implement function to list all contacts with index

--- src/main/java/seedu/command/ListContactsCommand.java
+++ src/main/java/seedu/command/ListContactsCommand.java
@@ -1,4 +1,16 @@
 package seedu.command;
 
-public class ListContactsCommand {
+import seedu.ui.TextUi;
+
+public class ListContactsCommand extends Command {
+    @Override
+    public void execute() {
+        int listSize = contactList.getListSize();
+
+        TextUi.viewContactsListMessage(listSize);
+        for (int i = 0; i < listSize; i++) {
+            System.out.println(i + ". " + contactList.getContactAtIndex(i).getName());
+        }
+        TextUi.printBottomLine();
+    }
 }
