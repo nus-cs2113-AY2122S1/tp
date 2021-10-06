d7985e836fa696623672810e662573e7d1ef8dc7
APZH <amospzh@gmail.com>
2021-10-07 01:34:25 +0800

    update Ui class with new logo and methods to print logo, cursor, and message

--- src/main/java/seedu/duke/ui/Ui.java
+++ src/main/java/seedu/duke/ui/Ui.java
@@ -4,4 +4,37 @@ package seedu.duke.ui;
  * Text UI of the CLImods application.
  */
 public class Ui {
+
+    private final String LOGO = "\n"
+            + "  /$$$$$$  /$$       /$$$$$$                               /$$          \n"
+            + " /$$__  $$| $$      |_  $$_/                              | $$          \n"
+            + "| $$  \\__/| $$        | $$   /$$$$$$/$$$$   /$$$$$$   /$$$$$$$  /$$$$$$$\n"
+            + "| $$      | $$        | $$  | $$_  $$_  $$ /$$__  $$ /$$__  $$ /$$_____/\n"
+            + "| $$      | $$        | $$  | $$ \\ $$ \\ $$| $$  \\ $$| $$  | $$|  $$$$$$\n"
+            + "| $$    $$| $$        | $$  | $$ | $$ | $$| $$  | $$| $$  | $$ \\____  $$\n"
+            + "|  $$$$$$/| $$$$$$$$ /$$$$$$| $$ | $$ | $$|  $$$$$$/|  $$$$$$$ /$$$$$$$/\n"
+            + " \\______/ |________/|______/|__/ |__/ |__/ \\______/  \\_______/|_______/\n"
+            + "------------------------------------------------------------------------\n"
+            + "Command-Line Interface for NUSMODS                              (v1.0.0)\n"
+            + "------------------------------------------------------------------------";
+
+    private final String LINE_SEPARATOR = "------------------------------------------------------------------------";
+    private final String MESSAGE_BORDER = "|| ";
+
+    public void printLogo() {
+        System.out.println(LOGO);
+    }
+
+    public void printCursor() {
+        System.out.print("[user]: ");
+    }
+
+    public void printMessage(String message) {
+        String[] messageLines = message.split("\\r?\\n");
+        for(int i = 0; i < messageLines.length; i++) {
+            System.out.print(MESSAGE_BORDER + messageLines[i] + "\n");
+        }
+        System.out.println(LINE_SEPARATOR);
+    }
+
 }
