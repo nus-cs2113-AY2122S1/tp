17eab006b1b9ae0a4e87411d3f598039cdf53957
Alvin.Avocados <alvinpang321@gmail.com>
2021-10-06 23:07:10 +0800

    Task.java: Add placeholder constructor for TaskDecoder

--- src/main/java/seedu/duke/items/Task.java
+++ src/main/java/seedu/duke/items/Task.java
@@ -3,6 +3,11 @@ package seedu.duke.items;
 import java.time.LocalDateTime;
 
 public class Task extends Item {
+
+    public Task(String taskTitle, String taskDeadline, String taskDescription) {
+        super();
+    }
+
     public static String getItemType() {
         return "task";
     }
