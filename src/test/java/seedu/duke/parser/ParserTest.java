0d49600e62ed7acc42f84036da36363f9b365b9a
APZH <amospzh@gmail.com>
2021-10-07 02:11:55 +0800

    fix checkstyle error in ParserTest

--- src/test/java/seedu/duke/parser/ParserTest.java
+++ src/test/java/seedu/duke/parser/ParserTest.java
@@ -3,11 +3,10 @@ package seedu.duke.parser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParserTest {
 
@@ -16,12 +15,12 @@ class ParserTest {
     private final String commandArguments = "cs2113 tp project --type deadline --start 22-09-2021"
             + " --end 29-09-2021 --recur weekly --priority 2";
 
-    private final String expectedSplit = "--type = deadline\n" +
-            "--recur = weekly\n" +
-            "--end = 29-09-2021\n" +
-            "--priority = 2\n" +
-            "mainArgument = cs2113 tp project\n" +
-            "--start = 22-09-2021\n";
+    private final String expectedSplit = "--type = deadline\n"
+            + "--recur = weekly\n"
+            + "--end = 29-09-2021\n"
+            + "--priority = 2\n"
+            + "mainArgument = cs2113 tp project\n"
+            + "--start = 22-09-2021\n";
 
     @Test
     @DisplayName("When we check the command is able to split all fields properly")
