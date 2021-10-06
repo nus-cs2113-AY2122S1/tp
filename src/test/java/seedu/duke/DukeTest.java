0edd1adaf6c057e7e48d3952c5b6431f841d79b5
leeyikai <e0544550@u.nus.edu>
2021-10-06 20:00:17 +0800

    Added unit test for setting location in trip

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -1,12 +1,21 @@
 package seedu.duke;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 class DukeTest {
+
+    @Test
+    public void testSetNameOfPerson () {
+        Trip trip = new Trip();
+        trip.setLocation("America");
+        assertEquals("America", trip.getLocation());
+    }
+    
     @Test
     public void sampleTest() {
-        assertTrue(true);
+       assertTrue(true);
     }
 }
