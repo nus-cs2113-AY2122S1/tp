2dd8f33447243c467947d0317eb7e2f2627b7040
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2021-10-06 22:59:03 +0800

    Refactor code

--- src/main/java/seedu/command/ListContactsCommand.java
+++ src/main/java/seedu/command/ListContactsCommand.java
@@ -6,11 +6,22 @@ public class ListContactsCommand extends Command {
     @Override
     public void execute() {
         int listSize = contactList.getListSize();
+        if (listSize == 0) {
+            TextUi.viewContactsEmptyListMessage();
+        } else {
+            listAllContacts(listSize);
+        }
+    }
 
+    private void listAllContacts(int listSize) {
         TextUi.viewContactsListMessage(listSize);
-        for (int i = 0; i < listSize; i++) {
-            System.out.println(i + ". " + contactList.getContactAtIndex(i).getName());
+        for (int index = 0; index < listSize; index++) {
+            TextUi.printContactWithIndex(index, getContactName(index));
         }
         TextUi.printBottomLine();
+    }
+
+    private String getContactName(int index) {
+        return contactList.getContactAtIndex(index).getName();
     }
 }
