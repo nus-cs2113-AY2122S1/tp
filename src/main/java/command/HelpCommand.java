44550768bc9227aa2326def289e00ebea588d754
alvintan01 <35805635+alvintan01@users.noreply.github.com>
2021-10-07 23:42:43 +0800

    Reorder help command

--- src/main/java/command/HelpCommand.java
+++ src/main/java/command/HelpCommand.java
@@ -16,12 +16,12 @@ public class HelpCommand extends Command {
     public void execute(Ui ui, HashMap<String, String> parameters, ArrayList<Medicine> medicines) {
         ArrayList<CommandSyntax> commandSyntaxes = new ArrayList<>();
         commandSyntaxes.add(new CommandSyntax(CommandList.ADD, CommandSyntax.ADD_COMMAND));
+        commandSyntaxes.add(new CommandSyntax(CommandList.LIST, CommandSyntax.LIST_COMMAND));
+        commandSyntaxes.add(new CommandSyntax(CommandList.UPDATE, CommandSyntax.UPDATE_COMMAND));
         commandSyntaxes.add(new CommandSyntax(CommandList.DELETE, CommandSyntax.DELETE_COMMAND));
         commandSyntaxes.add(new CommandSyntax(CommandList.EXIT, CommandSyntax.EXIT_COMMAND));
         commandSyntaxes.add(new CommandSyntax(CommandList.HELP, CommandSyntax.HELP_COMMAND));
-        commandSyntaxes.add(new CommandSyntax(CommandList.LIST, CommandSyntax.LIST_COMMAND));
         commandSyntaxes.add(new CommandSyntax(CommandList.PURGE, CommandSyntax.PURGE_COMMAND));
-        commandSyntaxes.add(new CommandSyntax(CommandList.UPDATE, CommandSyntax.UPDATE_COMMAND));
 
         ui.printHelpMessage(commandSyntaxes);
     }
