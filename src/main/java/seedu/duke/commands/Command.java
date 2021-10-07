3605bb69ed5d77964a592b3f1ccc5c7ad4b92c9c
ckteo <exetrtck@gmail.com>
2021-10-08 05:16:48 +0800

    Add base command class

---
+++ src/main/java/seedu/duke/commands/Command.java
@@ -0,0 +1,18 @@
+package seedu.duke.commands;
+
+import seedu.duke.ui.TextUI;
+
+/**
+ * The command class is an abstract class representing the underlying command object
+ * It is inherited by all other command objects
+ */
+public abstract class Command {
+    /**
+     * This method executes the command, performing the required operations
+     * It is only meant to be called by child classes
+     * @param ui Object that handles user IO
+     */
+    public void execute(TextUI ui) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/main/java/seedu/duke/commands/Parser.java
@@ -0,0 +1,12 @@
+package seedu.duke.commands;
+
+/**
+ * The parser class contains methods to process the string input by the user
+ */
+public class Parser {
+    private static final String COMMAND_EXIT = "exit";
+
+    public static Command parse(String input){
+
+    }
+}
