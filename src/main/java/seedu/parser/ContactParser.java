15b82f8027796459d4b1b98488304fab2eba392e
Ng Andre <ng.andr3@gmail.com>
2021-10-07 19:59:55 +0800

    Update constants

--- src/main/java/seedu/parser/ContactParser.java
+++ src/main/java/seedu/parser/ContactParser.java
@@ -11,9 +11,10 @@ public abstract class ContactParser {
     public static final int NUMBER_OF_EDIT_DETAILS = 3;
 
     public static final String DETAIL_SEPARATOR = " -";
-    public static final int DETAILS_TO_SPLIT = 2;
     public static final int FLAG_INDEX_IN_DETAILS = 0;
     public static final int DETAIL_INDEX_IN_DETAILS = 1;
+    public static final int EDIT_USER_INDEX = 1;
+    public static final int USER_INFO_INDEX = 2;
 
     public static final String NAME_FLAG = "n";
     public static final String GITHUB_FLAG = "g";
