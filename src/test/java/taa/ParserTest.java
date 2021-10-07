ee4f4de052f1d72364db872ea21c82ebc2624368
Leyond <leyondlee@users.noreply.github.com>
2021-10-07 19:55:55 +0800

    Add test for Parser.getArgumentsFromString

---
+++ src/test/java/taa/ParserTest.java
@@ -0,0 +1,29 @@
+package taa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+
+    @Test
+    void getArgumentsFromString_noKeys_expectEmpty() {
+        String inputString = "c/CS2113T n/Software Engineering and Object-oriented Programming";
+        String[] keys = {};
+        assertTrue(Parser.getArgumentsFromString(inputString, keys).isEmpty());
+    }
+
+    @Test
+    void getArgumentsFromString_oneKey_expectOneItem() {
+        String inputString = "c/CS2113T n/Software Engineering and Object-oriented Programming";
+        String[] keys = {"n"};
+        assertEquals(Parser.getArgumentsFromString(inputString, keys).size(), 1);
+    }
+
+    @Test
+    void getArgumentsFromString_twoKeys_expectTwoItems() {
+        String inputString = "c/CS2113T n/Software Engineering and Object-oriented Programming";
+        String[] keys = {"c","n"};
+        assertEquals(Parser.getArgumentsFromString(inputString, keys).size(), 2);
+    }
+}
