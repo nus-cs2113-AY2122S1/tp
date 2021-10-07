0b27e1aba4d9833ffbd06f7609c12e4eaece84c1
datn02 <ducanh02.work@gmail.com>
2021-10-07 23:03:11 +0800

    Fix coding style

--- src/test/java/seedu/duke/ParserTest.java
+++ src/test/java/seedu/duke/ParserTest.java
@@ -3,7 +3,8 @@ package seedu.duke;
 import org.junit.jupiter.api.Test;
 import seedu.duke.exceptions.DukeException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class ParserTest {
     @Test
--- src/test/java/seedu/duke/ingredients/IngredientsListTest.java
+++ src/test/java/seedu/duke/ingredients/IngredientsListTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import seedu.duke.exceptions.DukeException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class IngredientsListTest {
     @Test
