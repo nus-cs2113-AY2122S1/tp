32aaa7aa9bbfe434b71776d8cbf51ef9d7fc6d38
LAPTOP-H8FATR55\kairo <kairoskoh.1212@gmail.com>
2021-10-07 22:42:57 +0800

    Add basic feature for EmployeeParser.java

--- src/main/java/seedu/duke/employee/EmployeeParser.java
+++ src/main/java/seedu/duke/employee/EmployeeParser.java
@@ -1,4 +1,24 @@
 package seedu.duke.employee;
 
 public class EmployeeParser {
+
+    public void addEmployee(EmployeeList masterList, Employee employee) {
+        masterList.employeeList.add(employee);
+        masterList.totalEmployee += 1;
+    }
+
+    public void deleteEmployee(EmployeeList masterList, int employeeIndex) {
+        masterList.employeeList.remove(employeeIndex);
+        masterList.totalEmployee -= 1;
+    }
+
+    public void listEmployee(EmployeeList masterList) {
+        System.out.println("--------------------");
+        System.out.println("Here are the employees in your list:");
+        for(int i = 0; i < masterList.totalEmployee; i += 1){
+            System.out.println(i + masterList.employeeList.get(i).getName() );
+        }
+        System.out.println("--------------------");
+    }
+
 }
