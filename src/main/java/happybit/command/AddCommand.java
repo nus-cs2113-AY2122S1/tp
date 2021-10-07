9a651f8d71af8ecf63053d16abdf7567f011bbf7
flerovious <85042815+flerovious@users.noreply.github.com>
2021-10-07 21:37:30 +0800

    Add AddCommand

---
+++ src/main/java/happybit/command/AddCommand.java
@@ -0,0 +1,38 @@
+package happybit.command;
+
+import java.util.ArrayList;
+
+public class AddCommand {
+    // temporary attribute
+    private final ArrayList<String> habits = new ArrayList<>();
+
+    /**
+     * Adds habit to habit list
+     *
+     * @param habit Habit to add to habit list
+     */
+    public void addHabit(String habit) {
+        habits.add(habit);
+    }
+
+    /**
+     * Adds a habit
+     */
+    public void runCommand(String habit) {
+        addHabit(habit);
+    }
+
+    /**
+     * @return Size of habits list
+     */
+    public int getHabitListSize() {
+        return habits.size();
+    }
+
+    /**
+     * @return List of all current habits
+     */
+    public ArrayList<String> getHabits() {
+        return habits;
+    }
+}
---
+++ src/test/java/happybit/command/AddCommandTest.java
@@ -0,0 +1,34 @@
+package happybit.command;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AddCommandTest {
+
+    @Test
+    void testAddHabit() {
+        AddCommand addCommand = new AddCommand();
+        String H0 = "h0";
+        String H1 = "h1";
+        String H2 = "h2";
+
+        assertEquals(0, addCommand.getHabitListSize());
+
+        addCommand.addHabit(H0);
+        assertEquals(1, addCommand.getHabitListSize());
+        assertArrayEquals(new String[] {H0}, addCommand.getHabits().toArray());
+
+        addCommand.addHabit(H1);
+        assertEquals(2, addCommand.getHabitListSize());
+        assertArrayEquals(new String[] {H0, H1}, addCommand.getHabits().toArray());
+
+        addCommand.addHabit(H2);
+        assertEquals(3, addCommand.getHabitListSize());
+        assertArrayEquals(new String[] {H0, H1, H2}, addCommand.getHabits().toArray());
+    }
+}
