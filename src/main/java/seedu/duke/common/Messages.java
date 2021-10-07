a6cf0a5d2f2daae9853f61324de72a28573ec930
ckteo <exetrtck@gmail.com>
2021-10-08 04:48:37 +0800

    Resolve coding standard violations

--- src/main/java/seedu/duke/common/Messages.java
+++ src/main/java/seedu/duke/common/Messages.java
@@ -1,19 +1,18 @@
 package seedu.duke.common;
 
 public class Messages {
-    public static final String LOGO =
-            "        _.-\"\\\n" +
-            "    _.-\"     \\\n" +
-            " ,-\"          \\\n" +
-            "( \\            \\\n" +
-            " \\ \\            \\\n" +
-            "  \\ \\            \\\n" +
-            "   \\ \\         _.-;\n" +
-            "    \\ \\    _.-\"   :\n" +
-            "     \\ \\,-\"    _.-\"\n" +
-            "      \\(   _.-\"  libmgr\n" +
-            "       `--\"" +
-            "";
+    public static final String LOGO = "        _.-\"\\\n"
+            + "    _.-\"     \\\n"
+            + " ,-\"          \\\n"
+            + "( \\            \\\n"
+            + " \\ \\            \\\n"
+            + "  \\ \\            \\\n"
+            + "   \\ \\         _.-;\n"
+            + "    \\ \\    _.-\"   :\n"
+            + "     \\ \\,-\"    _.-\"\n"
+            + "      \\(   _.-\"  libmgr\n"
+            + "       `--\""
+            + "";
     public static final String WELCOME_MESSAGE = "Welcome to libmgr";
     public static final String GOODBYE_MESSAGE = "See you soon!";
 }
