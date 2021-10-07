b1eaea703cc9aaea6e41151976b20c8f993c0dfa
Leyond <leyondlee@users.noreply.github.com>
2021-10-07 20:00:59 +0800

    Remove wildcard import

--- src/test/java/taa/ParserTest.java
+++ src/test/java/taa/ParserTest.java
@@ -2,7 +2,8 @@ package taa;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParserTest {
 
