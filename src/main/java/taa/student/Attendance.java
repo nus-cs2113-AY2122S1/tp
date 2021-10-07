7d301b975418a3f403c62295a3e02d1e47fa8626
Kong Dak Nam <kongdaknam@gmail.com>
2021-10-07 23:47:04 +0800

    fix format issues

--- src/main/java/taa/student/Attendance.java
+++ src/main/java/taa/student/Attendance.java
@@ -4,17 +4,17 @@ public class Attendance {
     private int lessonNum;
     private boolean hadAttend;
 
-    public Attendance(int lessonNum){
+    public Attendance(int lessonNum) {
         this.lessonNum = lessonNum;
         this.hadAttend = false;
     }
 
     public int markAttendance() {
-        return (hadAttend? 1 : 0);
+        return (hadAttend ? 1 : 0);
     }
 
     @Override
-    public String toString(){
-        return lessonNum  + " - " + markAttendance();
+    public String toString() {
+        return lessonNum + " - " + markAttendance();
     }
 }
