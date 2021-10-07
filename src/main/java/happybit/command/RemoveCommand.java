fca65eed5f717d8fe2f3f972f7b22cd0b563ff17
kahhe <69589263+kahhe@users.noreply.github.com>
2021-10-07 17:09:25 +0800

    Fix Indentation

--- src/main/java/happybit/command/RemoveCommand.java
+++ src/main/java/happybit/command/RemoveCommand.java
@@ -4,29 +4,25 @@ import java.util.ArrayList;
 
 public class RemoveCommand {
 
-     //Temporary methods
-     protected ArrayList<String> goals = new ArrayList<>();
+     //Temporary method
+    protected ArrayList<String> goals = new ArrayList<>();
 
-     public void removeGoal(String goal) {
+    public void removeGoal(String goal) {
         goals.remove(goal);
-     }
+    }
 
-     //Semi-permanent methods
+    //Semi-permanent methods
 
-     //protected String goalName;
-     //public DeleteCommand(int goalName) {
-     //    this.goalName= goalName;
-     //}
-
-
-     /**
+    //protected String goalName;
+    //public DeleteCommand(int goalName) {
+    //    this.goalName= goalName;
+    //}
+    /**
      * Deletes a goal of a habit.
      *
      * @param goal Name of habit to be deleted.
      */
-    //@Override
     public void runCommand(String goal) {
         removeGoal(goal);
     }
-
 }
