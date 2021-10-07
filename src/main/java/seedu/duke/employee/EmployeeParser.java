a31cdb13b198519d1f745675ded6f2fd878afa1c
LAPTOP-H8FATR55\kairo <kairoskoh.1212@gmail.com>
2021-10-07 22:54:43 +0800

    Add EmployeeParserTest.java

--- src/main/java/seedu/duke/employee/EmployeeParser.java
+++ src/main/java/seedu/duke/employee/EmployeeParser.java
@@ -8,11 +8,17 @@ public class EmployeeParser {
     }
 
     public void deleteEmployee(EmployeeList masterList, int employeeIndex) {
+        if (masterList.totalEmployee < 1){
+            return;
+        }
         masterList.employeeList.remove(employeeIndex);
         masterList.totalEmployee -= 1;
     }
 
     public void listEmployee(EmployeeList masterList) {
+        if (masterList.totalEmployee < 1){
+            return;
+        }
         System.out.println("--------------------");
         System.out.println("Here are the employees in your list:");
         for(int i = 0; i < masterList.totalEmployee; i += 1){
