f1967a1d5477679e63574a801a0149fa3ea37bfa
STAung07 <swann1999@gmail.com>
2021-10-07 17:24:26 +0800

    Fixed Checkstyle Bugs

--- src/test/java/happybit/parser/InputParserTest.java
+++ src/test/java/happybit/parser/InputParserTest.java
@@ -9,18 +9,26 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class InputParserTest {
-    private static final String COMMAND_LIST_GREETING = "Hello! These are all the possible commands for this habit tracker :)";
-    private static final String ADD_HABIT_COMMAND = "add a habit: add <habit name>";
-    private static final String DELETE_HABIT_COMMAND = "delete a habit: delete <habit name>";
-    private static final String SET_GOAL_COMMAND = "set a goal for a habit: set <habit name> <goal name>";
-    private static final String REMOVE_GOAL_COMMAND = "remove a goal for a habit: remove <habit name> <goal name>";
-    private static final String LIST_HABIT_COMMAND = "list all habits user has input: list";
-    private static final String LIST_GOAL_COMMAND = "list all goals for that habit: list -<habit name>";
+    private static final String COMMAND_LIST_GREETING =
+            "Hello! These are all the possible commands for this habit tracker :)";
+    private static final String ADD_HABIT_COMMAND =
+            "add a habit: add <habit name>";
+    private static final String DELETE_HABIT_COMMAND =
+            "delete a habit: delete <habit name>";
+    private static final String SET_GOAL_COMMAND =
+            "set a goal for a habit: set <habit name> <goal name>";
+    private static final String REMOVE_GOAL_COMMAND =
+            "remove a goal for a habit: remove <habit name> <goal name>";
+    private static final String LIST_HABIT_COMMAND =
+            "list all habits user has input: list";
+    private static final String LIST_GOAL_COMMAND =
+            "list all goals for that habit: list -<habit name>";
     private static final String NEWLINE = System.lineSeparator();
-    private static final String DASHES = "________________________________________________________________________________________________________________________";
+    private static final String DASHES = "______________________________________"
+            + "__________________________________________________________________________________";
 
     private final PrintStream standardOut = System.out;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
