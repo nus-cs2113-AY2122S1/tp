8b5b4ca963428a9c3944a7f114bc6e7bc1b8bf9e
ckteo <exetrtck@gmail.com>
2021-10-08 04:51:28 +0800

    Resolve coding standard violations

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -6,9 +6,11 @@ import java.util.Scanner;
 
 public class Duke {
     private static TextUI ui;
+
     /**
      * Main entry-point for the java.duke.Duke application.
      */
+
     public static void main(String[] args) {
         ui = new TextUI();
         String userInput = ui.read();
