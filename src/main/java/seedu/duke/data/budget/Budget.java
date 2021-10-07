e8415041864cedfef84d32638ec8a8eeb8544ea8
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2021-10-07 16:45:56 +0800

    Basic Parser for Commands and formatting code
    Fixes #17

--- src/main/java/seedu/duke/data/budget/Budget.java
+++ src/main/java/seedu/duke/data/budget/Budget.java
@@ -1,4 +1,18 @@
 package seedu.duke.data.budget;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
 public class Budget {
+    public static String description;
+    public static double amountInDollars;
+    // public static ArrayList<Task> tasks = new ArrayList<>();
+
+    public Budget(String description, String by) {
+        this.description = description;
+        //this.by = by;
+    }
+
+
 }
