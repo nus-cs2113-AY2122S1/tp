8c742b55ea7803866b6647211f1ba4b1a83bf8e7
datn02 <ducanh02.work@gmail.com>
2021-10-07 22:48:08 +0800

    Remove unused code

--- src/main/java/seedu/duke/ingredients/IngredientList.java
+++ src/main/java/seedu/duke/ingredients/IngredientList.java
@@ -29,66 +29,6 @@ public class IngredientList {
     }
 
     /**
-     * Adds the new ingredient to the ingredient list.
-     * @param ingredient The ingredient object to be added
-     * @return Successful Addition Message
-     */
-    public String addNewIngredient(Ingredient ingredient) {
-        // TODO: Append new ingredient to the list
-        // We allow duplicate names for v1.0
-        return ADDED_MESSAGE + ingredient.toString();
-    }
-
-    /**
-     * Lists all the ingredients stored in the ingredients list.
-     * @return the list of ingredients
-     */
-    public String listAllIngredients() {
-        String resultMsg;
-        int i;
-
-        if (ingredientList.size() == 0) {
-            resultMsg = LIST_EMPTY_MESSAGE;
-            return resultMsg;
-        }
-
-        resultMsg = LIST_MESSAGE;
-
-        for (i = 0; i < ingredientList.size() - 1; i++) {
-            resultMsg += (i + 1) + "."
-                    + ingredientList.get(i).toString() + '\n' + '\t';
-        }
-        resultMsg = resultMsg + ingredientList.size() + "." + ingredientList.get(i).toString();
-
-        return resultMsg;
-    }
-
-    /**
-     * Deletes an ingredient at the specified index in the list.
-     * @param ingredientNumber The index of the ingredient to be deleted
-     * @return The details of the deleted ingredient
-     */
-    public String deleteIngredient(int ingredientNumber) {
-        String resultMsg;
-        Ingredient removedIngredient = ingredientList.remove(ingredientNumber - 1);
-
-        resultMsg = DELETE_MESSAGE + removedIngredient.toString();
-        return resultMsg;
-    }
-
-    /**
-     * Updates the ingredient at the specified index in the list by the amount specified.
-     * @param index the index of the ingredient to be updated
-     * @param amount the amount to add to the ingredient
-     * @return The details of the updated ingredient
-     */
-    public String updateIngredient(int index, double amount) {
-        //TODO: add code to update the ingredient at the particular index by the amount specified
-
-        return ingredientList.get(index - 1).toString();
-    }
-
-    /**
      * Gets the size of the current inventory, not including the amount of individual ingredient.
      * @return the size of current inventory
      */
