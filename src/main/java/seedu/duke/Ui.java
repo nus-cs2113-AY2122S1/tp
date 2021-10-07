a85932607bf929665ba74ec070ae7c3be58134d6
yipwayyne <E0533998@u.nus.edu>
2021-10-07 16:46:57 +0800

    Committed initial Ui class

---
+++ src/main/java/seedu/duke/Ui.java
@@ -0,0 +1,30 @@
+package seedu.duke;
+import java.util.Scanner;
+
+public class Ui {
+
+    private static Scanner in = new Scanner(System.in);
+
+    public static void showLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showWelcome() {
+        showLine();
+        String logo = " █████  ███  █   █ ████  ████  █      ████  █    █ █    █ █████ ████\n"
+                + "   █   █   █ █   █ █   █ █   █ █     █    █ ██   █ ██   █ █     █   █\n"
+                + "   █   █   █ █   █ █████ ████  █     ██████ █ █  █ █ █  █ █████ █████\n"
+                + "   █   █   █ █   █ █  █  █     █     █    █ █  █ █ █  █ █ █     █  █\n"
+                + "   █    ███   ███  █   █ █     █████ █    █ █   ██ █   ██ █████ █   █\n";
+        String greet = "Hello, Welcome to TourPlanner!\n"
+                + "What can I do for you?";
+        System.out.println(logo);
+        System.out.println(greet);
+        showLine();
+    }
+
+    public String readCommand() {
+        return in.nextLine();
+    }
+}
+
