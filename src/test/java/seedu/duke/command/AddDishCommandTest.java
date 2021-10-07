e65bc7142c7ae278b0f32c38e60f54c0884ddd5a
Sridharan Arvind Srinivasan <arv1920@gmail.com>
2021-10-07 22:28:47 +0800

    Add JUnit tests for AddDishCommand class

---
+++ src/test/java/seedu/duke/command/AddDishCommandTest.java
@@ -0,0 +1,22 @@
+package seedu.duke.command;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.DishList;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AddDishCommandTest {
+
+    @Test
+    void execute_inputParameters_add() {
+        ArrayList<String> inputParams = new ArrayList<>();
+        inputParams.add("chicken rice");
+
+        Command testAddDishCommand = new AddDishCommand();
+        testAddDishCommand.execute(inputParams);
+
+        assertEquals("chicken rice", DishList.dishList.get(0).getDishName());
+    }
+}
