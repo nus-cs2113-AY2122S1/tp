a53a21b57c962978668f77eeabe9c3c4b4f1d584
Yap Wei Xuan <e0533997@u.nus.edu>
2021-10-07 23:06:22 +0800

    Reformat code

--- src/test/java/seedu/duke/commands/AddExpenditureTest.java
+++ src/test/java/seedu/duke/commands/AddExpenditureTest.java
@@ -1,4 +1,9 @@
 package seedu.duke.commands;
 
+import org.junit.jupiter.api.Test;
+import seedu.duke.data.BudgetList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AddExpenditureTest {
 }
