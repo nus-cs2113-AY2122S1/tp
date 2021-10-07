f4f24946dfce72b06eb69a8a6b2edd8d4fe42c6b
LAPTOP-H8FATR55\kairo <kairoskoh.1212@gmail.com>
2021-10-07 22:42:47 +0800

    Add basic feature for EmployeeList.java

--- src/main/java/seedu/duke/employee/EmployeeList.java
+++ src/main/java/seedu/duke/employee/EmployeeList.java
@@ -1,4 +1,10 @@
 package seedu.duke.employee;
 
+import java.util.ArrayList;
+
 public class EmployeeList {
+
+    public ArrayList<Employee> employeeList = new ArrayList<>();
+    public int totalEmployee = 0;
+
 }
