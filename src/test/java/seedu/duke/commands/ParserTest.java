4381350ce283c50cc18cb89cb2ff4a9fb4572527
ckteo <exetrtck@gmail.com>
2021-10-08 05:48:15 +0800

    Add junit tests for Parser Object

---
+++ src/test/java/seedu/duke/commands/ParserTest.java
@@ -0,0 +1,18 @@
+package seedu.duke.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+    @Test
+    public void parse_exit_ExitCommandObject() {
+        Boolean type = Parser.parse("exit") instanceof ExitCommand;
+        assertTrue(type);
+    }
+
+    @Test
+    public void parse_null_null() {
+        assertNull(Parser.parse("foo 2"));
+    }
+}
