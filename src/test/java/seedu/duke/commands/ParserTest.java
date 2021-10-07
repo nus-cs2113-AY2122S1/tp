886405589d411e66ca404f0dc68cdfb20d472ed8
ckteo <exetrtck@gmail.com>
2021-10-08 05:51:39 +0800

    Fix coding standard violations

--- src/test/java/seedu/duke/commands/ParserTest.java
+++ src/test/java/seedu/duke/commands/ParserTest.java
@@ -2,7 +2,8 @@ package seedu.duke.commands;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ParserTest {
     @Test
