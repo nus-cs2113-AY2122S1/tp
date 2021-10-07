1efe73183628b0d2daa1ef9581463d6087d0d133
swongts <70316271+swongts@users.noreply.github.com>
2021-10-07 17:15:48 +0800

    Add parser
    Fixes #10

---
+++ src/main/java/seedu/duke/Parser.java
@@ -0,0 +1,16 @@
+package seedu.duke;
+
+public class Parser {
+    public static void parse(String input) {
+        String[] commandAndParams = splitCommandString(input, " ");
+        String command = commandAndParams[0];
+        String params = commandAndParams[1];
+        switch (command) {
+        }
+    }
+
+    public static String[] splitCommandString(String input, String separator) {
+        String[] split = input.trim().split(separator, 2);
+        return split.length == 2 ? split : new String[] {split[0], ""};
+    }
+}
