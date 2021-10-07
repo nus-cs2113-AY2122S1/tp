400a4cb54e9f6e679387391d72713b5249c4fbc9
ckteo <exetrtck@gmail.com>
2021-10-08 04:39:19 +0800

    Add styling

--- src/main/java/seedu/duke/ui/TextUI.java
+++ src/main/java/seedu/duke/ui/TextUI.java
@@ -2,21 +2,29 @@ package seedu.duke.ui;
 
 import java.io.PrintStream;
 import java.util.Scanner;
+import static seedu.duke.common.Messages.LOGO;
+import static seedu.duke.common.Messages.WELCOME_MESSAGE;
 
 public class TextUI {
     private final Scanner in;
     private final PrintStream out;
+    private static String prefix = "> ";
 
     public TextUI() {
         in = new Scanner(System.in);
         out = System.out;
+        print(LOGO);
+        print(WELCOME_MESSAGE);
     }
 
     public String read() {
-        return in.nextLine();
+        out.print(prefix);
+        String input = in.nextLine();
+        return input;
     }
 
     public void print(String message) {
         out.println(message);
     }
+
 }
