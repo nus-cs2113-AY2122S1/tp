62e2751de0659a92b81e45647fd00a4199ccae82
xuanlc113 <xuan.lc113@gmail.com>
2021-10-07 14:31:11 +0800

    load timetable data from file on startup

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -3,12 +3,14 @@ package seedu.duke;
 import seedu.command.Command;
 import seedu.parser.CommandParser;
 import seedu.storage.ModStorage;
+import seedu.storage.TimetableStorage;
 import seedu.timetable.Timetable;
 import seedu.ui.TextUi;
 
 public class Duke {
     private static String path = "data/Modules.json";
-    public static Timetable timetable = new Timetable(1);
+    private static String timetablePath = "data/timetable.json";
+    public static Timetable timetable;
     public static CommandParser commandParser = new CommandParser();
 
     public static void main(String[] args) {
@@ -16,6 +18,7 @@ public class Duke {
     }
 
     private void setup() {
+        timetable = new TimetableStorage(timetablePath).loadSchedule();
         TextUi.printWelcomeMessage();
         run();
     }
