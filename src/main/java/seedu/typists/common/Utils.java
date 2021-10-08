ca714a4c078831befc8eaf87689819d8c07f5858
Isabella-L <isabellalu01@gmail.com>
2021-10-08 22:30:14 +0800

    Update Utils.java

--- src/main/java/seedu/typists/common/Utils.java
+++ src/main/java/seedu/typists/common/Utils.java
@@ -1,11 +1,7 @@
 package seedu.typists.common;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
- * Utility methods
+ * Utility methods.
  */
 public class Utils {
 
