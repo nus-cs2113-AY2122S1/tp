c332edb0faa2bb9680c972b7d2da834bb1383e47
qqkoh <69311858+qqkoh@users.noreply.github.com>
2021-10-08 23:57:30 +0800

    Update AddExerciseCommandTest.java

--- src/test/java/seedu/duke/command/exercise/AddExerciseCommandTest.java
+++ src/test/java/seedu/duke/command/exercise/AddExerciseCommandTest.java
@@ -8,7 +8,8 @@ import seedu.duke.lists.WorkoutList;
 import seedu.duke.storage.Storage;
 import seedu.duke.ui.Ui;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AddExerciseCommandTest {
     //featureUnderTest_testScenario_expectedBehavior()
