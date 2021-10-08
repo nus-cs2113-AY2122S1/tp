c5da78db3098a8b1672888e7296366115c4a32f3
deon <dchung2422@gmail.com>
2021-10-08 23:31:06 +0800

    Improve code and Modify containsInvalidParameters

--- src/main/java/command/medicine/UpdateCommand.java
+++ src/main/java/command/medicine/UpdateCommand.java
@@ -29,7 +29,7 @@ public class UpdateCommand extends Command {
             CommandParameters.MAX_QUANTITY};
 
         boolean isInvalidParameter = CommandSyntax.containsInvalidParameters(ui, parameters, requiredParameter,
-                optionalParameters, CommandSyntax.UPDATE_COMMAND, "update");
+                optionalParameters, CommandSyntax.UPDATE_COMMAND, true);
         if (isInvalidParameter) {
             return;
         }
