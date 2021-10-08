f2475cb975faf8673da0c4ca50ad5537ab74c847
Isabella-L <isabellalu01@gmail.com>
2021-10-08 22:20:55 +0800

    improve skeleton
    To pass CI test

--- src/main/java/seedu/TypingGame/data/GamerInfo.java
+++ src/main/java/seedu/TypingGame/data/GamerInfo.java
@@ -1,10 +1,5 @@
 package seedu.TypingGame.data;
 
-import seedu.TypingGame.data.person.Person;
-import seedu.TypingGame.data.person.ReadOnlyPerson;
-import seedu.TypingGame.data.person.UniquePersonList;
-import seedu.TypingGame.data.person.UniquePersonList.DuplicatePersonException;
-import seedu.TypingGame.data.person.UniquePersonList.PersonNotFoundException;
 
 /**
  * Contains the data of the entire game.
