1358a78945ec7efec829eeb2f33307a13c1ff234
avellinwong01 <avellin.wong@gmail.com>
2021-10-10 02:23:14 +0800

    Add JUnit test for Unknown Command Class

--- src/test/java/seedu/duke/commands/ParserTest.java
+++ src/test/java/seedu/duke/commands/ParserTest.java
@@ -2,18 +2,18 @@ package seedu.duke.commands;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ParserTest {
     @Test
     public void parse_exit_ExitCommandObject() {
-        Boolean type = Parser.parse("exit") instanceof ExitCommand;
+        boolean type = Parser.parse("exit") instanceof ExitCommand;
         assertTrue(type);
     }
 
     @Test
-    public void parse_null_null() {
-        assertNull(Parser.parse("foo 2"));
+    public void parse_unknown_UnknownCommandObject() {
+        boolean type = Parser.parse("foo 2") instanceof UnknownCommand;
+        assertTrue(type);
     }
 }
