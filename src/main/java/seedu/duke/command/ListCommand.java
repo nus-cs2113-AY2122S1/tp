40f9de6e69bf4b6a0ccb24f78214a54a30f178b0
APZH <amospzh@gmail.com>
2021-10-09 22:41:15 +0800

    add ListCommand object

--- src/main/java/seedu/duke/command/ListCommand.java
+++ src/main/java/seedu/duke/command/ListCommand.java
@@ -1,15 +1,26 @@
 package seedu.duke.command;
 
+import seedu.duke.exception.EmptyTasklistException;
 import seedu.duke.task.TaskManager;
 
 import java.util.Map;
 
 public class ListCommand extends Command {
+
     public ListCommand(TaskManager taskManager, Map<String, String> commandOptions) {
+        super(taskManager, commandOptions);
     }
 
     @Override
     public CommandResult executeCommand() throws Exception {
-        return null;
+        String message = "";
+
+        try {
+            message = taskManager.listTasklist();
+        } catch (EmptyTasklistException e) {
+            message = e.toString();
+        }
+
+        return new CommandResult(taskManager, message, false, false);
     }
 }
