2c6f5ba1d1645300357b239b86fbcae9bc4ea289
Demonshaha <e0492492@u.nus.edu>
2021-10-09 10:56:38 +0800

    tostring override

--- src/main/java/seedu/duke/Client.java
+++ src/main/java/seedu/duke/Client.java
@@ -1,21 +1,23 @@
 package seedu.duke;
 
 public class Client {
-    private String name;
-    private String contactNum;
-    private String flight;
-    private String tour;
+    private final String name;
+    private final String contactNum;
+    private final String flight;
+    private final String accomms;
+    private final String tour;
 
-    public Client(String name, String contactNum, String flight, String tour) {
-        this.name = name;
-        this.contactNum = contactNum;
-        this.flight = flight;
-        this.tour = tour;
+    public Client(String[] values) {
+        name = values[0];
+        contactNum = values[1];
+        flight = values[2];
+        accomms = values[3];
+        tour = values[4];
     }
 
     @Override
     public String toString() {
         return "Client's name: " + name + System.lineSeparator() + "Client's contactNum: " + contactNum + System.lineSeparator()
-                + "Client's flight number: " + flight + System.lineSeparator() + "Client's tour" + tour + System.lineSeparator();
+                + "Client's flight number: " + flight + System.lineSeparator() + "Client's accomms: " + accomms + System.lineSeparator() + "Client's tour" + tour + System.lineSeparator();
     }
 }
