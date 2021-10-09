e1e912b5a16a0cd8448854ac8760203637180265
rachel <rach3lkeh@gmail.com>
2021-10-10 02:20:04 +0800

    update code to pass checkstyle test.

--- src/main/java/taa/Parser.java
+++ src/main/java/taa/Parser.java
@@ -1,7 +1,17 @@
 package taa;
 
-import taa.command.*;
+import taa.command.AddAssessmentCommand;
+import taa.command.AddModuleCommand;
+import taa.command.AddStudentCommand;
+import taa.command.Command;
+import taa.command.DeleteStudentCommand;
+import taa.command.EditStudentCommand;
 import taa.command.ExitCommand;
+import taa.command.ExitCommand;
+import taa.command.FindStudentCommand;
+import taa.command.ListAssessmentsCommand;
+import taa.command.ListModulesCommand;
+import taa.command.ListStudentsCommand;
 
 import java.util.ArrayList;
 import java.util.Collections;
