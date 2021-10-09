5e570012224ad92ae5d1cdbf1db0c44675fae188
avellinwong01 <avellin.wong@gmail.com>
2021-10-10 01:55:32 +0800

    Add Unknown Command Class

--- src/main/java/seedu/duke/common/Messages.java
+++ src/main/java/seedu/duke/common/Messages.java
@@ -15,4 +15,5 @@ public class Messages {
             + "";
     public static final String WELCOME_MESSAGE = "Welcome to libmgr";
     public static final String EXIT_MESSAGE = "See you soon!";
+    public static final String UNKNOWN_MESSAGE = "Sorry, I don't understand what you mean";
 }
