6e383e22a4f095f2452b55da991228d4b35a60de
Leyond <leyondlee@users.noreply.github.com>
2021-10-09 20:37:09 +0800

    Add Util class for basic utility functions

---
+++ src/main/java/taa/util/Util.java
@@ -0,0 +1,15 @@
+package taa.util;
+
+public class Util {
+    public static boolean isInteger(String string) {
+        boolean isInt;
+        try {
+            int value = Integer.parseInt(string);
+            isInt = true;
+        } catch (NumberFormatException e) {
+            isInt = false;
+        }
+
+        return isInt;
+    }
+}
