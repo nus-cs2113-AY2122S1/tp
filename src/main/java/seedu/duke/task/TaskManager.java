dc0091ba3a08bb4cf1d6366de2e571ab8da1a35f
APZH <amospzh@gmail.com>
2021-10-09 22:43:47 +0800

    add skeleton for TaskManager class (getters and setters for tasklist) + method to list the tasklist
    - The tasklist is stored as an arraylist of task objects
    - Add assertion to ensure that the size of the tasklist is not negative
    - Add exception to handle if the tasklist contains 0 tasks

--- src/main/java/seedu/duke/task/TaskManager.java
+++ src/main/java/seedu/duke/task/TaskManager.java
@@ -1,5 +1,41 @@
 package seedu.duke.task;
 
+import seedu.duke.exception.EmptyTasklistException;
+
+import java.util.ArrayList;
+
 public class TaskManager {
+
+    private ArrayList<Task> tasklist;
+
+    public TaskManager() {
+        this.tasklist = new ArrayList<Task>();
+    }
+
+    public String listTasklist() throws EmptyTasklistException {
+        assert tasklist.size() < 0 : "Tasklist cannot be negative";
+
+        if (tasklist.size() == 0) {
+            throw new EmptyTasklistException();
+        }
+
+        String tasks = "";
+
+        for (int i = 0; i < tasklist.size(); i++) {
+            
+        }
+
+
+    }
+
+    public ArrayList<Task> getTasklist() {
+        return this.tasklist;
+    }
+
+    public void setTasklist(ArrayList<Task> tasklist) {
+        this.tasklist = tasklist;
+    }
+
+
 
 }
