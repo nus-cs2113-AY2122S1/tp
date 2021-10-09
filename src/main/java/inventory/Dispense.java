953becc729e90b7b38df01e394894183e5f0de80
alvintan01 <35805635+alvintan01@users.noreply.github.com>
2021-10-09 23:47:43 +0800

    Update Dispense and Order objects

--- src/main/java/inventory/Dispense.java
+++ src/main/java/inventory/Dispense.java
@@ -8,15 +8,44 @@ import java.util.Date;
  */
 
 public class Dispense extends Medicine {
+    public static final String ID = "ID";
+    public static final String NAME = "NAME";
+    public static final String QUANTITY = "QUANTITY";
+    public static final String CUSTOMERS_NRIC = "CUSTOMER'S NRIC";
+    public static final String DATE = "DATE";
+    public static final String STAFF_NAME = "STAFF NAME";
+
+    public static final String[] COLUMNS = {ID, NAME, QUANTITY, CUSTOMERS_NRIC, DATE, STAFF_NAME};
+
+    private static int dispenseCount = 0;
+    protected int dispenseId;
     protected String customerNric;
     protected Date date;
     protected String staffName;
 
-    public Dispense(String medicineName, int quantity, String customerName, Date date, String staffName) {
+    public Dispense(String medicineName, int quantity, String customerNric, Date date, String staffName) {
         super(medicineName, quantity);
-        this.customerNric = customerName;
+        dispenseCount++;
+        this.dispenseId = dispenseCount;
+        this.customerNric = customerNric;
         this.date = date;
         this.staffName = staffName;
+    }
+
+    public static int getDispenseCount() {
+        return dispenseCount;
+    }
+
+    public static void setDispenseCount(int dispenseCount) {
+        Dispense.dispenseCount = dispenseCount;
+    }
+
+    public int getDispenseId() {
+        return dispenseId;
+    }
+
+    public void setDispenseId(int dispenseId) {
+        this.dispenseId = dispenseId;
     }
 
     public String getCustomerNric() {
--- src/main/java/inventory/Order.java
+++ src/main/java/inventory/Order.java
@@ -7,7 +7,15 @@ import java.util.Date;
  */
 
 public class Order extends Medicine {
-    public static int orderCount = 0;
+    public static final String ID = "ID";
+    public static final String NAME = "NAME";
+    public static final String QUANTITY = "QUANTITY";
+    public static final String DATE = "DATE";
+    public static final String STATUS = "STATUS";
+
+    public static final String[] COLUMNS = {ID, NAME, QUANTITY, DATE, STATUS};
+
+    private static int orderCount = 0;
     protected int orderId;
     protected Date date;
     protected boolean isDelivered;
@@ -48,7 +56,15 @@ public class Order extends Medicine {
         return isDelivered;
     }
 
-    public void setDelivered(boolean delivered) {
-        isDelivered = delivered;
+    public void setDelivered() {
+        isDelivered = true;
+    }
+
+    public String getStatus() {
+        if (isDelivered) {
+            return "DELIVERED";
+        } else {
+            return "PENDING";
+        }
     }
 }
