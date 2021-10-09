9e7553bb228f554de1f3e0bddf358ed334157826
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2021-10-10 01:28:17 +0800

    Abstract parseDetail function to abstract ContactParser as it is shared

--- src/main/java/seedu/parser/ContactParser.java
+++ src/main/java/seedu/parser/ContactParser.java
@@ -1,6 +1,7 @@
 package seedu.parser;
 
 import seedu.contact.DetailType;
+import seedu.exception.ForbiddenDetailException;
 import seedu.exception.InvalidFlagException;
 import seedu.exception.MissingArgException;
 import seedu.exception.MissingDetailException;
@@ -8,7 +9,8 @@ import seedu.exception.MissingDetailException;
 public abstract class ContactParser {
     public static final int CONTACT_PARAMS_START_INDEX = 1;
     public static final int NUMBER_OF_DETAILS = 2;
-    public static final int NUMBER_OF_EDIT_DETAILS = 3;
+    public static final int NUMBER_OF_ADD_ARGS = 2;
+    public static final int NUMBER_OF_EDIT_ARGS = 3;
 
     public static final String DETAIL_SEPARATOR = " -";
     public static final int FLAG_INDEX_IN_DETAILS = 0;
@@ -20,6 +22,45 @@ public abstract class ContactParser {
     public static final String GITHUB_FLAG = "g";
 
     public abstract String[] parseContactDetails(String userInput) throws InvalidFlagException,
-            MissingArgException, MissingDetailException;
+            MissingArgException, MissingDetailException, ForbiddenDetailException;
 
+    /**
+     * This method takes in the contactDetails array and populates it with contact details.
+     * It will sift out the flags to decide what details to populate, using the
+     * enumeration from DetailType.
+     *
+     * @param contactDetails String array containing contact details
+     * @param detail         Unparsed detail
+     * @throws InvalidFlagException If the flag given is not recognised
+     */
+    public void parseDetail(String[] contactDetails, String detail)
+            throws InvalidFlagException, MissingDetailException, ForbiddenDetailException {
+        String[] destructuredDetails = detail.split(" ", NUMBER_OF_DETAILS);
+        //for commands that specify a flag, but do not specify any argument for that flag
+        //IndexOutOfBoundsException should not be thrown as the first if case will be true
+        if (destructuredDetails.length == 1 || destructuredDetails[1].trim().equalsIgnoreCase("")) {
+            throw new MissingDetailException();
+        }
+        int indexToStore;
+        assert destructuredDetails.length == NUMBER_OF_DETAILS;
+        String flag = destructuredDetails[FLAG_INDEX_IN_DETAILS];
+        String detailToStore = destructuredDetails[DETAIL_INDEX_IN_DETAILS].trim();
+        indexToStore = getIndexToStore(flag);
+        contactDetails[indexToStore] = detailToStore;
+    }
+
+    private int getIndexToStore(String flag) throws InvalidFlagException {
+        int indexToStore;
+        switch (flag) {
+        case NAME_FLAG:
+            indexToStore = DetailType.NAME.getIndex();
+            break;
+        case GITHUB_FLAG:
+            indexToStore = DetailType.GITHUB.getIndex();
+            break;
+        default:
+            throw new InvalidFlagException();
+        }
+        return indexToStore;
+    }
 }
