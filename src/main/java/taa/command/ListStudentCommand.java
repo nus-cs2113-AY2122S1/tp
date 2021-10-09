d9745a088ef72db444d79d6318990ff8e9788fa2
Ho Zhen Hong <hozhenhong99@gmail.com>
2021-10-09 16:03:27 +0800

    implement basic commands for students

---
+++ src/main/java/taa/command/ListStudentCommand.java
@@ -0,0 +1,2 @@
+package taa.command;public class ListStudentCommand {
+}
---
+++ src/main/java/taa/command/ListStudentsCommand.java
@@ -0,0 +1,48 @@
+package taa.command;
+
+import taa.CustomException;
+import taa.Ui;
+import taa.module.Module;
+import taa.module.ModuleList;
+import taa.student.Student;
+
+public class ListStudentsCommand extends Command {
+
+    private static final String MESSAGE_STUDENT_ADDED_FORMAT = "Student %s, %s has been added to %s";
+    private static final String[] ADD_STUDENT_ARGUMENT_KEYS = {"c","s","i"};
+
+    public AddStudentCommand(String argument) {
+        super(argument, ADD_STUDENT_ARGUMENT_KEYS);
+    }
+
+    @Override
+    public void execute(ModuleList modules, Ui ui) throws CustomException {
+        if (argument.isEmpty()) {
+            // TODO Usage format message
+            throw new CustomException("");
+        }
+
+        if (!checkArgumentMap()) {
+            // TODO Invalid/missing arguments message
+            throw new CustomException("");
+        }
+
+        String moduleCode = argumentMap.get("c");
+        if (moduleCode.contains(" ")) {
+            // TODO Invalid module code message
+            throw new CustomException("");
+        }
+
+        String studentName = argumentMap.get("s");
+        String studentID = argumentMap.get("i");
+        Student student = new Student(studentName, studentID);
+        Module module = modules.getModule(moduleCode);
+        if (module == null) {
+            // TODO module not found message
+            throw new CustomException("Module not found");
+        }
+        module.addStudent(student);
+
+        ui.printMessage(String.format(MESSAGE_STUDENT_ADDED_FORMAT, studentName, studentID, moduleCode));
+    }
+}
