b77d475e644f31802642494f73c937921d59816f
Leyond <leyondlee@users.noreply.github.com>
2021-10-09 11:53:42 +0800

    Add function to search for module and comments

--- src/main/java/taa/module/ModuleList.java
+++ src/main/java/taa/module/ModuleList.java
@@ -19,13 +19,42 @@ public class ModuleList {
         return modules.size();
     }
 
+    /**
+     * Checks if an index is valid with respect to the modules list.
+     *
+     * @param index The index to check.
+     * @return true if valid, else false.
+     */
     public boolean isValidIndex(int index) {
         return (index >= 0 && index < getSize());
     }
 
+    /**
+     * Gets a module at the specified index.
+     *
+     * @param index The index of the module.
+     * @return A module object if index is valid, else null.
+     */
     public Module getModuleAt(int index) {
         if (isValidIndex(index)) {
             return modules.get(index);
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets the module from the list with the specified code.
+     *
+     * @param code The module code to search for.
+     * @return A Module object if found, else null.
+     */
+    public Module getModule(String code) {
+        for (int i = 0; i < getSize(); i += 1) {
+            Module module = getModuleAt(i);
+            if (module.getCode().equals(code)) {
+                return module;
+            }
         }
 
         return null;
