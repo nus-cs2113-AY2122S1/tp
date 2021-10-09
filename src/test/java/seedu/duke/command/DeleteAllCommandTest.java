390650781e6b785987c2ab623b63b1e01daffff0
ptejasv <e0564194@u.nus.edu>
2021-10-09 16:23:47 +0800

    Add JUnit tests for delete commands

---
+++ src/test/java/seedu/duke/command/DeleteAllCommandTest.java
@@ -0,0 +1,38 @@
+package seedu.duke.command;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.exception.DukeException;
+import seedu.duke.lesson.Lesson;
+import seedu.duke.lesson.LessonList;
+import seedu.duke.storage.Storage;
+import seedu.duke.task.Task;
+import seedu.duke.task.TaskList;
+import seedu.duke.ui.Ui;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class DeleteAllCommandTest {
+    @Test
+    public void testDeleteAll() {
+        Ui ui = new Ui();
+        TaskList taskList = new TaskList();
+        LessonList lessonList = new LessonList();
+        Storage storage = new Storage();
+
+        taskList.addTask(new Task("task 1", "mon", ""));
+        taskList.addTask(new Task("task 2", "fri", "someInfo"));
+        lessonList.addLesson(new Lesson("lesson 1", "tue", "2pm", "6pm"));
+        lessonList.addLesson(new Lesson("lesson 2", "thu", "1pm", "2pm"));
+
+        try {
+            Command deleteAllCommand = new DeleteAllCommand();
+            deleteAllCommand.execute(ui, taskList, lessonList, storage);
+            assertTrue(taskList.isEmpty());
+            assertTrue(lessonList.isEmpty());
+        } catch (DukeException e) {
+            fail();
+        }
+    }
+
+}
---
+++ src/test/java/seedu/duke/command/DeleteTaskCommandTest.java
@@ -0,0 +1,70 @@
+package seedu.duke.command;
+
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.exception.DukeException;
+import seedu.duke.lesson.LessonList;
+import seedu.duke.storage.Storage;
+import seedu.duke.task.Task;
+import seedu.duke.task.TaskList;
+import seedu.duke.ui.Ui;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class DeleteTaskCommandTest {
+    @Test
+    public void testDeleteAllTasks() {
+        Ui ui = new Ui();
+        TaskList taskList = new TaskList();
+        LessonList lessonList = new LessonList();
+        Storage storage = new Storage();
+
+        taskList.addTask(new Task("task 1", "mon", ""));
+        taskList.addTask(new Task("task 2", "fri", "someInfo"));
+
+        try {
+            Command deleteAllTasksCommand = new DeleteTaskCommand();
+            deleteAllTasksCommand.execute(ui, taskList, lessonList, storage);
+            assertTrue(taskList.isEmpty());
+        } catch (DukeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testDeleteTask() {
+        Ui ui = new Ui();
+        TaskList taskList = new TaskList();
+        LessonList lessonList = new LessonList();
+        Storage storage = new Storage();
+
+        taskList.addTask(new Task("task 1", "mon", ""));
+        taskList.addTask(new Task("task 2", "fri", "someInfo"));
+
+        try {
+            Command deleteTaskCommand = new DeleteTaskCommand(1);
+            deleteTaskCommand.execute(ui, taskList, lessonList, storage);
+            assertEquals(1, taskList.getSize());
+        } catch (DukeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testDeleteOobTask() {
+        Ui ui = new Ui();
+        TaskList taskList = new TaskList();
+        LessonList lessonList = new LessonList();
+        Storage storage = new Storage();
+
+        taskList.addTask(new Task("task 1", "mon", ""));
+        taskList.addTask(new Task("task 2", "fri", "someInfo"));
+
+        Command deleteOobTaskCommand = new DeleteTaskCommand(4);
+        assertThrows(DukeException.class, () -> deleteOobTaskCommand.execute(ui, taskList, lessonList, storage));
+    }
+
+}
