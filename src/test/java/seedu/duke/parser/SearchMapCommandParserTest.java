3c7b3b18e4ab2b5bed8ac87f89cfb7f512d8dc68
titustortoiseturtle1999 <ngtitus@ymail.com>
2021-10-09 22:11:05 +0800

    fix checkstyle error

--- src/test/java/seedu/duke/parser/SearchMapCommandParserTest.java
+++ src/test/java/seedu/duke/parser/SearchMapCommandParserTest.java
@@ -1,24 +1,19 @@
 package seedu.duke.parser;
 
 import org.junit.jupiter.api.Test;
-import org.w3c.dom.xpath.XPathResult;
 import seedu.duke.commands.SearchMapCommand;
-import seedu.duke.modules.Module;
 import seedu.duke.storage.Storage;
-import seedu.duke.universities.University;
 import seedu.duke.universities.UniversityList;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SearchMapCommandParserTest {
 
     @Test
-    public void testCorrectUniversityName_success() throws IOException{
+    public void testCorrectUniversityName_success() throws IOException {
         try {
             UniversityList universityMasterList = new UniversityList(Storage.loadUniversities());
             SearchMapCommand command = new SearchMapCommandParser().parse("Aalto University", universityMasterList);
