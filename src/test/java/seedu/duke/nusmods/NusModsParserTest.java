2d6730b90862249ec06d7a71d19024a4c89c9d2a
Hu Jialun <hujialun@comp.nus.edu.sg>
2021-10-11 01:48:26 +0800

    Add unit tests for NusModsParser
    This tests a successful scenario and a failure where the network is down and no local cache is available.
    
    It relies on the CS2113T data in AY20/21. We may want to write fixed data into a file for testing later to ensure reproducibility.

---
+++ src/test/java/seedu/duke/nusmods/NusModsParserTest.java
@@ -0,0 +1,34 @@
+package seedu.duke.nusmods;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+import seedu.duke.task.type.Event;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class NusModsParserTest {
+    NusModsParser parser = new NusModsParser();
+
+    @Test
+    void getModuleEvents_CS2113T_success() throws IOException {
+        Event[] moduleEvents = parser.getModuleEvents("CS2113T", Collections.singleton("C02"));
+        assertEquals(4, moduleEvents.length);
+    }
+
+    @Test
+    void getModuleEvents_noNetworkAndNoLocalCache_failure() {
+        try {
+            FileUtils.deleteDirectory(new File(NusModsParser.CACHEDIR)); // remove local cache if existing
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.setProperty("https.proxyHost", "123.456.7.8"); // simulate network down
+        assertThrows(IOException.class,
+                () -> parser.getModuleEvents("CS2113T", Collections.singleton("C02")));
+    }
+}
