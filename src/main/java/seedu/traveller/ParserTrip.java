5cb7ae948bdcf1ca78a28fa9b6c70ba567e0af29
gavienwz <e0544185@u.nus.edu>
2021-10-10 15:29:04 +0800

    Add edit command

--- src/main/java/seedu/traveller/ParserTrip.java
+++ src/main/java/seedu/traveller/ParserTrip.java
@@ -1,10 +1,6 @@
 package seedu.traveller;
 
-import seedu.traveller.commands.Command;
-import seedu.traveller.commands.ExitCommand;
-import seedu.traveller.commands.NewCommand;
-import seedu.traveller.commands.ViewAllCommand;
-import seedu.traveller.commands.DeleteCommand;
+import seedu.traveller.commands.*;
 import seedu.traveller.exceptions.CommandNotFoundException;
 import seedu.traveller.exceptions.InvalidNewFormatException;
 import seedu.traveller.exceptions.TravellerException;
@@ -41,6 +37,21 @@ public class ParserTrip {
             }
 
             break;
+//        case "edit":
+//            try {
+//                String tripName = userInput[1];
+//                String origin = userInput[2];
+//                String destination = userInput[3];
+//                Vertex s = GraphList.findVertex(origin);
+//                Vertex t = GraphList.findVertex(destination);
+//                List<Vertex> path = Dijkstra.run(s, t);
+//                command = new EditCommand(tripName, origin, destination, path);
+//            } catch (ArrayIndexOutOfBoundsException e) {
+//                throw new InvalidNewFormatException();
+//            } catch (EmptyVertexException e) {
+//                System.out.println("Either of the nodes doesn't exist!");
+//            }
+//            break;
         case "delete":
             command = new DeleteCommand(userInput[1]);
             break;
