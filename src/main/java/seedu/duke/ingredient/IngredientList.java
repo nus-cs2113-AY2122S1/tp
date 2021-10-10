1f0f59eba2db3237284f1525600e078f2b49075b
leecheokfeng <leecheokfeng@gmail.com>
2021-10-10 21:01:45 +0800

    Create ingredient class and functions to list/remove/add

--- src/main/java/seedu/duke/ingredient/IngredientList.java
+++ src/main/java/seedu/duke/ingredient/IngredientList.java
@@ -1,4 +1,8 @@
 package seedu.duke.ingredient;
 
+import java.util.ArrayList;
+
 public class IngredientList {
+    public ArrayList<Ingredient> ingredientList = new ArrayList<>();
+    public int totalIngredients = 0;
 }
