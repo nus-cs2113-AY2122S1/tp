d35c648fbf6f1d9642675978824ca99a44574a44
crabnuggets <alvinpang321@gmail.com>
2021-10-11 01:27:37 +0800

    Update file saving for dateTime object compatibility

--- src/main/java/seedu/duke/storage/EventDecoder.java
+++ src/main/java/seedu/duke/storage/EventDecoder.java
@@ -1,16 +1,18 @@
 package seedu.duke.storage;
 
+import seedu.duke.Parser;
 import seedu.duke.items.Event;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EventDecoder {
 
     private static final int INDEX_OF_TITLE = 0;
-    private static final int INDEX_OF_DATETIME = 1;
-    private static final int INDEX_OF_VENUE = 2;
-    private static final int INDEX_OF_DESCRIPTION = 3;
+    private static final int INDEX_OF_DESCRIPTION = 1;
+    private static final int INDEX_OF_DATETIME = 2;
+    private static final int INDEX_OF_VENUE = 3;
     private static final int INDEX_OF_BUDGET = 4;
 
     public static ArrayList<Event> decodeEventsList(List<String> encodedEventList) {
@@ -27,7 +29,7 @@ public class EventDecoder {
         String eventDateTime = eventDetails[INDEX_OF_DATETIME];
         String eventVenue = eventDetails[INDEX_OF_VENUE];
         String eventDescription = eventDetails[INDEX_OF_DESCRIPTION];
-        int eventBudget = Integer.parseInt(eventDetails[INDEX_OF_BUDGET]);
+        double eventBudget = Double.parseDouble(eventDetails[INDEX_OF_BUDGET]);
 
         return new Event(eventTitle, eventDescription, eventDateTime, eventVenue, eventBudget);
     }
