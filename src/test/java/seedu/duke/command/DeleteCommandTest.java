84497788b49887a07c8fed0983c3314b188eef5a
datn02 <ducanh02.work@gmail.com>
2021-10-10 11:26:20 +0800

    Add Junit tests for command classes

---
+++ src/test/java/seedu/duke/command/DeleteCommandTest.java
@@ -0,0 +1,20 @@
+package seedu.duke.command;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.exceptions.DukeException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class DeleteCommandTest {
+    @Test
+    public void deleteCommandTest_invalidNumberInput_expectException() {
+        try {
+            int removeNumber = -1;
+            String resultMsg = new DeleteCommand(removeNumber).run();
+            fail();
+        } catch (DukeException e) {
+            assertEquals("Ingredient number have not existed!", e.getMessage());
+        }
+    }
+}
