dea82bb821ac1c367c9b6d9913dd492807746264
qqkoh <qianqikoh@protonmail.com>
2021-10-11 09:22:28 +0800

    Add more JUnit parser tests.

--- src/test/java/seedu/duke/parser/ParserTest.java
+++ src/test/java/seedu/duke/parser/ParserTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import seedu.duke.command.Command;
 import seedu.duke.command.IncorrectCommand;
+import seedu.duke.command.exercise.AddExerciseCommand;
+import seedu.duke.command.exercise.MarkExerciseAsDoneCommand;
 import seedu.duke.command.workout.DeleteWorkoutCommand;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -35,7 +37,7 @@ class ParserTest {
     }
 
     @Test
-    void parseCommand_deleteWorkoutCommandWrong_returnsIncorrectCommand() {
+    void parseCommand_deleteWorkoutCommandNoWorkoutIndex_returnsIncorrectCommand() {
         String[] inputs = {"delete", "delete /w ", "delete "};
         for (String input : inputs) {
             Command result = parser.parseCommand(input);
@@ -50,4 +52,40 @@ class ParserTest {
         DeleteWorkoutCommand result = (DeleteWorkoutCommand) parser.parseCommand(input);
         assertEquals(result.getWorkoutIndex(), testIndex);
     }
+
+    @Test
+    void parseCommand_addExerciseCommandNoWorkoutIndex_returnsIncorrectCommand() {
+        String input = "add /w /e Exercise /s 10 /r 100";
+        Command result = parser.parseCommand(input);
+        assertTrue(result instanceof IncorrectCommand);
+    }
+
+    @Test
+    void parseCommand_addExerciseCommandNoSets_returnsIncorrectCommand() {
+        String input = "add /w 1 /e Exercise /s  /r 100";
+        Command result = parser.parseCommand(input);
+        assertTrue(result instanceof IncorrectCommand);
+    }
+
+    @Test
+    void parseCommand_addExerciseCorrect_returnsAddExerciseCommand() {
+        String input = "add /w 1 /e Exercise /s 10 /r 100";
+        Command result = parser.parseCommand(input);
+        assertTrue(result instanceof AddExerciseCommand);
+    }
+
+    @Test
+    void parseCommand_markExerciseAsDoneNoWorkoutIndex_returnsIncorrectCommand() {
+        String input = "done /w 1 /e ";
+        Command result = parser.parseCommand(input);
+        assertTrue(result instanceof IncorrectCommand);
+    }
+
+    @Test
+    void parseCommand_markExerciseAsDoneCorrect_returnsMarkExerciseAsDoneCommand() {
+        String input = "done /w 1 /e 3";
+        Command result = parser.parseCommand(input);
+        assertTrue(result instanceof MarkExerciseAsDoneCommand);
+    }
+
 }
