82983567674b82c24718835dee82c247e82d022a
Isabella-L <isabellalu01@gmail.com>
2021-10-11 15:57:56 +0800

    Create start page
    Create landing page for typist.

--- src/main/java/seedu/typists/Main.java
+++ src/main/java/seedu/typists/Main.java
@@ -1,21 +1,33 @@
 package seedu.typists;
 
-import java.util.Scanner;
+import seedu.typists.parser.Parser;
+import seedu.typists.ui.TextUi;
 
+
+//solution below adapted from https://github.com/se-edu/addressbook-level2/
 public class Main {
+    /** Version info of the program. */
+    public static final String VERSION = "Typist - Version 1.0";
+    TextUi UiBot;
+    Parser ParseBot;
+
+    public void start() {
+        this.UiBot = new TextUi();
+        this.ParseBot = new Parser();
+        UiBot.showWelcomeMessage(VERSION);
+    }
+
+    /** Runs the program until termination.  */
+    public void run() {
+        start();
+//        runCommandLoopUntilExitCommand();
+//        exit();
+    }
+
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+        new Main().run();
     }
 }
--- src/main/java/seedu/typists/ui/TextUi.java
+++ src/main/java/seedu/typists/ui/TextUi.java
@@ -1,9 +1,48 @@
 package seedu.typists.ui;
 
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+import static java.lang.System.lineSeparator;
+import static java.lang.System.out;
+import static seedu.typists.common.Messages.LOGO;
+import static seedu.typists.common.Messages.MESSAGE_ACKNOWLEDGE;
+import static seedu.typists.common.Messages.MESSAGE_WELCOME;
+
 /**
  * Text UI of the application.
  */
 public class TextUi {
 
+    private final SimpleDateFormat timeFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    private final String DIVIDER = "****************************************************************";
+    private final String LINE_PREFIX = "     | ";
+    private final String LS = lineSeparator();
+
+    public String getTimeStamp() {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        return timeFormatter.format(timestamp);
+    }
+
+    //solution below adapted from https://github.com/se-edu/addressbook-level2/
+    public void showWelcomeMessage(String version) {
+        getTimeStamp();
+        printScreen(
+                version,
+                getTimeStamp(),
+                DIVIDER,
+                LOGO,
+                MESSAGE_WELCOME,
+                MESSAGE_ACKNOWLEDGE,
+                DIVIDER
+        );
+    }
+
+    public void printScreen(String... message) {
+        for (String m: message) {
+            out.println(LINE_PREFIX + m.replace("\n", LS + LINE_PREFIX));
+        }
+    }
 }
