e3e6c693b9ca7ba92a58da8a36d42581de36cda4
AnShengLee <anshengl@yahoo.com.sg>
2021-10-11 18:39:07 +0800

    Add exceptions mainly for Parser class

---
+++ src/main/java/seedu/exceptions/InvalidIncomeIndexException.java
@@ -0,0 +1,7 @@
+package seedu.exceptions;
+
+public class InvalidIncomeIndexException extends Exception {
+    public InvalidIncomeIndexException(String message) {
+        super(message);
+    }
+}
