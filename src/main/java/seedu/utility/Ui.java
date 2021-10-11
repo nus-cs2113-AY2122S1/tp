0c426120b83d0f360b9dbf407009bc85d1199e74
KZQ1999 <e0543569@u.nus.edu>
2021-10-11 22:50:57 +0800

    Replaced dummy help method with actual printing messages of various formats and commands, made user feedback clearer with help of lines

--- src/main/java/seedu/utility/Ui.java
+++ src/main/java/seedu/utility/Ui.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Ui {
+    public static final String HELP_COMMAND_MESSAGE = "This is a list of commands and their format!";
     private Scanner in;
 
     private static final String PRODUCT_LOGO = "███████ ████████  ██████  ███    ██ ██   ██ ███████"
@@ -24,11 +25,28 @@ public class Ui {
             + "██   ██    ██    ██          ██  ██ \n"
             + "██████     ██    ███████        ██ ";
 
-    
-    
-    private static final List<String> commands = Arrays.asList("help", "add_ex", "del_ex",
-            "list_ex", "total_ex", "add_in", "del_in", "list_in", "total_in");
+    private static final String LISTING_EXPENSE_MESSAGE = "Below is a list of all of your recent spending!";
+    private static final String LISTING_INCOME_MESSAGE = "Below is a list of all of your recent earnings!";
 
+    private static final String HELP_FORMAT = "List out all commands: help:)";
+    private static final String ADD_EXPENSE_FORMAT = "Adding Expense: add_ex d/DESCRIPTION a/AMOUNT";
+    private static final String DEL_EXPENSE_FORMAT = "Deleting Expense: del_ex i/INDEX";
+    private static final String LIST_EXPENSE_FORMAT = "Listing Expense: list_ex";
+    private static final String TOTAL_EXPENSE_FORMAT = "Show Total Expense: total_ex";
+    private static final String ADD_INCOME_FORMAT = "Adding Income: add_in d/DESCRIPTION a/AMOUNT";
+    private static final String DEL_INCOME_FORMAT = "Deleting Income: del_in i/INDEX";
+    private static final String LIST_INCOME_FORMAT = "Listing Income: list_in";
+    private static final String TOTAL_INCOME_FORMAT = "Show Total Income: total_in";
+
+
+
+    private static final List<String> commands = Arrays.asList(HELP_FORMAT, ADD_EXPENSE_FORMAT, DEL_EXPENSE_FORMAT,
+            LIST_EXPENSE_FORMAT, TOTAL_EXPENSE_FORMAT, ADD_INCOME_FORMAT, DEL_INCOME_FORMAT, LIST_INCOME_FORMAT,
+            TOTAL_INCOME_FORMAT);
+
+
+    
+    
     public Ui() {
         this.in = new Scanner(System.in);
     }
@@ -52,6 +70,9 @@ public class Ui {
     }
     
     public void listExpense(ArrayList<Entry> entries) {
+        printLine();
+        System.out.println(LISTING_EXPENSE_MESSAGE);
+        printLine();
         int i = 1;
         for (Entry entry:entries) {
             if (entry instanceof Expense) {
@@ -60,10 +81,15 @@ public class Ui {
                 System.out.println(entry);
                 i++;
             }
-        }  
+        }
+        printLine();
+
     }
     
     public void listIncome(ArrayList<Entry> entries) {
+        printLine();
+        System.out.println(LISTING_INCOME_MESSAGE);
+        printLine();
         int i = 1;
         for (Entry entry:entries) {
             if (entry instanceof Income) {
@@ -73,6 +99,8 @@ public class Ui {
                 i++;
             }
         }
+        printLine();
+
     }
     
     public void printTotalExpense(ArrayList<Entry> entries) {
@@ -83,6 +111,8 @@ public class Ui {
             }
         }
         System.out.printf("Your total expense is: %f\n", totalExpense);
+        printLine();
+
     }
     
     public void printTotalIncome(ArrayList<Entry> entries) {
@@ -93,32 +123,50 @@ public class Ui {
             }
         }
         System.out.printf("Your total income is: %f\n", totalIncome);
+        printLine();
+
     }
     
     public void printExpenseAdded(Expense expense) {
+        printLine();
         System.out.println("Your most recent spending: ");
         System.out.println(expense);
+        printLine();
+
     }
 
     public void printExpenseDeleted(Expense expense) {
+        printLine();
         System.out.println("You removed this: ");
         System.out.println(expense);
+        printLine();
+
     }
     
     public void printIncomeAdded(Income income) {
+        printLine();
         System.out.println("Your most recent earning: ");
         System.out.println(income);
+        printLine();
+
     }
 
     public void printIncomeDeleted(Income income) {
+        printLine();
         System.out.println("You removed this: ");
         System.out.println(income);
+        printLine();
+
     }
     
     public void printHelp() {
+        printLine();
+        System.out.println(HELP_COMMAND_MESSAGE);
+        printLine();
         for (String command:commands) {
-            System.out.println(command + ": ");
+            System.out.println(command);
         }
+        printLine();
     }
     
     public void printBye() {
