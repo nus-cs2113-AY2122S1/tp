563fcea78fcf8cff1ad88889d799bf2bbd1a87e5
RemusTeo <remustck@gmail.com>
2021-10-11 23:46:33 +0800

    Improve comment

--- src/test/java/parser/DateParserTest.java
+++ src/test/java/parser/DateParserTest.java
@@ -14,8 +14,8 @@ public class DateParserTest {
     public void stringToDate_validDate_expectValid() {
         try {
             // Depreciated Date Initialisation
-            // Year = actualYear - 1900
-            // Month = 0-11, therefore 8 is Sep
+            // Year = actualYear - 1900 [2021 - 1900 = 121]
+            // Month = 0-11 [8 = Sep]
             Date date = new Date(121, 8, 13); // Mon Sep 13 00:00:00 SGT 2021
             Date parsedDate = DateParser.stringToDate("13-9-2021");
             assertEquals(date, parsedDate);
