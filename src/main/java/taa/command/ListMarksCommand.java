63cbca947f81d2f4d5b21c6ce72c38754cec60df
Leyond <leyondlee@users.noreply.github.com>
2021-10-11 19:42:26 +0800

    Refactor to use StudentList class

--- src/main/java/taa/command/ListMarksCommand.java
+++ src/main/java/taa/command/ListMarksCommand.java
@@ -7,6 +7,7 @@ import taa.exception.TaaException;
 import taa.module.Module;
 import taa.module.ModuleList;
 import taa.student.Student;
+import taa.student.StudentList;
 
 /**
  * Class that deals with the command for the listing of marks.
@@ -18,11 +19,13 @@ public class ListMarksCommand extends Command {
         KEY_MODULE_CODE,
         KEY_ASSESSMENT_NAME
     };
-    private static final String MESSAGE_FORMAT_LIST_MARKS_USAGE = "Usage: %s "
-            + "%s/<MODULE_CODE> %s/<ASSESSMENT_NAME>";
+
     private static final String MESSAGE_LIST_MARKS_HEADER = "Here is the list of students and "
             + "their marks for %s:";
     private static final String MARKS_LIST_EMPTY = "There are no marks inputted!";
+
+    private static final String MESSAGE_FORMAT_LIST_MARKS_USAGE = "Usage: %s "
+            + "%s/<MODULE_CODE> %s/<ASSESSMENT_NAME>";
 
     public ListMarksCommand(String argument) {
         super(argument, ADD_ASSESSMENT_ARGUMENT_KEYS);
@@ -30,6 +33,7 @@ public class ListMarksCommand extends Command {
 
     /**
      * Returns a string with the student name and the marks associated with the student.
+     *
      * @param studentName Name of the student.
      * @param marks Marks of the student.
      * @return String with the name and marks of the student separated with a vertical line.
@@ -60,23 +64,59 @@ public class ListMarksCommand extends Command {
         if (module == null) {
             throw new TaaException(MESSAGE_MODULE_NOT_FOUND);
         }
-        if (module.getNumberOfStudents() <= 0) {
-            //TODO no students in assessment message
-            throw new TaaException("No students to list!");
+
+        StudentList studentList = module.getStudentList();
+        if (studentList.getSize() <= 0) {
+            throw new TaaException(MESSAGE_NO_STUDENTS);
         }
 
-        AssessmentList list = module.getAssessmentList();
-        Assessment assessment = list.getAssessment(argumentMap.get(KEY_ASSESSMENT_NAME));
+        AssessmentList assessmentList = module.getAssessmentList();
+        Assessment assessment = assessmentList.getAssessment(argumentMap.get(KEY_ASSESSMENT_NAME));
         if (assessment == null) {
-            // TODO no such assessment message
-            throw new TaaException("Assessment does not exist!");
+            throw new TaaException(MESSAGE_INVALID_ASSESSMENT_NAME);
         }
 
-        listMarks(ui, module);
+        listMarks(ui, module, assessment);
+    }
+
+    /**
+     * Lists the student and the marks they attained for an assessment.
+     *
+     * @param ui The ui instance to handle interactions with the user.
+     * @param module The module that the student and assessment belong to.
+     */
+    private void listMarks(Ui ui, Module module, Assessment assessment) {
+        StudentList studentList = module.getStudentList();
+        String assessmentName = assessment.getName();
+
+        StringBuilder stringBuilder = new StringBuilder(String.format(MESSAGE_LIST_MARKS_HEADER, assessmentName));
+        int studentIndex = 0;
+        for (Student student : studentList.getStudents()) {
+            studentIndex += 1;
+            if (student.marksExist(assessmentName)) {
+                stringBuilder.append("\n");
+                stringBuilder.append(studentIndex);
+                stringBuilder.append(". ");
+                stringBuilder.append(marksToString(student.getName(), student.getMarks(assessmentName)));
+            } else {
+                stringBuilder.append("\n");
+                stringBuilder.append(studentIndex);
+                stringBuilder.append(". ");
+                stringBuilder.append(student.getName());
+                stringBuilder.append(" | ");
+                stringBuilder.append("NIL");
+            }
+        }
+        if (studentIndex <= 0) {
+            ui.printMessage(MARKS_LIST_EMPTY);
+        } else {
+            ui.printMessage(stringBuilder.toString());
+        }
     }
 
     /**
      * Returns the usage message of the list marks command.
+     *
      * @return String which contains the usage message.
      */
     @Override
@@ -87,38 +127,5 @@ public class ListMarksCommand extends Command {
                 KEY_MODULE_CODE,
                 KEY_ASSESSMENT_NAME
         );
-    }
-
-    /**
-     * Lists the student and the marks they attained for an assessment.
-     *
-     * @param ui The ui instance to handle interactions with the user.
-     * @param module The module that the student and assessment belong to.
-     */
-    private void listMarks(Ui ui, Module module) {
-        String assessmentName = argumentMap.get("a");
-        StringBuilder marksList = new StringBuilder(String.format(MESSAGE_LIST_MARKS_HEADER, assessmentName));
-        int studentIndex = 0;
-        for (Student student : module.getStudents()) {
-            studentIndex += 1;
-            if (student.marksExist(assessmentName)) {
-                marksList.append("\n");
-                marksList.append(studentIndex);
-                marksList.append(". ");
-                marksList.append(marksToString(student.getName(), student.getMarks(assessmentName)));
-            } else {
-                marksList.append("\n");
-                marksList.append(studentIndex);
-                marksList.append(". ");
-                marksList.append(student.getName());
-                marksList.append(" | ");
-                marksList.append("Marks not added.");
-            }
-        }
-        if (studentIndex <= 0) {
-            ui.printMessage(MARKS_LIST_EMPTY);
-        } else {
-            ui.printMessage(marksList.toString());
-        }
     }
 }
