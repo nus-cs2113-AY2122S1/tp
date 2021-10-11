d7eb744bca99daf77957ecb6baf85320649a9cf9
AnShengLee <anshengl@yahoo.com.sg>
2021-10-11 15:54:17 +0800

    Fix wrong assertion in Main

--- src/main/java/seedu/duke/StonksXD.java
+++ src/main/java/seedu/duke/StonksXD.java
@@ -1,6 +1,7 @@
 package seedu.duke;
 
 import seedu.commands.Command;
+import seedu.commands.ExitCommand;
 import seedu.commands.InvalidCommand;
 import seedu.utility.FinancialTracker;
 import seedu.utility.Ui;
@@ -25,7 +26,7 @@ public class StonksXD {
             Command command = parser.parseCommand(fullCommand);
             command.execute(finances,ui);
             if (command.isExit()) {
-                assert command.getClass() == InvalidCommand.class;
+                assert command.getClass() == ExitCommand.class;
                 exitFlag = false;
             }
         }
