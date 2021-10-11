3d19eb18cae8672f346273689a03d10354d6dd1a
Ng Andre <ng.andr3@gmail.com>
2021-10-11 10:13:51 +0800

    Add new fields for contact with getter and setter methods

--- src/main/java/seedu/contact/Contact.java
+++ src/main/java/seedu/contact/Contact.java
@@ -3,6 +3,10 @@ package seedu.contact;
 public class Contact {
     private String name;
     private String github;
+    private String email;
+    private String linkedin;
+    private String twitter;
+    private String telegram;
 
     public Contact(String name, String github) {
         this.name = name;
@@ -23,5 +27,37 @@ public class Contact {
 
     public void setGithub(String github) {
         this.github = github;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    public String getTelegram() {
+        return telegram;
+    }
+
+    public void setTelegram(String telegram) {
+        this.telegram = telegram;
     }
 }
