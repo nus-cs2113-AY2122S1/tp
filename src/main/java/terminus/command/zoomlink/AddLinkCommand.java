8f6a589c7a21310a6c60692f2fb43c031aa22492
LouisLouis19 <e0661385@u.nus.edu>
2021-10-11 09:01:20 +0800

    Add JavaDoc for AddLinkCommand class

--- src/main/java/terminus/command/zoomlink/AddLinkCommand.java
+++ src/main/java/terminus/command/zoomlink/AddLinkCommand.java
@@ -27,16 +27,34 @@ public class AddLinkCommand extends Command {
 
     }
 
+    /**
+     * Returns the command format to Add a Link
+     *
+     * @return The string containing the command format to add a link
+     */
     @Override
     public String getFormat() {
         return CommonFormat.COMMAND_ADD_SCHEDULE_FORMAT;
     }
 
+    /**
+     * Returns the description of Add Link Command
+     *
+     * @return The string containing the description of an add command
+     */
     @Override
     public String getHelpMessage() {
         return Messages.MESSAGE_COMMAND_ADD;
     }
 
+    /**
+     * Parses the arguments in an add link command
+     * to its respective description, day, start-time, and link
+     *
+     * @param arguments The string arguments to be parsed in to the respective fields.
+     * @throws InvalidArgumentException Exception for when argument parsing fails
+     * @throws InvalidTimeFormatException Exception for when time format is invalid
+     */
     @Override
     public void parseArguments(String arguments) throws InvalidArgumentException, InvalidTimeFormatException {
         // Perform required checks with regex
@@ -55,6 +73,16 @@ public class AddLinkCommand extends Command {
         this.link = argArray.get(3);
     }
 
+    /**
+     * Executes the add link command
+     * Prints the relevant response to the Ui
+     *
+     * @param ui     The Ui object to send messages to the users.
+     * @param module The NusModule contain the list of all notes and schedules.
+     * @return CommandResult to indicate the success and additional information about the execution
+     * @throws InvalidCommandException Exception for when the user command is not found
+     * @throws InvalidArgumentException Exception for when the argument parsing fails
+     */
     @Override
     public CommandResult execute(Ui ui, NusModule module) throws InvalidCommandException, InvalidArgumentException {
         ContentManager contentManager = module.getContentManager();
@@ -65,6 +93,12 @@ public class AddLinkCommand extends Command {
         return new CommandResult(true, false);
     }
 
+    /**
+     * Checks if arguments are non-empty and valid
+     *
+     * @param argArray The command arguments in an array list
+     * @return True if the appropriate number of arguments are present, false otherwise.
+     */
     private boolean isValidScheduleArguments(ArrayList<String> argArray) {
         boolean isValid = true;
         if (argArray.size() != ADD_SCHEDULE_ARGUMENTS) {
