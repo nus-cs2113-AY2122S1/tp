68eb7d4f494338205aa5809342cdf0a60451664d
LouisLouis19 <e0661385@u.nus.edu>
2021-10-12 21:08:31 +0800

    Add JUnit test for view link command

--- src/test/java/terminus/command/link/DeleteLinkCommandTest.java
+++ src/test/java/terminus/command/link/DeleteLinkCommandTest.java
@@ -31,7 +31,7 @@ public class DeleteLinkCommandTest {
     }
 
     @Test
-    void execute_success() throws InvalidCommandException, InvalidArgumentException {
+    void execute_deleteLink_success() throws InvalidCommandException, InvalidArgumentException {
         for (int i = 0; i < 3; i++) {
             Command addLinkCommand = linkCommandParser.parseCommand("add \"test_desc\" \"Monday\" \"12:00\" \"https://zoom.us/test\"");
             CommandResult addResult = addLinkCommand.execute(ui, nusModule);
