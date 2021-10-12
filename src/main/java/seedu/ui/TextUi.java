30d5d62895717805ce4332a20eca39ab404b9356
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2021-10-12 20:44:06 +0800

    Add method for invalid github username message

--- src/main/java/seedu/ui/TextUi.java
+++ src/main/java/seedu/ui/TextUi.java
@@ -150,6 +150,11 @@ public class TextUi {
         printDoubleLineMessage(message);
     }
 
+    public static void invalidGithubUsernameInput() {
+        String message = "The github username is not correctly formatted";
+        printDoubleLineMessage(message);
+    }
+
     public static void missingNameMessage() {
         String message = "There are missing details.\n"
                 + "Please specify a name when creating a contact with the flag -n";
