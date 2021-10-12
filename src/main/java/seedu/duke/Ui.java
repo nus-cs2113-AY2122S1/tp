d7c9da829ce9829a49cd4441613bc0003160470c
Joshua Lee <joshualeeky@u.nus.edu>
2021-10-12 17:44:30 +0800

    Resolved test errors

--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -33,9 +33,12 @@ public class Ui {
 
     public static void printExpensesSummary(Trip t) {
         System.out.println("This is the summary for your " + t.getLocation() + " trip " + t.getDateOfTripString());
-        System.out.println("Total budget for this trip: " + t.getBudget());
-        System.out.println("Total expenditure so far: " + t.getTotalExpenses());
-        System.out.println("Current budget left for this trip: " + t.getBudgetLeft());
+        System.out.println("Total budget for this trip: ");
+        printMoney(t.getBudget());
+        System.out.println("Total expenditure so far: ");
+        printMoney(t.getTotalExpenses());
+        System.out.println("Current budget left for this trip: ");
+        printMoney(t.getBudgetLeft());
     }
 
     public static void printExpenseAddedSuccess() {
