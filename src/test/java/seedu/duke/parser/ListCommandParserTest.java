c4e4a8dff771d27655dd15eb04a91d0d38638d2a
MAZJ124 <yuangazi@hotmail.com>
2021-10-13 02:04:31 +0800

    Update JUnit testing after change in command class

--- src/test/java/seedu/duke/parser/ListCommandParserTest.java
+++ src/test/java/seedu/duke/parser/ListCommandParserTest.java
@@ -3,7 +3,10 @@ package seedu.duke.parser;
 import org.junit.jupiter.api.Test;
 import seedu.duke.commands.ListCommand;
 import seedu.duke.modules.Module;
+import seedu.duke.modules.ModuleList;
+import seedu.duke.modules.ModuleMapping;
 import seedu.duke.storage.Storage;
+import seedu.duke.universities.University;
 import seedu.duke.universities.UniversityList;
 
 import java.io.IOException;
@@ -15,6 +18,9 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ListCommandParserTest {
 
+    private static UniversityList universitySelectedList = new UniversityList();
+    private static ModuleList moduleSelectedList = new ModuleList();
+
     @Test
     public void test_moduleMasterListFlag_success() throws IOException, ParseException {
         String flag = "/m";
@@ -24,7 +30,7 @@ public class ListCommandParserTest {
         for (int i = 0; i < moduleMasterList.size(); i++) {
             message += "[" + (i + 1) + "] " + moduleMasterList.get(i).getModuleCode() + System.lineSeparator();
         }
-        assertEquals(message, lcp.parse(flag));
+        assertEquals(message, lcp.parse(flag, universitySelectedList, moduleSelectedList));
     }
 
     @Test
@@ -36,28 +42,30 @@ public class ListCommandParserTest {
         for (int i = 0; i < universityMasterList.getSize(); i++) {
             message += "[" + (i + 1) + "] " + universityMasterList.get(i).getName() + System.lineSeparator();
         }
-        assertEquals(message, lcp.parse(flag));
+        assertEquals(message, lcp.parse(flag, universitySelectedList, moduleSelectedList));
     }
 
     @Test
-    public void test_selectedModuleListFlag_success() {
+    public void test_selectedModuleListFlag_success() throws ParseException, IOException {
         String flag = "/t";
+        String emptyListMessage = "The module list is empty!";
         ListCommandParser lcp = new ListCommandParser();
-
+        assertEquals(emptyListMessage, lcp.parse(flag, universitySelectedList, moduleSelectedList));
     }
 
     @Test
-    public void test_selectedUniversityListFlag_success() {
+    public void test_selectedUniversityListFlag_success() throws ParseException, IOException {
         String flag = "/s";
+        String emptyListMessage = "The university list is empty!";
         ListCommandParser lcp = new ListCommandParser();
-
+        assertEquals(emptyListMessage, lcp.parse(flag, universitySelectedList, moduleSelectedList));
     }
 
     @Test
     public void test_emptyFlag_exceptionThrown() {
         String flag = "";
         ListCommandParser lcp = new ListCommandParser();
-        assertThrows(ParseException.class, () -> lcp.parse(flag));
+        assertThrows(ParseException.class, () -> lcp.parse(flag, universitySelectedList, moduleSelectedList));
     }
 
 }
