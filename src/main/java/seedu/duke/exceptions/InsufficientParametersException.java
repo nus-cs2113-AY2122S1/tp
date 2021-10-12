2815a0d05f3dec06507dded5cf0f1a56e13fc56b
mudkip8 <mahvin1970@hotmail.com>
2021-10-12 10:21:23 +0800

    Removed Extra Classes

--- src/main/java/seedu/duke/exceptions/InsufficientParametersException.java
+++
@@ -1,9 +0,0 @@
-package seedu.duke.exceptions;
-
-public class InsufficientParametersException extends Exception {
-
-    @Override
-    public String getMessage() {
-        return "Insufficient parameters entered. Please try again";
-    }
-}
