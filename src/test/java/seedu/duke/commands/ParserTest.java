86e129d1f6e5bf5489f1dcc325d2f6ac5794379b
exetr <exetrtck@gmail.com>
2021-10-12 21:17:38 +0800

    Update JUnit tests for Parser class

--- src/test/java/seedu/duke/commands/ParserTest.java
+++ src/test/java/seedu/duke/commands/ParserTest.java
@@ -5,15 +5,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ParserTest {
+    Parser parser = new Parser();
     @Test
     public void parse_exit_ExitCommandObject() {
-        boolean type = Parser.parse("exit") instanceof ExitCommand;
+        boolean type = parser.parse("exit") instanceof ExitCommand;
         assertTrue(type);
     }
 
     @Test
     public void parse_unknown_UnknownCommandObject() {
-        boolean type = Parser.parse("foo 2") instanceof UnknownCommand;
+        boolean type = parser.parse("foo 2") instanceof UnknownCommand;
         assertTrue(type);
     }
 }
