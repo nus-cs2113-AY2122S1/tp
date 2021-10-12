3e56416390cba8c750f788ade9236b1c613005f9
WDOneM <58932649+maxwireddevs@users.noreply.github.com>
2021-10-12 18:03:04 +0800

    Wiki import and test

--- src/main/java/seedu/typists/Main.java
+++ src/main/java/seedu/typists/Main.java
@@ -38,8 +38,6 @@ public class Main {
         WikiImport wiki = new WikiImport();
         try {
             String article = wiki.getArticle("GitHub");
-            System.out.println(article.length());
-            System.out.println(article);
         } catch (InvalidArticleException e) {
             e.printStackTrace();
         }
