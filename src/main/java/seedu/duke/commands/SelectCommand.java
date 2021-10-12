7c6ba3c74b07bbf85d6ab69b4e2defc0e73a7a7a
alwinangys <alwinang99@gmail.com>
2021-10-12 10:24:42 +0800

    Add initial SelectCommand class

--- src/main/java/seedu/duke/commands/SelectCommand.java
+++ src/main/java/seedu/duke/commands/SelectCommand.java
@@ -1,4 +1,51 @@
 package seedu.duke.commands;
 
-public class SelectCommand {
+
+// meant to be used after FindCommand, where the user can view the details of a specific item
+// this class assumes that there were no exceptions thrown while using FindCommand and that at least one item
+// was found in FindCommand
+
+//                        filteredItemList
+// FindCommand(keyword) --------------------> SelectCommand(index)
+
+import seedu.duke.Ui;
+import seedu.duke.items.Event;
+import seedu.duke.items.Item;
+import seedu.duke.items.Task;
+
+
+public class SelectCommand extends Command{
+
+    protected int itemIndex;
+
+    // eg select 1
+    public SelectCommand(String[] command) {
+        itemIndex = Integer.parseInt(command[1]);
+    }
+
+    public CommandResult execute() {
+        Item selectedItem = FindCommand.filteredItemList.get(itemIndex);
+        if (isEvent(selectedItem)) {
+            Ui.printLineBreak();
+            System.out.println("Here are the details of the event:");
+            Ui.printEvent((Event) selectedItem);
+            return new CommandResult(Ui.getLineBreak());
+        } else if (isTask(selectedItem)) {
+            Ui.printLineBreak();
+            System.out.println("Here are the details of the task:");
+            Ui.printTask((Task) selectedItem);
+            return new CommandResult(Ui.getLineBreak());
+        }
+        return new CommandResult("I can't select the item you want!");
+    }
+
+    private boolean isEvent(Item selectedItem) {
+        String type = selectedItem.getItemType();
+        return type.equalsIgnoreCase("event");
+    }
+
+    private boolean isTask(Item selectedItem) {
+        String type = selectedItem.getItemType();
+        return type.equalsIgnoreCase("task");
+    }
 }
