19512fec3fe41161f4f161ecb78f105a0580ba5a
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2021-10-12 20:45:12 +0800

    Add method for invalid linkedin username message

--- src/main/java/seedu/ui/TextUi.java
+++ src/main/java/seedu/ui/TextUi.java
@@ -170,6 +170,11 @@ public class TextUi {
         printDoubleLineMessage(message);
     }
 
+    public static void invalidLinkedinInput() {
+        String message = "The linkedin username is not correctly formatted";
+        printDoubleLineMessage(message);
+    }
+
     public static void missingNameMessage() {
         String message = "There are missing details.\n"
                 + "Please specify a name when creating a contact with the flag -n";
