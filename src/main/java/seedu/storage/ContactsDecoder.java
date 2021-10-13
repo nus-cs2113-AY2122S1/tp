7d2f9dbbe3266e039dc49af6fc2de0acbacc8cf4
lezong <lezongmun@gmail.com>
2021-10-13 14:23:13 +0800

    Add assertions

--- src/main/java/seedu/storage/ContactsDecoder.java
+++ src/main/java/seedu/storage/ContactsDecoder.java
@@ -30,9 +30,6 @@ public class ContactsDecoder {
 
     private static void decodeContact(ContactList contactList, String contactText) {
         String[] destructuredInputs = contactText.split(SEPARATOR);
-        String[] compiledDetails = new String[NUMBER_OF_FIELDS];
-        assert destructuredInputs.length > 0;
-        decodeDetails(compiledDetails, destructuredInputs);
         // Add the decoded details into the contact list
         try {
             String contactName = destructuredInputs[DetailType.NAME.getIndex()];
@@ -46,16 +43,6 @@ public class ContactsDecoder {
             contactList.addContact(newContact);
         } catch (IndexOutOfBoundsException e) {
             ExceptionTextUi.corruptLineMessage(contactText);
-        }
-    }
-
-    private static void decodeDetails(String[] compiledDetails, String[] destructuredInputs) {
-        if (compiledDetails.length != destructuredInputs.length) {
-            return;
-        }
-        assert compiledDetails.length == NUMBER_OF_FIELDS;
-        for (int i = 0; i < NUMBER_OF_FIELDS; i++) {
-            compiledDetails[i] = destructuredInputs[i];
         }
     }
 }
