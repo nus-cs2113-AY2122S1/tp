962578431e7aabbb5ccdf6dfe9fe6de76ff9f60f
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-10-13 13:41:21 +0800

    Validate that quantity is not a negative number when parsing

--- src/main/java/expiryeliminator/parser/argparser/QuantityParser.java
+++ src/main/java/expiryeliminator/parser/argparser/QuantityParser.java
@@ -4,12 +4,17 @@ import expiryeliminator.parser.exception.InvalidArgFormatException;
 
 public class QuantityParser extends MultipleArgParser<Integer> {
     private static final String MESSAGE_INVALID_NUMBER = "Quantity must be a valid number";
+    private static final String MESSAGE_NEGATIVE_NUMBER = "Quantity cannot be a negative number.";
 
     @Override
     public Integer parse(String quantityString) throws InvalidArgFormatException {
         checkArgNotBlank(quantityString, "Quantity cannot be blank.");
         try {
-            return Integer.parseInt(quantityString);
+            final int quantity = Integer.parseInt(quantityString);
+            if (quantity < 0) {
+                throw new InvalidArgFormatException(MESSAGE_NEGATIVE_NUMBER);
+            }
+            return quantity;
         } catch (NumberFormatException e) {
             throw new InvalidArgFormatException(MESSAGE_INVALID_NUMBER);
         }
