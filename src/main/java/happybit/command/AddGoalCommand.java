10015acab1d922cdfb83db5e87d89d80d85773a8
Daren Tan <daren9901@gmail.com>
2021-10-13 10:18:19 +0800

    Add Increment: AddCommand Class
    Implemented a command class to add goals and habits.

--- src/main/java/happybit/command/AddGoalCommand.java
+++ src/main/java/happybit/command/AddGoalCommand.java
@@ -1,14 +1,22 @@
 package happybit.command;
 
 import happybit.exception.HBCommandException;
+import happybit.goal.Goal;
 import happybit.goal.GoalList;
 import happybit.storage.Storage;
 import happybit.ui.Ui;
 
-public class AddCommand extends Command {
+public class AddGoalCommand extends AddCommand {
+
+    protected Goal goal;
+
+    public AddGoalCommand(Goal goal) {
+        this.goal = goal;
+    }
 
     @Override
     public void runCommand(GoalList goalList, Ui ui, Storage storage) throws HBCommandException {
-        // do nothing
+        goalList.addGoal(goal);
     }
+
 }
