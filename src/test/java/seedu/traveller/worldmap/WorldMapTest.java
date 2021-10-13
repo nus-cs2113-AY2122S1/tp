216d3dd40feaee86214e2a008ed030d860a11026
JaCh23 <jach2355@gmail.com>
2021-10-14 02:43:12 +0800

    gradle syntax

--- src/test/java/seedu/traveller/worldmap/WorldMapTest.java
+++ src/test/java/seedu/traveller/worldmap/WorldMapTest.java
@@ -37,18 +37,17 @@ public class WorldMapTest {
     }
 
     @Test
-    public void calcMinDistance_sinLax_success(){
+    public void calcMinDistance_sinLax_success() {
         Country countrySin = new Country("SIN", 0);
         Country countryMly = new Country("MLY", 1);
         Country countryJpn = new Country("JPN", 3);
 
         List<Country> path = new ArrayList<>();
-        List<Double> distances = new ArrayList<>();
-
         path.add(countrySin);
         path.add(countryMly);
         path.add(countryJpn);
 
+        List<Double> distances = new ArrayList<>();
         distances.add(1.0);
         distances.add(6.0);
 
@@ -58,53 +57,56 @@ public class WorldMapTest {
                 WorldMap.calcMinDistance("SIN", "JPN").toString());
     }
 
-//    @Test
-//    public void calcMinDistance_laxSin_success() {
-//        Country countrySin = new Country("SIN", 0);
-//        Country countryMly = new Country("MLY", 1);
-//        Country countryJpn = new Country("JPN", 3);
-//
-//        List<Country> path = new ArrayList<>();
-//        List<Double> distances = new ArrayList<>();
-//
-//        path.add(countryJpn);
-//        path.add(countryMly);
-//        path.add(countrySin);
-//
-//        distances.add(6.0);
-//        distances.add(1.0);
-//
-//        MinCalcResult minCalcResult = new MinCalcResult(countrySin, countryJpn, path, distances);
-//
-//        assertEquals(minCalcResult.toString(),
-//                WorldMap.calcMinDistance("JPN", "SIN").toString());
-//
-//    }
+    /*
+    @Test
+    public void calcMinDistance_laxSin_success() {
+        Country countrySin = new Country("SIN", 0);
+        Country countryMly = new Country("MLY", 1);
+        Country countryJpn = new Country("JPN", 3);
 
-//    @Test
-//    public void updateMinDistance_sinLax_success() {
-//        Country countrySin = new Country("SIN", 0);
-//        Country countryMly = new Country("MLY", 1);
-//        Country countryJpn = new Country("JPN", 3);
-//
-//        List<Country> path = new ArrayList<>();
-//        List<Double> distances = new ArrayList<>();
-//
-//        path.add(countryJpn);
-//        path.add(countryMly);
-//        path.add(countrySin);
-//
-//        distances.add(6.0);
-//        distances.add(1.0);
-//
-//        Country.updateNeighbour(999.0, countrySin, countryJpn);
-//
-//        MinCalcResult minCalcResult = new MinCalcResult(countrySin, countryJpn, path, distances);
-//
-//        assertEquals(minCalcResult.toString(),
-//                WorldMap.calcMinDistance("JPN", "SIN").toString());
-//
-//    }
+        List<Country> path = new ArrayList<>();
+        List<Double> distances = new ArrayList<>();
 
-    
+        path.add(countryJpn);
+        path.add(countryMly);
+        path.add(countrySin);
+
+        distances.add(6.0);
+        distances.add(1.0);
+
+        MinCalcResult minCalcResult = new MinCalcResult(countrySin, countryJpn, path, distances);
+
+        assertEquals(minCalcResult.toString(),
+                WorldMap.calcMinDistance("JPN", "SIN").toString());
+
+    }
+
+    @Test
+    public void updateMinDistance_sinLax_success() {
+        Country countrySin = new Country("SIN", 0);
+        Country countryMly = new Country("MLY", 1);
+        Country countryJpn = new Country("JPN", 3);
+
+        List<Country> path = new ArrayList<>();
+        List<Double> distances = new ArrayList<>();
+
+        path.add(countryJpn);
+        path.add(countryMly);
+        path.add(countrySin);
+
+        distances.add(6.0);
+        distances.add(1.0);
+
+        Country.updateNeighbour(999.0, countrySin, countryJpn);
+
+        MinCalcResult minCalcResult = new MinCalcResult(countrySin, countryJpn, path, distances);
+
+        assertEquals(minCalcResult.toString(),
+        WorldMap.calcMinDistance("JPN", "SIN").toString());
+
+      }
+      */
+
+
+
 }
