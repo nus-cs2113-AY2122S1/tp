e8fd5daa21ab18cf263b5fa56ee118d5e07b883f
Jon-the-MELON <limyongkiat@yahoo.com.sg>
2021-10-13 19:58:08 +0800

    added assert and junit testing for student marks

--- src/test/java/taa/StudentTest.java
+++ src/test/java/taa/StudentTest.java
@@ -1,5 +1,7 @@
 package taa;
 
+import taa.assessment.Assessment;
+import taa.assessment.AssessmentList;
 import taa.student.Student;
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +12,13 @@ public class StudentTest {
     public void testStringConversion() {
         assertEquals("A1234567B (Jon Lim)", new Student("A1234567B", "Jon Lim").toString());
     }
-    
+
+    @Test
+    void setMarks_newMarksArguments_marksAddedCorrectly() {
+        Student student = new Student("A0123456X", "Jim");
+        AssessmentList assessments = new AssessmentList();
+        assessments.addAssessment(new Assessment("Midterms", 20));
+        student.setMarks("Midterms", 99.9);
+        assertEquals(99.9, student.getMarks("Midterms"));
+    }
 }
