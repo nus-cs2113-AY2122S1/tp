ed26d7e46a590f5396c739a234e89a63848350a5
Daren Tan <daren9901@gmail.com>
2021-10-13 21:17:31 +0800

    Add Increment: (Command)Parser Class
    Implemented the Parser class skeleton for the different types of commands.

---
+++ src/main/java/happybit/parser/DoneParser.java
@@ -0,0 +1,14 @@
+package happybit.parser;
+
+import happybit.command.Command;
+import happybit.exception.HaBitParserException;
+
+public class DoneParser {
+
+    public static Command parseDoneHabitCommand(String commandInstruction) throws HaBitParserException  {
+        // write parse code
+        // command yet to be defined
+        return null;
+    }
+
+}
---
+++ src/main/java/happybit/parser/ListParser.java
@@ -0,0 +1,16 @@
+package happybit.parser;
+
+import happybit.command.Command;
+import happybit.command.ListHabitsCommand;
+import happybit.exception.HaBitParserException;
+import happybit.goal.GoalList;
+
+public class ListParser {
+
+    public static Command parseListHabitCommand(String commandInstruction) throws HaBitParserException {
+        // write parse code
+        // return new ListHabitsCommand(<ADD PARAMS>);
+        return null;
+    }
+
+}
