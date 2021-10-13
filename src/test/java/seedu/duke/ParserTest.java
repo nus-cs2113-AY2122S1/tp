7760f31c12c556789c54e8a1b39e25e2dd23c8da
swongts <70316271+swongts@users.noreply.github.com>
2021-10-13 19:30:14 +0800

    Add JUnit test for parser

--- src/test/java/seedu/duke/ParserTest.java
+++ src/test/java/seedu/duke/ParserTest.java
@@ -2,16 +2,45 @@ package seedu.duke;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 public class ParserTest {
+
     @Test
-    void parse() {
+    void parse_addCommand_correctCommandCreated() throws TourPlannerException{
+        parseAndAssertCommandType("add botuan /cn 9487 /f hgus /a hotel-one /t aus-a", AddCommand.class);
+
     }
 
     @Test
-    void splitCommandString() {
+    void parse_clearCommand_correctCommandCreated() throws TourPlannerException{
+        parseAndAssertCommandType("clear", ClearCommand.class);
+
     }
 
     @Test
-    void parseCut() {
+    void parse_listCommand_correctCommandCreated() throws TourPlannerException{
+        parseAndAssertCommandType("list", ListCommand.class);
+
     }
+
+    @Test
+    void parse_cutCommand_correctCommandCreated() throws TourPlannerException{
+        parseAndAssertCommandType("cut 0", CutCommand.class);
+
+    }
+
+    @Test
+    void parse_byeCommand_correctCommandCreated() throws TourPlannerException{
+        parseAndAssertCommandType("bye", ByeCommand.class);
+
+    }
+
+    private <T extends Command> T parseAndAssertCommandType(String input, Class<T> expectedCommandClass) throws TourPlannerException {
+        final Command result = Parser.parse(input);
+        assertTrue(result.getClass().isAssignableFrom(expectedCommandClass));
+        return (T) result;
+    }
+
 }
