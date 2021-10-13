c4af18ff487c33d687c019f2db0cfbd5703f7a5b
JaCh23 <jach2355@gmail.com>
2021-10-14 02:34:56 +0800

    modifying input data, creating test cases

--- src/test/java/seedu/traveller/worldmap/WorldMapTest.java
+++ src/test/java/seedu/traveller/worldmap/WorldMapTest.java
@@ -35,4 +35,76 @@ public class WorldMapTest {
         Country countrySin = WorldMap.getCountry("SIN");
         assertEquals(new Country("SIN", 0).toString(), countrySin.toString());
     }
+
+    @Test
+    public void calcMinDistance_sinLax_success(){
+        Country countrySin = new Country("SIN", 0);
+        Country countryMly = new Country("MLY", 1);
+        Country countryJpn = new Country("JPN", 3);
+
+        List<Country> path = new ArrayList<>();
+        List<Double> distances = new ArrayList<>();
+
+        path.add(countrySin);
+        path.add(countryMly);
+        path.add(countryJpn);
+
+        distances.add(1.0);
+        distances.add(6.0);
+
+        MinCalcResult minCalcResult = new MinCalcResult(countrySin, countryJpn, path, distances);
+
+        assertEquals(minCalcResult.toString(),
+                WorldMap.calcMinDistance("SIN", "JPN").toString());
+    }
+
+//    @Test
+//    public void calcMinDistance_laxSin_success() {
+//        Country countrySin = new Country("SIN", 0);
+//        Country countryMly = new Country("MLY", 1);
+//        Country countryJpn = new Country("JPN", 3);
+//
+//        List<Country> path = new ArrayList<>();
+//        List<Double> distances = new ArrayList<>();
+//
+//        path.add(countryJpn);
+//        path.add(countryMly);
+//        path.add(countrySin);
+//
+//        distances.add(6.0);
+//        distances.add(1.0);
+//
+//        MinCalcResult minCalcResult = new MinCalcResult(countrySin, countryJpn, path, distances);
+//
+//        assertEquals(minCalcResult.toString(),
+//                WorldMap.calcMinDistance("JPN", "SIN").toString());
+//
+//    }
+
+//    @Test
+//    public void updateMinDistance_sinLax_success() {
+//        Country countrySin = new Country("SIN", 0);
+//        Country countryMly = new Country("MLY", 1);
+//        Country countryJpn = new Country("JPN", 3);
+//
+//        List<Country> path = new ArrayList<>();
+//        List<Double> distances = new ArrayList<>();
+//
+//        path.add(countryJpn);
+//        path.add(countryMly);
+//        path.add(countrySin);
+//
+//        distances.add(6.0);
+//        distances.add(1.0);
+//
+//        Country.updateNeighbour(999.0, countrySin, countryJpn);
+//
+//        MinCalcResult minCalcResult = new MinCalcResult(countrySin, countryJpn, path, distances);
+//
+//        assertEquals(minCalcResult.toString(),
+//                WorldMap.calcMinDistance("JPN", "SIN").toString());
+//
+//    }
+
+    
 }
