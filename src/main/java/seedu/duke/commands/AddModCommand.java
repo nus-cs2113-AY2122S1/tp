b49eda1e54ae2a05168f47ed11c1950a7286e17b
Jiale-Sun <e0638853@u.nus.edu>
2021-10-13 17:59:41 +0800

    commit before pulling

--- src/main/java/seedu/duke/commands/AddModCommand.java
+++ src/main/java/seedu/duke/commands/AddModCommand.java
@@ -3,14 +3,19 @@ package seedu.duke.commands;
 import seedu.duke.modules.Module;
 import seedu.duke.modules.ModuleList;
 import seedu.duke.universities.UniversityList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AddModCommand extends Command {
     public static final String COMMAND_WORD = "addmod";
+
+    private Logger logger = Logger.getLogger("AddModCommandLog");
 
     private final Module moduleToAdd;
 
     public AddModCommand(Module moduleToAdd, UniversityList universitySelectedList, ModuleList moduleSelectedList) {
         super(universitySelectedList, moduleSelectedList);
+        logger.log(Level.INFO, "Start parsing addmod command");
         this.moduleToAdd = moduleToAdd;
         moduleSelectedList.addModule(moduleToAdd);
         System.out.println("New module added: " + moduleToAdd.getModuleCode());
