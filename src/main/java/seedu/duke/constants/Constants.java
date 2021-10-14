95b5bcd85c1f953ac7f23892010505848c30f77a
titustortoiseturtle1999 <ngtitus@ymail.com>
2021-10-14 23:56:32 +0800

    add ui and constants package for print uni & mod

---
+++ src/main/java/seedu/duke/constants/Constants.java
@@ -0,0 +1,8 @@
+package seedu.duke.constants;
+
+public class Constants {
+    // Ui
+    public static final String INDEX_WRAP_FRONT = "[";
+    public static final String INDEX_WRAP_BACK = "]";
+
+}
---
+++ src/main/java/seedu/duke/ui/Ui.java
@@ -0,0 +1,23 @@
+package seedu.duke.ui;
+import seedu.duke.modules.Module;
+import seedu.duke.universities.University;
+import seedu.duke.constants.Constants;
+
+public class Ui {
+    public static void printModule(Module mod, int index ) {
+        printIndex(index, false);
+        System.out.println(" " + mod.getModuleCode());
+    }
+    public static void printUniversity(University uni, int index) {
+        printIndex(index, false);
+        System.out.println(" " + uni.getName());
+    }
+    public static void printIndex(int index, boolean println) {
+        String format = Constants.INDEX_WRAP_FRONT + index + Constants.INDEX_WRAP_BACK;
+        if (println) {
+            System.out.println(format);
+        } else {
+            System.out.print(format);
+        }
+    }
+}
