84a039ba4b1139010079e428c5c875c5c6fffd4c
kahhe <69589263+kahhe@users.noreply.github.com>
2021-10-14 22:30:47 +0800

    Add Increment: assertions

--- src/main/java/happybit/parser/DeleteParser.java
+++ src/main/java/happybit/parser/DeleteParser.java
@@ -10,6 +10,7 @@ public class DeleteParser {
     private static final String ERROR_BLANK = "No instruction given.";
     private static final String ERROR_NOT_NUM = "Expected a number.";
     private static final String ERROR_HABIT_NOT_EXIST = "Habit index does not exist.";
+    private static final String ERROR_INCOMPLETE = "Instruction incomplete or improper.";
 
     public static Command parseDeleteGoalCommand(String commandInstruction) throws HaBitParserException  {
         if (commandInstruction == null) {
@@ -19,6 +20,7 @@ public class DeleteParser {
         try {
             int goalIndex;
             goalIndex = Integer.parseInt(commandInstruction) - 1;
+            assert (goalIndex > 0) : "Index of goal should be 1 or higher";
             return new DeleteGoalCommand(goalIndex);
 
             //todo: if goal is a name
@@ -38,8 +40,13 @@ public class DeleteParser {
             int habitIndex;
             String[] numbers = commandInstruction.split(" ");
 
+            if (numbers.length != 2) {
+                throw new HaBitParserException(ERROR_INCOMPLETE);
+            }
             goalIndex = Integer.parseInt(numbers[0]) - 1;
             habitIndex = Integer.parseInt(numbers[1]) - 1;
+            assert (goalIndex > 0) : "Index of goal should be 1 or higher";
+            assert (habitIndex > 0) : "Index of habit should be 1 or higher";
             return new DeleteHabitCommand(goalIndex, habitIndex);
 
         } catch (NumberFormatException e) {
