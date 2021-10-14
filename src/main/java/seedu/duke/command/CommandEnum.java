c204b62f3ed03c8aa545cbde034587dadcc3d2d6
SeanRobertDH <seandh1998@gmail.com>
2021-10-15 00:07:22 +0800

    Add an enum class to store all valid commands

---
+++ src/main/java/seedu/duke/command/CommandEnum.java
@@ -0,0 +1,25 @@
+package seedu.duke.command;
+
+import seedu.duke.exception.InvalidRecurrenceException;
+import seedu.duke.task.RecurrenceEnum;
+
+public enum CommandEnum {
+    BYE, HELP, LIST, TODO, DEADLINE, EVENT, INVALID;
+
+    @Override
+    public String toString() {
+        return super.toString().toLowerCase();
+    }
+
+    public static CommandEnum getCommand(String command) {
+        if (command == null) {
+            return CommandEnum.INVALID;
+        }
+        for (CommandEnum commandEnum : values()) {
+            if (command.equalsIgnoreCase(commandEnum.name())) {
+                return commandEnum;
+            }
+        }
+        return CommandEnum.INVALID;
+    }
+}
