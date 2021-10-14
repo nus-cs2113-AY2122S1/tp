3e80d4c05f41d7744703fecf9411b2e2b0f226f3
titustortoiseturtle1999 <ngtitus@ymail.com>
2021-10-14 23:59:33 +0800

    fix checkstyle errors in Ui

--- src/main/java/seedu/duke/ui/Ui.java
+++ src/main/java/seedu/duke/ui/Ui.java
@@ -1,17 +1,21 @@
 package seedu.duke.ui;
+
 import seedu.duke.modules.Module;
 import seedu.duke.universities.University;
 import seedu.duke.constants.Constants;
 
 public class Ui {
-    public static void printModule(Module mod, int index ) {
+
+    public static void printModule(Module mod, int index) {
         printIndex(index, false);
         System.out.println(" " + mod.getModuleCode());
     }
+
     public static void printUniversity(University uni, int index) {
         printIndex(index, false);
         System.out.println(" " + uni.getName());
     }
+    
     public static void printIndex(int index, boolean println) {
         String format = Constants.INDEX_WRAP_FRONT + index + Constants.INDEX_WRAP_BACK;
         if (println) {
