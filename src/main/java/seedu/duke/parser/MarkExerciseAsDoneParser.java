12e6e07ee63f66a6f42ccdac715053d944635162
JmattChiam <joelmchiam@gmail.com>
2021-10-14 15:08:54 +0800

    Add Parser child classes
    Added child classes for Parser.
    Renamed Generator Class to CommandManager.

---
+++ src/main/java/seedu/duke/parser/MarkExerciseAsDoneParser.java
@@ -0,0 +1,31 @@
+package seedu.duke.parser;
+
+import seedu.duke.command.Command;
+import seedu.duke.command.IncorrectCommand;
+import seedu.duke.command.exercise.MarkExerciseAsDoneCommand;
+import seedu.duke.exception.GetJackDException;
+
+public class MarkExericseAsDoneParser extends Parser {
+
+    public MarkExericseAsDoneParser(String userInputString) {
+        this.userInputString = userInputString;
+    }
+
+    private Command prepareMarkExerciseAsDone(String commandArgs) {
+        try {
+            String[] indices = getWorkoutAndExerciseIndices(commandArgs);
+            int workoutIndex = parseArgsAsIndex(indices[0]);
+            int exerciseIndex = parseArgsAsIndex(indices[1]);
+
+            return new MarkExerciseAsDoneCommand(workoutIndex, exerciseIndex);
+        } catch (GetJackDException e) {
+            return new IncorrectCommand(MESSAGE_INVALID_COMMAND + MarkExerciseAsDoneCommand.MESSAGE_USAGE);
+        }
+    }
+
+    @Override
+    public Command parseInput() {
+        String commandArgs = getCommandArguments(userInputString);
+        return prepareMarkExerciseAsDone(commandArgs);
+    }
+}
