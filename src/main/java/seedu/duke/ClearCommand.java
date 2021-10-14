ef4e84c592f7b3d3e6f9d070de72649e8584087e
swongts <70316271+swongts@users.noreply.github.com>
2021-10-14 16:48:56 +0800

    Add exceptions for extra params after commands that dont require params Add assertion for clear command and parser

--- src/main/java/seedu/duke/ClearCommand.java
+++ src/main/java/seedu/duke/ClearCommand.java
@@ -3,6 +3,7 @@ package seedu.duke;
 public class ClearCommand extends Command {
     public void execute(ClientList clientList, Ui ui) {
         clientList.clearAllClients();
+        assert clientList.getClientCount() == 0;
         ui.showClear();
     }
 
