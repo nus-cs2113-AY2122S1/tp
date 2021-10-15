84538dae8b5b5938c544657edb81654247a8e348
richwill28 <e0550368@u.nus.edu>
2021-10-15 18:09:07 +0800

    Create ParseException class

---
+++ src/main/java/seedu/duke/logic/parser/exceptions/ParseException.java
@@ -0,0 +1,9 @@
+package seedu.duke.logic.parser.exceptions;
+
+import seedu.duke.DukeException;
+
+public class ParseException extends DukeException {
+    public ParseException(String message) {
+        super(message);
+    }
+}
