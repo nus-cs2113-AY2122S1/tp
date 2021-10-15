0b4e36deb527c4301eb50e71efbb7b8406fc1b1f
WDOneM <58932649+maxwireddevs@users.noreply.github.com>
2021-10-15 22:50:34 +0800

    wiki

---
+++ src/test/java/seedu/WikiImportTest.java
@@ -0,0 +1,29 @@
+package seedu;
+
+import org.junit.jupiter.api.Test;
+import seedu.typists.content.WikiImport;
+import seedu.typists.exception.InvalidArticleException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class WikiImportTest {
+
+    // methodBeingTested_inputGiven_expectedOutcome
+    // Input, GitHub, relevant article (731 characters)
+    @Test
+    void getArticle_GitHub_expectArticle() throws InvalidArticleException {
+        String article = "GitHub";
+        WikiImport wiki = new WikiImport();
+        String content = wiki.getArticle(article);
+        assertEquals(731, content.length());
+    }
+
+    // test failure case
+    // method being tested, Githob, expect exception
+    @Test
+    void getArticle_Githob_expectException() {
+        String article = "Githob";
+        WikiImport wiki = new WikiImport();
+        assertThrows(InvalidArticleException.class, () -> wiki.getArticle(article));
+    }
+}
