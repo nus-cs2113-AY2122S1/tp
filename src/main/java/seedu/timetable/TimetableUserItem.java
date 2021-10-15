6d22a760df3f2f2f904ac6c99d5d6c1f47a2b79e
xuanlc113 <xuan.lc113@gmail.com>
2021-10-15 15:01:13 +0800

    add timetable user item class

--- src/main/java/seedu/timetable/TimetableUserItem.java
+++ src/main/java/seedu/timetable/TimetableUserItem.java
@@ -2,4 +2,10 @@ package seedu.timetable;
 
 public class TimetableUserItem extends TimetableItem {
 
+    private String description;
+
+    public TimetableUserItem(String title, String day, String startTime, String endTime, String description) {
+        super(title, day, startTime, endTime);
+        this.description = description;
+    }
 }
