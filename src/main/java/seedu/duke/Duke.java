11e8fc883e46461d675845bb728c79288ae38ab6
richwill28 <e0550368@u.nus.edu>
2021-10-15 23:03:22 +0800

    Initialize logger

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,6 +1,11 @@
 package seedu.duke;
 
 import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import seedu.duke.logic.commands.Command;
 import seedu.duke.model.lesson.LessonList;
@@ -11,6 +16,8 @@ import seedu.duke.commons.core.Messages;
 import seedu.duke.ui.Ui;
 
 public class Duke {
+    private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
     private final Ui ui;
     private final Storage storage;
     private TaskList taskList;
@@ -28,7 +35,9 @@ public class Duke {
             taskList = new TaskList(TaskList.deserialize(storage.loadData()));
             lessonList = new LessonList(LessonList.deserialize(storage.loadData()));
             ui.printMessage(Messages.SUCCESS_RETRIEVING_DATA);
+            LOGGER.info("Successfully retrieved data from the save file.");
         } catch (DukeException | IOException e) {
+            LOGGER.warning("Failed to retrieve data from the save file.");
             ui.printMessage(e.getMessage());
             storage.createNewData(ui);
             taskList = new TaskList();
@@ -41,6 +50,7 @@ public class Duke {
      * to the corresponding command and executes it.
      */
     public void startProgram() {
+        LOGGER.info("Executing main routine.");
         boolean isExit = false;
         while (!isExit) {
             try {
@@ -49,9 +59,11 @@ public class Duke {
                 command.execute(ui, storage, taskList, lessonList);
                 isExit = command.isExit();
             } catch (DukeException | IOException e) {
+                LOGGER.warning("Invalid command.");
                 ui.printMessage(e.getMessage());
             }
         }
+        LOGGER.info("Main routine has ended.");
     }
 
     public void run() {
@@ -62,8 +74,25 @@ public class Duke {
 
     /** The main method. Creates an instance of Duke and run it. */
     public static void main(String[] args) {
-        assert false : "dummy assertion set to fail";
+        LOGGER.info("Initializing Duke.");
         Duke duke = new Duke();
         duke.run();
+    }
+
+    private void initializeLogger() {
+        LogManager.getLogManager().reset();
+        LOGGER.setLevel(Level.ALL);
+
+        ConsoleHandler ch = new ConsoleHandler();
+        ch.setLevel(Level.SEVERE);
+        LOGGER.addHandler(ch);
+
+        try {
+            FileHandler fh = new FileHandler("duke.log");
+            fh.setLevel(Level.FINE);
+            LOGGER.addHandler(fh);
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, Messages.ERROR_FILE_LOGGER, e);
+        }
     }
 }
