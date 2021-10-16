2a5e85d8d6a38a749400d2f7e951b58ca37ce57a
AnShengLee <anshengl@yahoo.com.sg>
2021-10-16 20:01:11 +0800

    Update Main and Messages for DataManager

--- src/main/java/seedu/duke/StonksXD.java
+++ src/main/java/seedu/duke/StonksXD.java
@@ -2,13 +2,18 @@ package seedu.duke;
 
 import seedu.commands.Command;
 import seedu.commands.ExitCommand;
+
+import seedu.utility.DataManager;
 import seedu.utility.FinancialTracker;
+
 import seedu.utility.Ui;
+
 
 public class StonksXD {
     private Ui ui;
     private FinancialTracker finances;
     private Parser parser;
+    private DataManager dataManager;
 
     public StonksXD() {
         this.ui = new Ui();
@@ -17,6 +22,7 @@ public class StonksXD {
     }
 
     public void run() {
+        dataManager.load(parser, finances, ui);
         ui.printWelcome();
 
         boolean exitFlag = true;
@@ -28,6 +34,9 @@ public class StonksXD {
                 assert command.getClass() == ExitCommand.class;
                 exitFlag = false;
             }
+
+            dataManager.save(parser, finances, ui);
+
         }
         ui.printBye();
     }
--- src/main/java/seedu/utility/Messages.java
+++ src/main/java/seedu/utility/Messages.java
@@ -12,4 +12,9 @@ public class Messages {
     public static final String NON_POSITIVE_INDEX_MESSAGE = "Only positive values are allowed for index.";
     public static final String UNABLE_TO_DELETE_MESSAGE = "Entry not deleted because entry not found!";
     public static final String BLANK_DESCRIPTION_MESSAGE = "Your description is empty!";
+    public static final String HAS_CORRUPTED_DATA_ENTRIES = "StonksXD_Data.csv has corrupted entries, " +
+            "some or all data maybe lost.";
+    public static final String UNABLE_TO_FIND_DATA_FILE = "Unable to find StonksXD_Data.csv, a new one has been made.";
+    public static final String ERROR_SAVING_ENTRIES = "Unable to save entries into StonksXD_Data.csv, " +
+            "some or all data maybe lost.";
 }
