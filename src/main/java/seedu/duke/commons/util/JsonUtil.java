814054419b2b86f5648cd02693f4fbd90b1c7e64
ptejasv <e0564194@u.nus.edu>
2021-10-16 16:28:02 +0800

    Use filepaths to handle json file

--- src/main/java/seedu/duke/commons/util/JsonUtil.java
+++ src/main/java/seedu/duke/commons/util/JsonUtil.java
@@ -9,20 +9,26 @@ import seedu.duke.model.module.Module;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.Reader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class JsonUtil {
     private Module[] modules;
     private static Logger logger;
-    private static final String FULL_MODULE_FILEPATH = System.getProperty("user.dir") + "/src/main/resources"
-            + "/ModuleInfo.json";
+    private static final String ROOT_DIRECTORY = System.getProperty("user.dir");
+    private static final String MODULE_DIRECTORY_PATH = "src/main/resources";
+    private static final String MODULE_FILE_NAME = "ModuleInfo.json";
+//    private static final Path FULL_MODULE_FILEPATH = Paths.get(ROOT_DIRECTORY, "src", "main",
+//            "resources", "ModuleInfo.json");
+    private static final Path FULL_MODULE_FILEPATH = Paths.get(ROOT_DIRECTORY, MODULE_DIRECTORY_PATH, MODULE_FILE_NAME);
 
     public JsonUtil() throws ModuleLoadException, FileNotFoundException {
         Gson gson = new Gson();
         logger = Logger.getLogger(JsonUtil.class.getName());
 
-        Reader reader = new FileReader(FULL_MODULE_FILEPATH);
+        Reader reader = new FileReader(FULL_MODULE_FILEPATH.toString());
         modules = gson.fromJson(reader, Module[].class);
 
         if (modules.length == 0) {
