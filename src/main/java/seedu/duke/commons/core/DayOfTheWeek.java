db4d8588e3f06106b630fd386bb8bc95abdd2238
richwill28 <e0550368@u.nus.edu>
2021-10-18 01:09:04 +0800

    Add new method to convert day to proper format

--- src/main/java/seedu/duke/commons/core/DayOfTheWeek.java
+++ src/main/java/seedu/duke/commons/core/DayOfTheWeek.java
@@ -1,12 +1,7 @@
 package seedu.duke.commons.core;
 
-import java.time.LocalDate;
-
 public enum DayOfTheWeek {
     MON, TUE, WED, THU, FRI, SAT, SUN;
-
-    public static final String TODAY_PERIOD = "today";
-    public static final String TOMORROW_PERIOD = "tomorrow";
 
     /**
      * Determines if the input parameter is a valid day of the week.
@@ -16,42 +11,31 @@ public enum DayOfTheWeek {
      */
     public static boolean is(String param) {
         for (DayOfTheWeek day : DayOfTheWeek.values()) {
-            if (param.equalsIgnoreCase(day.toString())) {
+            if (param.substring(0, 3).equalsIgnoreCase(day.toString())) {
                 return true;
             }
         }
         return false;
     }
 
-    /**
-     * Checks if period given is "today" or "tomorrow".
-     *
-     * @param period the given period
-     * @return true if period is "today" or "tomorrow", false otherwise
-     */
-    public static boolean isTodayOrTomorrow(String period) {
-        return period.equalsIgnoreCase(TODAY_PERIOD) | period.equalsIgnoreCase(TOMORROW_PERIOD);
-    }
-
-    /**
-     * Returns the DayOfTheWeek for today.
-     *
-     * @return DayOfTheWeek for today in lowercase
-     */
-    public static String getCurrentDayOfWeek() {
-        LocalDate currentDate = LocalDate.now();
-        String currentDayOfWeek = currentDate.getDayOfWeek().toString();
-        return currentDayOfWeek.substring(0,3).toLowerCase();
-    }
-
-    /**
-     * Returns the DayOfTheWeek for tomorrow.
-     *
-     * @return DayOfTheWeek for tomorrow in lowercase
-     */
-    public static String getNextDayOfWeek() {
-        LocalDate currentDate = LocalDate.now().plusDays(1);
-        String currentDayOfWeek = currentDate.getDayOfWeek().toString();
-        return currentDayOfWeek.substring(0,3).toLowerCase();
+    public static String toProper(String param) {
+        switch (param.substring(0, 3).toLowerCase()) {
+        case "mon":
+            return "Monday";
+        case "tue":
+            return "Tuesday";
+        case "wed":
+            return "Wednesday";
+        case "thu":
+            return "Thursday";
+        case "fri":
+            return "Friday";
+        case "sat":
+            return "Saturday";
+        case "sun":
+            return "Sunday";
+        default:
+            return "";
+        }
     }
 }
