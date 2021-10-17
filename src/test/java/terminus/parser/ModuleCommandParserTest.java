ea2d0a8fe025b7ef04ba03f7fae1fdadf72b9ff8
3m0W33D <sky.t4y@gmail.com>
2021-10-17 21:51:22 +0800

    Add module parsing JUnit test

---
+++ src/test/java/terminus/parser/ModuleCommandParserTest.java
@@ -0,0 +1,109 @@
+package terminus.parser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import terminus.command.ExitCommand;
+import terminus.command.HelpCommand;
+import terminus.command.module.AddModuleCommand;
+import terminus.command.module.DeleteModuleCommand;
+import terminus.command.module.ViewModuleCommand;
+import terminus.exception.InvalidArgumentException;
+import terminus.exception.InvalidCommandException;
+
+public class ModuleCommandParserTest {
+
+    private ModuleCommandParser commandParser;
+
+    @BeforeEach
+    void setUp() {
+        commandParser = ModuleCommandParser.getInstance();
+    }
+
+    @Test
+    void parseCommand_invalidCommand_exceptionThrown() {
+        assertThrows(InvalidCommandException.class, () -> commandParser.parseCommand("ex it"));
+        assertThrows(InvalidCommandException.class, () -> commandParser.parseCommand("helpa"));
+        assertThrows(InvalidCommandException.class, () -> commandParser.parseCommand("adda"));
+        assertThrows(InvalidCommandException.class, () -> commandParser.parseCommand("vie wer"));
+        assertThrows(InvalidCommandException.class, () -> commandParser.parseCommand("deleterr"));
+        assertThrows(InvalidCommandException.class, () -> commandParser.parseCommand(""));
+    }
+
+    @Test
+    void parseCommand_resolveExitCommand_success()
+            throws InvalidCommandException, InvalidArgumentException {
+        assertTrue(commandParser.parseCommand("exit") instanceof ExitCommand);
+        assertTrue(commandParser.parseCommand("EXIT") instanceof ExitCommand);
+        assertTrue(commandParser.parseCommand("   exit   ") instanceof ExitCommand);
+        assertTrue(commandParser.parseCommand("eXiT a") instanceof ExitCommand);
+    }
+
+    @Test
+    void parseCommand_resolveHelpCommand_success()
+            throws InvalidCommandException, InvalidArgumentException {
+        assertTrue(commandParser.parseCommand("help") instanceof HelpCommand);
+        assertTrue(commandParser.parseCommand("HELP") instanceof HelpCommand);
+        assertTrue(commandParser.parseCommand("   help   ") instanceof HelpCommand);
+        assertTrue(commandParser.parseCommand("HeLp    a") instanceof HelpCommand);
+    }
+
+    @Test
+    void parseCommand_resolveAddCommand_exceptionThrown()
+            throws InvalidCommandException, InvalidArgumentException {
+        assertThrows(InvalidArgumentException.class, () -> commandParser.parseCommand("add"));
+        assertThrows(InvalidArgumentException.class,
+            () -> commandParser.parseCommand("add \"test\" \"test1\" "));
+        assertThrows(InvalidArgumentException.class, () -> commandParser.parseCommand("add \"    test     \" "));
+    }
+
+    @Test
+    void parseCommand_resolveAddCommand_success() throws InvalidCommandException, InvalidArgumentException {
+        assertTrue(commandParser.parseCommand("add \"test\" ") instanceof AddModuleCommand);
+        assertTrue(commandParser.parseCommand("add \"username\"") instanceof AddModuleCommand);
+        assertTrue(commandParser.parseCommand("add \"test1\"test2\"") instanceof AddModuleCommand);
+    }
+
+    @Test
+    void parseCommand_resolveDeleteCommand_exceptionThrown() throws InvalidCommandException, InvalidArgumentException {
+        assertThrows(InvalidArgumentException.class, () -> commandParser.parseCommand("delete"));
+        assertThrows(InvalidArgumentException.class, () -> commandParser.parseCommand("delete abcd"));
+        assertThrows(InvalidArgumentException.class, () -> commandParser.parseCommand("delete -5"));
+    }
+
+    @Test
+    void parseCommand_resolveDeleteCommand_success()
+            throws InvalidCommandException, InvalidArgumentException {
+        assertTrue(commandParser.parseCommand("delete 1") instanceof DeleteModuleCommand);
+        assertTrue(commandParser.parseCommand("delete 2") instanceof DeleteModuleCommand);
+    }
+
+    @Test
+    void parseCommand_resolveViewCommand_success() throws InvalidCommandException, InvalidArgumentException {
+        assertTrue(commandParser.parseCommand("view") instanceof ViewModuleCommand);
+    }
+
+    @Test
+    void getCommandList_containsBasicCommands() {
+        assertTrue(commandParser.getCommandList().contains("exit"));
+        assertTrue(commandParser.getCommandList().contains("add"));
+        assertTrue(commandParser.getCommandList().contains("back"));
+        assertTrue(commandParser.getCommandList().contains("delete"));
+        assertTrue(commandParser.getCommandList().contains("view"));
+        assertTrue(commandParser.getCommandList().contains("help"));
+    }
+
+    @Test
+    void getWorkspace_isModule() {
+        assertEquals("module", commandParser.getWorkspace());
+    }
+
+    @Test
+    void getHelpMenu_isNotEmpty() {
+        assertTrue(commandParser.getHelpMenu().length > 0);
+    }
+
+}
---
+++ src/test/java/terminus/parser/ModuleWorkspaceCommandParserTest.java
@@ -0,0 +1,72 @@
+package terminus.parser;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import terminus.command.ExitCommand;
+import terminus.command.HelpCommand;
+import terminus.command.NotesCommand;
+import terminus.command.ScheduleCommand;
+import terminus.exception.InvalidArgumentException;
+import terminus.exception.InvalidCommandException;
+
+public class ModuleWorkspaceCommandParserTest {
+
+    private ModuleWorkspaceCommandParser commandParser;
+
+    @BeforeEach
+    void setUp() {
+        commandParser = ModuleWorkspaceCommandParser.getInstance();
+        commandParser.setWorkspace("2106");
+    }
+
+    @Test
+    void parseCommand_invalidCommand_exceptionThrown() {
+        assertThrows(InvalidCommandException.class, () -> commandParser.parseCommand("ex it"));
+        assertThrows(InvalidCommandException.class, () -> commandParser.parseCommand("helpa"));
+        assertThrows(InvalidCommandException.class, () -> commandParser.parseCommand("adda"));
+        assertThrows(InvalidCommandException.class, () -> commandParser.parseCommand("vie wer"));
+        assertThrows(InvalidCommandException.class, () -> commandParser.parseCommand("deleterr"));
+        assertThrows(InvalidCommandException.class, () -> commandParser.parseCommand(""));
+    }
+
+    @Test
+    void parseCommand_resolveExitCommand_success()
+            throws InvalidCommandException, InvalidArgumentException {
+        assertTrue(commandParser.parseCommand("exit") instanceof ExitCommand);
+        assertTrue(commandParser.parseCommand("EXIT") instanceof ExitCommand);
+        assertTrue(commandParser.parseCommand("   exit   ") instanceof ExitCommand);
+        assertTrue(commandParser.parseCommand("eXiT a") instanceof ExitCommand);
+    }
+
+    @Test
+    void parseCommand_resolveHelpCommand_success()
+            throws InvalidCommandException, InvalidArgumentException {
+        assertTrue(commandParser.parseCommand("help") instanceof HelpCommand);
+        assertTrue(commandParser.parseCommand("HELP") instanceof HelpCommand);
+        assertTrue(commandParser.parseCommand("   help   ") instanceof HelpCommand);
+        assertTrue(commandParser.parseCommand("HeLp    a") instanceof HelpCommand);
+    }
+
+    @Test
+    void parseCommand_resolveNoteCommand_success()
+            throws InvalidCommandException, InvalidArgumentException {
+        assertTrue(commandParser.parseCommand("note") instanceof NotesCommand);
+        assertTrue(commandParser.parseCommand("note add \"test\" \"test\"") instanceof NotesCommand);
+        assertTrue(commandParser.parseCommand("note view") instanceof NotesCommand);
+        assertTrue(commandParser.parseCommand("note delete 1") instanceof NotesCommand);
+    }
+
+    @Test
+    void parseCommand_resolveScheduleCommand_success()
+            throws InvalidCommandException, InvalidArgumentException {
+        assertTrue(commandParser.parseCommand("schedule") instanceof ScheduleCommand);
+        assertTrue(commandParser.parseCommand(
+                "schedule add \"test desc\" \"Tuesday\" \"10:00\" \"https://zoom.us/test\"")
+                instanceof ScheduleCommand);
+        assertTrue(commandParser.parseCommand("schedule view") instanceof ScheduleCommand);
+        assertTrue(commandParser.parseCommand("schedule delete 1") instanceof ScheduleCommand);
+    }
+}
