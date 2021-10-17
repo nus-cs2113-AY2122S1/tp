bb721aa11488c29d951b795c6aed03da7f36ce94
richwill28 <e0550368@u.nus.edu>
2021-10-17 22:55:21 +0800

    Add TimeParseException class

---
+++ src/main/java/seedu/duke/commons/util/exceptions/TimeParseException.java
@@ -0,0 +1,9 @@
+package seedu.duke.commons.util.exceptions;
+
+import seedu.duke.DukeException;
+
+public class TimeParseException extends DukeException {
+    public TimeParseException(String message) {
+        super(message);
+    }
+}
