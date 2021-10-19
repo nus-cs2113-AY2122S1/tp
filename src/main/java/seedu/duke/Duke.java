f722184db2c85425a4d9e3e5ace4133d9033f220
poppolette <66718378+poppolette@users.noreply.github.com>
2021-10-20 01:34:01 +0800

    Update Duke.java

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -5,7 +5,6 @@ public class Duke {
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        assert false : "dummy assertion set to fail";
         Ui.printGreeting();
         Parser.waitForQuery();
     }
