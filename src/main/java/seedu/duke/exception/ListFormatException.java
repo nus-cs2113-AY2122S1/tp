0055ac2c0360185ee625152d8769d01b3270ffc9
APZH <amospzh@gmail.com>
2021-10-20 02:06:55 +0800

    update ListFormatException message

--- src/main/java/seedu/duke/exception/ListFormatException.java
+++ src/main/java/seedu/duke/exception/ListFormatException.java
@@ -2,8 +2,8 @@ package seedu.duke.exception;
 
 public class ListFormatException extends Exception {
 
-    private static final String MESSAGE = "[!] Your list command is wrong...\n"
-            + "Please follow the format: list or list --[type, priority, recur]";
+    private static final String MESSAGE = "[!] Your list command is using an invalid flag...\n"
+            + "Please follow the format: list or list --[type, priority, recur] <argument>";
 
     @Override
     public String toString() {
