f7c619f0e4443dc8705241de779fd62da2acc638
Ng Andre <ng.andr3@gmail.com>
2021-10-19 11:05:09 +0800

    Inherit ContactParser for SearchContactParser and implement getDetailFlag method

--- src/main/java/seedu/parser/SearchContactParser.java
+++ src/main/java/seedu/parser/SearchContactParser.java
@@ -1,13 +1,9 @@
 package seedu.parser;
 
+import seedu.exception.InvalidFlagException;
 import seedu.exception.MissingArgException;
 
-import java.util.Locale;
-
-public class SearchContactParser {
-
-    private static final int NUMBER_OF_SEARCH_ARGS = 3;
-
+public class SearchContactParser extends ContactParser {
     //return only search query as a String
     public String parseSearchQuery(String userInput) throws MissingArgException {
         String[] destructuredInputs = userInput.split(" ", NUMBER_OF_SEARCH_ARGS);
@@ -16,6 +12,27 @@ public class SearchContactParser {
         } else if (destructuredInputs.length == 3) { //search for name with flag
             return destructuredInputs[2].toLowerCase().trim();
         }
-        throw new MissingArgException(); //if no query is specified
+        //no query specified
+        //destructuredInputs.length == 1
+        throw new MissingArgException();
+    }
+
+    public int getDetailFlag(String userInput) throws MissingArgException, InvalidFlagException {
+        String[] destructuredInputs = userInput.split(" ", NUMBER_OF_SEARCH_ARGS);
+        //"search  " with one or two spaces should not have an issue since the input is trimmed
+        if (destructuredInputs.length == 1) { //no arguments specified, only "search"
+            throw new MissingArgException();
+        } else if (destructuredInputs.length == 2) { //no flag specified
+            return -1;
+        } else {
+            assert destructuredInputs.length == 3;
+            return getIndexToStore(destructuredInputs[2]);
+        }
+    }
+
+    //method not used for Search but has to be implemented for classes inheriting ContactParser
+    public String[] parseContactDetails(String userInput) {
+        assert false;
+        return new String[0];
     }
 }
