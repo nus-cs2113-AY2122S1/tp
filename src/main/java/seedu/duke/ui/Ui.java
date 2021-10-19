b327e6138a43b5ab730ad69c594b84e3aacd886f
titustortoiseturtle1999 <ngtitus@ymail.com>
2021-10-19 15:37:22 +0800

    add logo

--- src/main/java/seedu/duke/ui/Ui.java
+++ src/main/java/seedu/duke/ui/Ui.java
@@ -32,6 +32,6 @@ public class Ui {
 
     public static void welcome() {
         System.out.println(AsciiConstants.GLOBE);
-        System.out.println();
+        System.out.println(AsciiConstants.LOGO);
     }
 }
