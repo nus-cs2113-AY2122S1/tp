395a76b9ca2fffd6e101f136c8dece09d00e2faa
ChuaXiongWei <e0543484@u.nus.edu>
2021-10-20 16:39:09 +0800

    Updated UI and Help

--- src/main/java/gordon/command/HelpCommand.java
+++ src/main/java/gordon/command/HelpCommand.java
@@ -3,14 +3,7 @@ package gordon.command;
 import gordon.kitchen.Cookbook;
 
 public class HelpCommand extends Command {
-    static final String helpString = "To add a recipe: add \"recipe name\" \"/ingredients\" 1+2+3 \"/steps\" 1+2+3 \"/calories\" 123" + System.lineSeparator() +
-            "To delete a recipe: deleteRecipe \"Index of recipe\"" + System.lineSeparator() +
-            "To see all your recipes: listRecipes" + System.lineSeparator() +
-            "To find a recipe: find \"Keyword\"" + System.lineSeparator() +
-            "To check a specific recipe: check \"Name of Recipe\"" + System.lineSeparator() +
-            "To tag a recipe: tag / recipeName / tagName1 + tagName2 + ..." + System.lineSeparator() +
-            "To untag a recipe: untag / recipeName / tagName1 + tagName2 + ..." + System.lineSeparator() +
-            "To list all tags: listTags";
+    static final String helpString = "To add a recipe: add \"recipe name\" \"/ingredients\" 1+2+3 \"/steps\" 1+2+3 \"/calories\" 123";
 
     @Override
     public void execute(Cookbook cookbook) {
