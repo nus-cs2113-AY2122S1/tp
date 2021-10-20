ecb076e62ad764dbb11d8de0c50443719d77334e
ChuaXiongWei <e0543484@u.nus.edu>
2021-10-20 16:42:17 +0800

    Updated UI and Help

--- src/main/java/gordon/util/UI.java
+++ src/main/java/gordon/util/UI.java
@@ -12,14 +12,14 @@ public class UI {
     }
 
     public void printHelp() {
-        System.out.println("1. Add a recipe: add \"recipe name\" \"/ingredients\" 1+2+3 \"/steps\" 1+2+3 \"/calories\" 123" + System.lineSeparator() +
-                "2. Delete a recipe: deleteRecipe \"Index of recipe\"" + System.lineSeparator() +
-                "3. List all your recipes: listRecipes" + System.lineSeparator() +
-                "4. Find a recipe: find \"Keyword\"" + System.lineSeparator() +
-                "5. Check a specific recipe: check \"Name of Recipe\"" + System.lineSeparator() +
-                "6. Tag a recipe: tag \"/ recipeName\" \"/ tagName1 + tagName2 + ...\"" + System.lineSeparator() +
-                "7. Untag a recipe: untag \"/ recipeName\" \"/ tagName1 + tagName2 + ...\"" + System.lineSeparator() +
-                "8. List all tags: listTags" + System.lineSeparator() +
-                "9. Help me: help");
+        System.out.println("1. Add a recipe: add \"recipe name\" \"/ingredients\" 1+2+3 \"/steps\" 1+2+3 \"/calories\" 123");
+        System.out.println("2. Delete a recipe: deleteRecipe \"Index of recipe\"");
+        System.out.println("3. List all your recipes: listRecipes");
+        System.out.println("4. Find a recipe: find \"Keyword\"");
+        System.out.println("5. Check a specific recipe: check \"Name of Recipe\"");
+        System.out.println("6. Tag a recipe: tag \"/ recipeName\" \"/ tagName1 + tagName2 + ...\"");
+        System.out.println("7. Untag a recipe: untag \"/ recipeName\" \"/ tagName1 + tagName2 + ...\"");
+        System.out.println("8. List all tags: listTags");
+        System.out.println("9. Help me: help");
     }
 }
