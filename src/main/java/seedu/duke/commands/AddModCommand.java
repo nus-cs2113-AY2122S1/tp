07b86dcc2f25a27bcb75c83fd0b8cdc87a95a29c
Jiale-Sun <e0638853@u.nus.edu>
2021-10-20 18:20:05 +0800

    commit before pull

--- src/main/java/seedu/duke/commands/AddModCommand.java
+++ src/main/java/seedu/duke/commands/AddModCommand.java
@@ -4,6 +4,7 @@ import seedu.duke.modules.Module;
 import seedu.duke.modules.ModuleList;
 import seedu.duke.storage.SelectedModuleStorage;
 import seedu.duke.universities.UniversityList;
+import seedu.duke.ui.Ui;
 
 import java.io.IOException;
 
@@ -25,6 +26,8 @@ public class AddModCommand extends Command {
         assert moduleSelectedList.get(moduleSelectedList.getSize() - 1)
                 .getModuleCode().equals(moduleToAdd.getModuleCode());
         SelectedModuleStorage.write(moduleSelectedList);
-        System.out.println("New module added: " + moduleToAdd.getModuleCode());
+        System.out.println("New module added: ");
+        Ui.printIndex(indexToAdd, 0);
+        Ui.printModule(moduleToAdd, indexToAdd);
     }
 }
