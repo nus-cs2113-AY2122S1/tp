67fcb2fc451a9d0370ccbf62054d2eb75ff2978b
Ashraf Jaffar <a.ashrafjfr@gmail.com>
2021-10-21 03:28:37 +0800

    Add duplicate catcher in edit execution

--- src/main/java/seedu/command/EditContactCommand.java
+++ src/main/java/seedu/command/EditContactCommand.java
@@ -1,10 +1,21 @@
 package seedu.command;
 
+import seedu.contact.Contact;
+import seedu.contact.ContactList;
 import seedu.exception.InvalidFlagException;
 import seedu.ui.TextUi;
 import seedu.ui.ExceptionTextUi;
+import seedu.ui.UserInputTextUi;
+
+import java.util.ArrayList;
 
 public class EditContactCommand extends Command {
+    public static final int NAME_INDEX = 0;
+    public static final int GITHUB_INDEX = 1;
+    public static final int LINKEDIN_INDEX = 2;
+    public static final int TELEGRAM_INDEX = 3;
+    public static final int TWITTER_INDEX = 4;
+    public static final int EMAIL_INDEX = 5;
     String[] contactDetails;
     int contactIndex;
 
@@ -15,8 +26,14 @@ public class EditContactCommand extends Command {
 
     public void execute() {
         try {
-            contactList.editContact(contactDetails, contactIndex);
-            TextUi.editContactMessage(contactList.getContactAtIndex(contactIndex));
+            Contact preEditContact = duplicateContact(contactList.getContactAtIndex(contactIndex));
+            Contact postEditContact = editTempContact(contactDetails,preEditContact);
+            if (duplicateCatcher(postEditContact, contactList, contactIndex)) {
+                TextUi.ignoreEditContact();
+            } else {
+                contactList.editContact(contactDetails, contactIndex);
+                TextUi.editContactMessage(postEditContact);
+            }
         } catch (IndexOutOfBoundsException | InvalidFlagException e) {
             ExceptionTextUi.invalidIndexMessage();
         }
