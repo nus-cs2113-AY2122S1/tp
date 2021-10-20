ec0d06e1160b4898b589893ebe4e188faa36add9
datn02 <ducanh02.work@gmail.com>
2021-10-20 17:55:40 +0800

    Added some test cases

--- src/test/java/seedu/duke/command/AddCommandTest.java
+++ src/test/java/seedu/duke/command/AddCommandTest.java
@@ -4,21 +4,31 @@ import org.junit.jupiter.api.Test;
 import seedu.duke.exceptions.DukeException;
 import seedu.duke.ingredients.Ingredient;
 import seedu.duke.ingredients.IngredientList;
+import seedu.duke.localtime.CurrentDate;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddCommandTest {
-   /*
+
+    private final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
     @Test
     public void addCommandTest_sampleIngredient() throws DukeException {
-        Ingredient sampleIngredient = new Ingredient("onion", 15.0, "grams", "10/08/2021");
+        LocalDate expiryDate = LocalDate.parse("10/08/2021", DATE_FORMATTER);
+        Ingredient sampleIngredient = new Ingredient("onion", 15.0, "kg", expiryDate);
         String resultMsg = new AddCommand(sampleIngredient).run();
         String expected = "Got it. This ingredient has been added to the inventory:\n"
                 + "\t" + sampleIngredient.toString() + '\n'
                 + "Current inventory has " + IngredientList.getInstance().getInventoryStock()
-                + " items.";
+                + " items.\n"
+                + "This ingredient will expire in " + ChronoUnit.DAYS.between(CurrentDate.getCurrentDate(), expiryDate)
+                + " days.";
 
         assertEquals(expected, resultMsg);
     }
-    */
+
 }
--- src/test/java/seedu/duke/command/UpdateCommandTest.java
+++ src/test/java/seedu/duke/command/UpdateCommandTest.java
@@ -6,26 +6,35 @@ import seedu.duke.ingredients.Ingredient;
 import seedu.duke.ingredients.IngredientList;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UpdateCommandTest {
 
-    /*@Test
+    private final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    @Test
     public void updateCommandTest_dukeExceptionThrown() throws DukeException, IOException {
-        Ingredient ingredient1 = new Ingredient("Carrot", 300, "grams", "21st Oct");
-        Ingredient ingredient2 = new Ingredient("Tomato", 200, "ounces", "1st Nov");
-        Ingredient ingredient3 = new Ingredient("Carrot", 1.5, "kilograms", "2nd Oct");
+        LocalDate expiryDate1 = LocalDate.parse("21/10/2021", DATE_FORMATTER);
+        LocalDate expiryDate2 = LocalDate.parse("01/11/2021", DATE_FORMATTER);
+        LocalDate expiryDate3 = LocalDate.parse("02/10/2021", DATE_FORMATTER);
+        LocalDate expiryDate4 = LocalDate.parse("04/11/2021", DATE_FORMATTER);
+
+        Ingredient ingredient1 = new Ingredient("Carrot", 300, "grams", expiryDate1);
+        Ingredient ingredient2 = new Ingredient("Tomato", 200, "ounces", expiryDate2);
+        Ingredient ingredient3 = new Ingredient("Carrot", 1.5, "kilograms", expiryDate3);
 
         IngredientList.getInstance().add(ingredient1);
         IngredientList.getInstance().add(ingredient2);
         IngredientList.getInstance().add(ingredient3);
 
-        Ingredient updatedIngredient = new Ingredient("Carrot", 450.0, "grams", "4th Nov");
+        Ingredient updatedIngredient = new Ingredient("Carrot", 450.0, "grams", expiryDate4);
         String resultMsg = new UpdateCommand(updatedIngredient).run();
         String expected = "Got it. This ingredient has been updated:\n" + "\t" + updatedIngredient.toString();
 
         assertEquals(expected, resultMsg);
-    }*/
+    }
 }
