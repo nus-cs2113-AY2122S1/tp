946f85d3064aa41c8496876979a870a8213da864
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2021-10-22 01:06:21 +0800

    formatting

--- src/main/java/seedu/duke/parser/Parser.java
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -1,6 +1,21 @@
 package seedu.duke.parser;
 
-import seedu.duke.commands.*;
+import seedu.duke.commands.AddBudgetCommand;
+import seedu.duke.commands.AddCommand;
+import seedu.duke.commands.AddExpenditureCommand;
+import seedu.duke.commands.AddLoanCommand;
+import seedu.duke.commands.Command;
+import seedu.duke.commands.DeleteAllExpenditureCommand;
+import seedu.duke.commands.DeleteBudgetCommand;
+import seedu.duke.commands.DeleteCommand;
+import seedu.duke.commands.DeleteMultipleExpenditureCommand;
+import seedu.duke.commands.DeleteSingleExpenditureCommand;
+import seedu.duke.commands.ExitCommand;
+import seedu.duke.commands.HelpCommand;
+import seedu.duke.commands.InvalidCommand;
+import seedu.duke.commands.ListRecordsCommand;
+import seedu.duke.commands.YearCommand;
+import seedu.duke.commands.FindCommand;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
