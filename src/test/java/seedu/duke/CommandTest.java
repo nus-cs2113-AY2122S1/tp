540d9a4fa1ddc312d9f745e286dbcaed1e518af1
Nirmaleshwar <e0415906@u.nus.edu>
2021-10-21 20:43:30 +0800

    CheckStyle issue corrected

--- src/test/java/seedu/duke/CommandTest.java
+++ src/test/java/seedu/duke/CommandTest.java
@@ -49,7 +49,7 @@ public class CommandTest {
     @Test
     public void testClearAllEntriesCommand() {
         ClearAllEntriesCommand testCommand = new ClearAllEntriesCommand();
-        testCommand.execute(testTracker, testUi);
+        testCommand.execute(testTracker, testUi, budgetManager);
         assertEquals(0, testTracker.getIncomeSize());
         assertEquals(0, testTracker.getExpenseSize());
     }
@@ -57,32 +57,32 @@ public class CommandTest {
     @Test
     public void testTotalExpenseCommand() {
         TotalExpenseCommand testCommand = new TotalExpenseCommand();
-        testCommand.execute(testTracker, testUi);
+        testCommand.execute(testTracker, testUi, budgetManager);
         assertEquals(0, testTracker.getTotalIncome());
         Expense testExpense1 = new Expense("Bubble Tea", 4.80, ExpenseCategory.FOOD);
         Expense testExpense2 = new Expense("Chicken Rice", 3.00, ExpenseCategory.FOOD);
         AddExpenseCommand testCommand2 = new AddExpenseCommand(testExpense1);
         AddExpenseCommand testCommand3 = new AddExpenseCommand(testExpense2);
-        testCommand2.execute(testTracker, testUi);
-        testCommand3.execute(testTracker, testUi);
+        testCommand2.execute(testTracker, testUi, budgetManager);
+        testCommand3.execute(testTracker, testUi, budgetManager);
         TotalExpenseCommand testCommand4 = new TotalExpenseCommand();
-        testCommand4.execute(testTracker, testUi);
+        testCommand4.execute(testTracker, testUi, budgetManager);
         assertEquals(7.80, testTracker.getTotalExpense());
     }
 
     @Test
     public void testTotalIncomeCommand() {
         TotalIncomeCommand testCommand = new TotalIncomeCommand();
-        testCommand.execute(testTracker, testUi);
+        testCommand.execute(testTracker, testUi, budgetManager);
         assertEquals(0, testTracker.getTotalExpense());
         Income testIncome1 = new Income("Pocket Money", 100, IncomeCategory.ALLOWANCE);
         Income testIncome2 = new Income("Chicken Rice", 2500, IncomeCategory.SALARY);
         AddIncomeCommand testCommand2 = new AddIncomeCommand(testIncome1);
         AddIncomeCommand testCommand3 = new AddIncomeCommand(testIncome2);
-        testCommand2.execute(testTracker, testUi);
-        testCommand3.execute(testTracker, testUi);
+        testCommand2.execute(testTracker, testUi, budgetManager);
+        testCommand3.execute(testTracker, testUi, budgetManager);
         TotalIncomeCommand testCommand4 = new TotalIncomeCommand();
-        testCommand4.execute(testTracker, testUi);
+        testCommand4.execute(testTracker, testUi, budgetManager);
         assertEquals(2600, testTracker.getTotalIncome());
     }
 }
