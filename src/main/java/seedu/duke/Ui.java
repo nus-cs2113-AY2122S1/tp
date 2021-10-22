ff7a3a4f40c07fb800a71cdc8a679a2cac9eeac5
YipWayne <79963329+YipWayne@users.noreply.github.com>
2021-10-22 11:24:46 +0800

    Removed additional showAddTour command

--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -101,10 +101,6 @@ public class Ui {
         show(ADD_MESSAGE + "\n" + flight);
     }
 
-    public void showAddTour(Tour tour) {
-        show(ADD_MESSAGE + "\n" + tour);
-    }
-
     /**
      * Prints an exit message to the text Ui to acknowledge exiting the application.
      */
