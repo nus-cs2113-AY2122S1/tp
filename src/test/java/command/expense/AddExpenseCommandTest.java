aa8503323161171e17191873e86d5da13b9def35
tngjunwei <tngjunwei@gmail.com>
2021-10-22 11:37:48 +0800

    Add JUnit tests for v1.0 commands

---
+++ src/test/java/command/expense/AddExpenseCommandTest.java
@@ -0,0 +1,16 @@
+package command.expense;
+
+import command.CommandLineFactory;
+import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AddExpenseCommandTest {
+
+    @Test
+    public void TestAddExpense() {
+        CommandLine cmd = CommandLineFactory.getCmd();
+        int exitCode = cmd.execute("expense", "add", "Eat Lunch", "-v=20.99");
+        assertEquals(0, exitCode);
+    }
+}
