eb2c80cf8172fdd3c92c92bafccc4e45dbb3d677
YipWayne <79963329+YipWayne@users.noreply.github.com>
2021-10-22 18:06:27 +0800

    Update ListFlightCommand.java

--- src/main/java/seedu/duke/ListFlightCommand.java
+++ src/main/java/seedu/duke/ListFlightCommand.java
@@ -1,6 +1,6 @@
 package seedu.duke;
 
-public class ListFlightCommand extends Command{
+public class ListFlightCommand extends Command {
     public void execute() {
         ui.showListFlight(flights);
     }
