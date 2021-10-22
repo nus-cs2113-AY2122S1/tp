ccda72d38b6dde7b3cd72898c0887306eb0dd9c6
kishorkumar11 <h.kishorkumar7@gmail.com>
2021-10-22 18:56:42 +0800

    Add JunitTests for RecommendParser and RecommendCommand

--- src/test/java/seedu/duke/parser/RecommendWorkoutParserTest.java
+++ src/test/java/seedu/duke/parser/RecommendWorkoutParserTest.java
@@ -10,8 +10,9 @@ class RecommendWorkoutParserTest {
     @Test
     void parseInput_invalidUserInput_commandOfTypeIncorrectCommandReturned() {
         Parser p1 = new RecommendWorkoutParser("recommend ehbstbehg");
-        Parser p2 = new RecommendWorkoutParser("recommend");
+        Parser p2 = new RecommendWorkoutParser("recommend ");
         assertEquals(IncorrectCommand.class, p1.parseInput().getClass());
         assertEquals(IncorrectCommand.class, p2.parseInput().getClass());
     }
+
 }
