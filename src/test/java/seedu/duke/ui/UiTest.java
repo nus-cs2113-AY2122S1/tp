42496625d132dc77e980ea21b3de201c4cad7b59
Roycius <roycius@gmail.com>
2021-10-22 15:44:48 +0800

    Fix JUnit

--- src/test/java/seedu/duke/ui/UiTest.java
+++ src/test/java/seedu/duke/ui/UiTest.java
@@ -1,6 +1,7 @@
 package seedu.duke.ui;
 
 import org.junit.jupiter.api.Test;
+import seedu.duke.Duke;
 import seedu.duke.DukeException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -8,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class UiTest {
     @Test
     public void moduleInfo_invalidModuleCode_exceptionThrown() {
+        Duke duke = new Duke();
         Ui ui = new Ui();
         assertThrows(DukeException.class, () -> ui.printModuleInfo("hello"));
     }
