6833a14604a228adc301964405c9f7624b72dc70
Leyond <leyondlee@users.noreply.github.com>
2021-10-22 15:06:35 +0800

    Add ability to enable/disable logger and set default to disabled

--- src/main/java/taa/logger/TaaLogger.java
+++ src/main/java/taa/logger/TaaLogger.java
@@ -13,13 +13,20 @@ public class TaaLogger {
     public static final TaaLogger LOGGER = new TaaLogger();
 
     private final Logger logger;
+    private boolean isInit;
 
     private TaaLogger() {
         this.logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-        init();
+        this.isInit = false;
     }
 
     private void init() {
+        if (isInit) {
+            return;
+        }
+
+        isInit = true;
+
         FileHandler fileHandler;
         try {
             fileHandler = new FileHandler(LOG_FILENAME);
@@ -37,15 +44,40 @@ public class TaaLogger {
         }
     }
 
+    public static void setEnabled(boolean isEnabled) {
+        Logger logger = LOGGER.logger;
+
+        if (isEnabled) {
+            logger.setLevel(Level.INFO);
+        } else {
+            logger.setLevel(Level.OFF);
+        }
+    }
+
     public void logInfo(String message) {
+        if (logger.getLevel() == Level.OFF) {
+            return;
+        }
+
+        init();
         logger.log(Level.INFO, message);
     }
 
     public void logWarning(String message) {
+        if (logger.getLevel() == Level.OFF) {
+            return;
+        }
+
+        init();
         logger.log(Level.WARNING, message);
     }
 
     public void logSevere(String message) {
+        if (logger.getLevel() == Level.OFF) {
+            return;
+        }
+
+        init();
         logger.log(Level.SEVERE, message);
     }
 }
