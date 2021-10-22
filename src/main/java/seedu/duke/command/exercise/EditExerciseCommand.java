3130f9b50a1ee524c8ebb1e4f163d69ced84e6e1
kishorkumar11 <h.kishorkumar7@gmail.com>
2021-10-23 00:16:19 +0800

    Add JavaDoc

--- src/main/java/seedu/duke/command/exercise/EditExerciseCommand.java
+++ src/main/java/seedu/duke/command/exercise/EditExerciseCommand.java
@@ -7,6 +7,9 @@ import seedu.duke.exercises.Exercise;
 import seedu.duke.lists.WorkoutList;
 import seedu.duke.storage.Storage;
 
+/**
+ * To edit an existing exercise in a workout.
+ */
 public class EditExerciseCommand extends Command {
     public static final String COMMAND_WORD = "edit";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the exercise in the workout.\n"
@@ -25,6 +28,16 @@ public class EditExerciseCommand extends Command {
     private final int newReps;
     private final int newSets;
 
+    /**
+     * Instantiates object and sets workoutIndex and exerciseIndex along with the new exercise description, sets and
+     * reps.
+     *
+     * @param workoutIndex   is the index of Workout that the exercise is in
+     * @param exerciseIndex  is the index of exercise to edit
+     * @param newDescription is the new exercise description that the user wants to update with
+     * @param newReps        is the new reps that the user wants to update with
+     * @param newSets        is the new sets that the user wants to update with
+     */
     public EditExerciseCommand(int workoutIndex, int exerciseIndex, String newDescription, int newReps, int newSets) {
         this.workoutIndex = workoutIndex;
         this.exerciseIndex = exerciseIndex;
@@ -36,6 +49,13 @@ public class EditExerciseCommand extends Command {
         assert exerciseIndex >= 0;
     }
 
+    /**
+     * Executes edit exercise command to edit an exercise of given index from a particular workout.
+     *
+     * @param workouts is the list of Workouts
+     * @param storage  is a storage object
+     * @throws GetJackDException if there is an invalid index used or an error occurs within the storage
+     */
     @Override
     public CommandResult executeUserCommand(WorkoutList workouts, Storage storage) throws GetJackDException {
         try {
