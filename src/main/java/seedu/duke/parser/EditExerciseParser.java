718c823c0c978cabbfcaeb567371411154ba34d0
kishorkumar11 <h.kishorkumar7@gmail.com>
2021-10-23 00:09:14 +0800

    Update Ui test

--- src/main/java/seedu/duke/parser/EditExerciseParser.java
+++ src/main/java/seedu/duke/parser/EditExerciseParser.java
@@ -8,31 +8,30 @@ import seedu.duke.exception.GetJackDException;
 import java.util.ArrayList;
 
 public class EditExerciseParser extends Parser {
-    private int exerciseIndex;
     private static int workoutIndex;
-
-    public static int getWorkoutIndex() {
-        return workoutIndex;
-    }
 
     public EditExerciseParser(String userInputString) {
         this.userInputString = userInputString;
     }
 
+    public static int getWorkoutIndex() {
+        return workoutIndex;
+    }
+
     private Command prepareEditExercise(String commandArgs) {
         try {
-            int [] indices = parseWorkoutAndExerciseIndex(commandArgs);
-            exerciseIndex = indices[0];
-            workoutIndex =  indices[1];
+            int[] indices = parseWorkoutAndExerciseIndex(commandArgs);
+            int exerciseIndex = indices[0];
+            workoutIndex = indices[1];
 
             // String excluding the exerciseIndex and workoutIndex
-            String [] params = commandArgs.split(PARAMETER_SEPARATOR);
+            String[] params = commandArgs.split(PARAMETER_SEPARATOR);
             ArrayList<String> paramsWithoutIndices = new ArrayList<>();
-            for(int i = 2; i < params.length; i++) {
+            for (int i = 2; i < params.length; i++) {
                 paramsWithoutIndices.add(params[i]);
             }
-            String newnewArgs = String.join(PARAMETER_SEPARATOR, paramsWithoutIndices);
-            String newArgs = commandArgs.substring(6).trim();
+
+            String newArgs = String.join(PARAMETER_SEPARATOR, paramsWithoutIndices);
             String[] exerciseArgs = getExerciseArgs(newArgs, true);
 
             String newDescription = exerciseArgs[0].trim();
