4e2add6fd66ca997e2160036ed27052429edde88
Ng Andre <ng.andr3@gmail.com>
2021-10-22 14:56:36 +0800

    Update constant for search parser

--- src/main/java/seedu/parser/SearchContactParser.java
+++ src/main/java/seedu/parser/SearchContactParser.java
@@ -4,22 +4,19 @@ import seedu.contact.DetailType;
 import seedu.exception.InvalidFlagException;
 import seedu.exception.MissingArgException;
 
-import java.util.Locale;
-
 import static seedu.parser.ContactParser.FLAG_INDEX_IN_DETAILS;
 import static seedu.parser.ContactParser.NUMBER_OF_DETAILS;
-import static seedu.parser.ContactParser.NUMBER_OF_SEARCH_ARGS;
 
 public class SearchContactParser implements ContactDetails {
     //return only search query as a String
     public String parseSearchQuery(String searchInput) throws MissingArgException {
         String[] destructuredInputs = searchInput.trim().split(" ", NUMBER_OF_DETAILS);
         if (searchInput.trim().charAt(FLAG_INDEX_IN_DETAILS) == '-') { //if there is a flag, remove it
-            if (destructuredInputs.length < 2) { //input starts with - but only flag or weird input
+            if (destructuredInputs.length < NUMBER_OF_DETAILS) { //input starts with - but only flag or weird input
                 throw new MissingArgException();
             }
             //contains flag and search query
-            return destructuredInputs[1].toLowerCase().trim();
+            return destructuredInputs[NUMBER_OF_DETAILS - 1].toLowerCase().trim();
         }
         //assert no flag exists therefore searchInput is the search query
         return searchInput;
@@ -31,10 +28,10 @@ public class SearchContactParser implements ContactDetails {
             //if a flag is specified then parse the flag
             //handles inputs "search -g" which is missing search query
             String[] destructuredInputs = searchInput.trim().split(" ", NUMBER_OF_DETAILS);
-            if (destructuredInputs.length < 2) { //input starts with - but only flag or weird input
+            if (destructuredInputs.length < NUMBER_OF_DETAILS) { //input starts with - but only flag or weird input
                 throw new MissingArgException();
             }
-            String flag = destructuredInputs[0].trim().substring(1);
+            String flag = destructuredInputs[FLAG_INDEX_IN_DETAILS].trim().substring(1);
             return getIndexToStore(flag);
         }
         //no flag is specified
@@ -44,11 +41,11 @@ public class SearchContactParser implements ContactDetails {
     public String getSearchInput(String userInput) throws MissingArgException {
         //remove the search command
         String[] destructuredInputs = userInput.trim().split(" ", NUMBER_OF_DETAILS);
-        if (destructuredInputs.length < 2) { //no arguments specified, only "search"
+        if (destructuredInputs.length < NUMBER_OF_DETAILS) { //no arguments specified, only "search"
             throw new MissingArgException();
         }
-        assert destructuredInputs.length == 2;
-        return destructuredInputs[1];
+        assert destructuredInputs.length == NUMBER_OF_DETAILS;
+        return destructuredInputs[NUMBER_OF_DETAILS - 1];
     }
 
 }
