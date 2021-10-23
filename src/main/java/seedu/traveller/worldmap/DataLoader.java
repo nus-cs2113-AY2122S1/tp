c0e3ec7fd94b287ff9faa668458165d21c8b891c
gavienwz <e0544185@u.nus.edu>
2021-10-23 17:57:39 +0800

    Add defense to data loader Flights.txt now hardcoded to 5 countries. Country codes must be length of 3 Distances must be positive numbers Separator is "|" and cannot be first/last symbol

--- src/main/java/seedu/traveller/worldmap/DataLoader.java
+++ src/main/java/seedu/traveller/worldmap/DataLoader.java
@@ -1,5 +1,6 @@
 package seedu.traveller.worldmap;
 
+import seedu.traveller.exceptions.IllegalFlightFileException;
 import seedu.traveller.worldmap.exceptions.WorldMapException;
 import seedu.traveller.worldmap.exceptions.FlightDataNotFoundException;
 
@@ -13,33 +14,47 @@ import java.util.logging.Logger;
 
 public class DataLoader {
     private static final Logger logger = Logger.getLogger(DataLoader.class.getName());
-    private final int numberOfCities = 5;
+    private final int numberOfCountries = 5;
     private final String filePath = "./flightData/flights.txt";
     private final String separator = "\\|";
 
-    protected void loadCountries(String[] countryCodes, GraphList graphList) {
+    protected void loadCountries(String[] countryCodes, GraphList graphList) throws IllegalFlightFileException {
         logger.log(Level.INFO, "Loading countries: " + Arrays.toString(countryCodes));
-        for (int j = 0; j < numberOfCities; j++) {
+        assert countryCodes.length == numberOfCountries;
+        for (int j = 0; j < numberOfCountries; j++) {
             String countryCode = countryCodes[j];
+            if (countryCode.length() != 3) {
+                throw new IllegalFlightFileException();
+            }
+            assert countryCode.length() == 3;
             int countryIndex = j + 1;
             Country v = new Country(countryCode, countryIndex);
             graphList.addVertex(v);
         }
     }
 
-    protected void loadDistances(String[] rawDistances, GraphList graphList) {
+    protected void loadDistances(String[] rawDistances, GraphList graphList) throws IllegalFlightFileException {
         logger.log(Level.INFO, "Loading distances: " + Arrays.toString(rawDistances));
         for (int k = 0; k < rawDistances.length; k++) {
-            double distance = Double.parseDouble(rawDistances[k]);
+            double distance;
+            try {
+                distance = Double.parseDouble(rawDistances[k]);
+            } catch (NumberFormatException e){
+                throw new IllegalFlightFileException();
+            }
+            if (distance < 0) {
+                throw new IllegalFlightFileException();
+            }
             Country sourceCountry = graphList.getVertexArray().get(k);
             Country destinationCountry = graphList.getVertexArray().get(rawDistances.length);
             if (distance != 0) {
+                assert distance > 0;
                 graphList.createEdge(distance, destinationCountry, sourceCountry);
             }
         }
     }
 
-    public GraphList readData() throws WorldMapException {
+    public GraphList readData() throws WorldMapException{
         logger.setLevel(Level.INFO);
         logger.log(Level.INFO, "Reading data from " + filePath);
         File data = new File(filePath);
@@ -53,14 +68,15 @@ public class DataLoader {
         GraphList graphList = new GraphList();
         String[] rawInput;
         while (scanner.hasNext()) {
-            for (int i = 0; i < numberOfCities; i++) {
-                rawInput = scanner.nextLine().split(separator);
-
-                // Reading first line of country codes
+            for (int i = 0; i < numberOfCountries; i++) {
                 if (i == 0) {
+                    rawInput = scanner.nextLine().split(separator, numberOfCountries);
+                    assert rawInput.length == numberOfCountries;
                     loadCountries(rawInput, graphList);
                     continue;
                 }
+                rawInput = scanner.nextLine().split(separator, i);
+                assert rawInput.length == i;
                 loadDistances(rawInput, graphList);
             }
         }
