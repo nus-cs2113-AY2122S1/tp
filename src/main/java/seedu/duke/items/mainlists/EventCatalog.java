6042266d9272ef82f6cf08b42fab3c27439f64db
Alvinlj00 <limjunalvin@gmail.com>
2021-10-23 18:35:31 +0800

    Add Member and MemberRoster classes

---
+++ src/main/java/seedu/duke/items/mainlists/EventCatalog.java
@@ -0,0 +1,47 @@
+package seedu.duke.items.mainlists;
+
+import seedu.duke.items.Event;
+import seedu.duke.items.Item;
+
+import java.util.ArrayList;
+
+public class EventCatalog extends ArrayList<Event> {
+
+    private static ArrayList<Event> eventCatalog;
+    private static EventCatalog theOne = null;
+
+    private EventCatalog() {
+        eventCatalog = new ArrayList<>();
+    }
+
+    public static EventCatalog getInstance() {
+        if (theOne == null) {
+            theOne = new EventCatalog();
+        }
+        return theOne;
+    }
+
+    private static <T extends Item> void swap(int i, ArrayList<T> list) {
+        T t;
+        t = list.get(i);
+        list.set(i, list.get(i + 1));
+        list.set(i + 1, t);
+    }
+
+    public static <T extends Item> void bubbleSortItems(ArrayList<T> list) {
+        for (int j = 0; j < list.size() - 1; j++) {
+            for (int i = 0; i < list.size() - j - 1; i++) {
+                if (list.get(i + 1).getDateTime().isBefore(list.get(i).getDateTime())) {
+                    swap(i, list);
+                }
+            }
+        }
+    }
+
+    public void sortCatalog() {
+        bubbleSortItems(eventCatalog);
+        for (Event event : eventCatalog) {
+            bubbleSortItems(event.getTaskList());
+        }
+    }
+}
