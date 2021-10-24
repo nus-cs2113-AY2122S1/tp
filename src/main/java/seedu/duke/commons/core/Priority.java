e93d11c256d2e0defd91a5448470795a4a3007c4
ptejasv <e0564194@u.nus.edu>
2021-10-24 15:04:24 +0800

    Add comments to relevant functions

--- src/main/java/seedu/duke/commons/core/Priority.java
+++ src/main/java/seedu/duke/commons/core/Priority.java
@@ -3,6 +3,12 @@ package seedu.duke.commons.core;
 public enum Priority {
     LOW, MEDIUM, HIGH, NONE;
 
+    /**
+     * Determines if the input parameter is a valid priority type.
+     *
+     * @param param the input parameter
+     * @return true if the input is valid, false otherwise
+     */
     public static boolean is(String param) {
         for (Priority priority : Priority.values()) {
             if (param.equalsIgnoreCase(priority.toString())) {
@@ -12,6 +18,12 @@ public enum Priority {
         return false;
     }
 
+    /**
+     * Returns a properly capitalised priority type
+     *
+     * @param param the priority with any capitalisation
+     * @return either "Low", "Medium", "High" or "None"
+     */
     public static String toProper(String param) {
         return param.substring(0, 1).toUpperCase() + param.substring(1).toLowerCase();
     }
