c672dd5669cd3f424355211334a8b14dae453e44
Tan Hui En <e0559373@u.nus.edu>
2021-10-24 18:53:04 +0800

    update storage file when goal is deleted

--- src/main/java/happybit/command/DeleteGoalCommand.java
+++ src/main/java/happybit/command/DeleteGoalCommand.java
@@ -1,6 +1,7 @@
 package happybit.command;
 
 import happybit.exception.HaBitCommandException;
+import happybit.exception.HaBitStorageException;
 import happybit.goal.GoalList;
 import happybit.storage.Storage;
 import happybit.ui.PrintManager;
@@ -16,6 +17,12 @@ public class DeleteGoalCommand extends DeleteCommand {
     @Override
     public void runCommand(GoalList goalList, PrintManager printManager, Storage storage) throws HaBitCommandException {
         goalList.deleteGoal(goalIndex, printManager);
+
+        try {
+            storage.export(goalList.getGoalList());
+        } catch (HaBitStorageException e) {
+            printManager.showError(e.getMessage());
+        }
     }
 
 }
