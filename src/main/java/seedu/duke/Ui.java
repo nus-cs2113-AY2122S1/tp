998c39968f96f2767c5f4f3eda5237a432bf8a94
Joshua Lee <joshualeeky@u.nus.edu>
2021-10-25 00:07:04 +0800

    Resolved checkstyle error

--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -279,11 +279,11 @@ public class Ui {
         System.out.println("The save file could not be created. Exiting the program now...");
     }
 
-    public static void printInvalidPerson(String name){
+    public static void printInvalidPerson(String name) {
         System.out.println(name + " is not part of the trip. "
                 + "Please enter the names of the people who are involved in this expense again, separated by a comma.");
         System.out.println("These are the names of the people who are part of the trip:");
-        for (Person person : Storage.getOpenTrip().getListOfPersons()){
+        for (Person person : Storage.getOpenTrip().getListOfPersons()) {
             System.out.println(person.getName());
         }
     }
