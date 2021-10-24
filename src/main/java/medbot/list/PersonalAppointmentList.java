8853bb739de86273deced1f5c2fe6009d00912ce
Justin Wong <justinfidelis@gmail.com>
2021-10-24 11:57:09 +0800

    Cleaned some PersonalAppointmentList methods

--- src/main/java/medbot/list/PersonalAppointmentList.java
+++ src/main/java/medbot/list/PersonalAppointmentList.java
@@ -1,7 +1,5 @@
 package medbot.list;
 
-import java.time.LocalDateTime;
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.NavigableSet;
 import java.util.TreeSet;
@@ -12,9 +10,9 @@ import medbot.exceptions.MedBotException;
 public class PersonalAppointmentList {
     private static final String ERROR_APPOINTMENT_ID_NOT_SET = "Appointment ID is not set.";
     private static final String ERROR_ADD_APPOINTMENT_CLASH = "New appointment clashes with another appointment.";
-    private static final String ERROR_EDIT_APPOINTMENT_CLASH = "New appointment time clashes with another appointment.";
+    private static final String END_LINE = System.lineSeparator();
 
-    protected NavigableSet<Appointment> appointments = new TreeSet<>((o1, o2) -> {
+    private final NavigableSet<Appointment> appointments = new TreeSet<>((o1, o2) -> {
         if (o1.getDateTimeCode() > o2.getDateTimeCode()) {
             return 1;
         } else if (o1.getDateTimeCode() < o2.getDateTimeCode()) {
@@ -30,7 +28,6 @@ public class PersonalAppointmentList {
 
     public void addAppointment(Appointment appointment) throws MedBotException {
         int appointmentId = appointment.getAppointmentId();
-        int dateTimeCode = appointment.getDateTimeCode();
         if (appointmentId == 0) {
             throw new MedBotException(ERROR_APPOINTMENT_ID_NOT_SET);
         }
@@ -42,10 +39,8 @@ public class PersonalAppointmentList {
     }
 
     public int getAppointmentId(int dateTimeCode) {
-        Iterator<Appointment> it = appointments.iterator();
-        while (it.hasNext()) {
-            Appointment appointment = it.next();
-            if (appointment.getDateTimeCode() == dateTimeCode && appointment != null) {
+        for (Appointment appointment : appointments) {
+            if (appointment != null && appointment.getDateTimeCode() == dateTimeCode) {
                 return appointment.getAppointmentId();
             }
         }
@@ -64,24 +59,14 @@ public class PersonalAppointmentList {
         throw new MedBotException(getAppointmentNotFoundErrorMessage(dateTimeCode));
     }
 
-    private void mergeAppointmentDataByTime(Appointment oldAppointment, Appointment newAppointment) {
-        if (oldAppointment.getDateTimeCode() != newAppointment.getDateTimeCode()
-                && newAppointment.getDateTimeCode() == 0) {
-            oldAppointment.mergeAppointmentData(newAppointment);
-        }
-
-    }
-
     private String getAppointmentNotFoundErrorMessage(int dateTimeCode) {
         return "No appointment at : " + Appointment.getDateTimeString(dateTimeCode) + "found.";
     }
 
     public String listAppointments() {
         String output = "";
-        Iterator<Appointment> it = appointments.iterator();
-        while (it.hasNext()) {
-            output += it.next();
-            output += "\n";
+        for (Appointment appointment : appointments) {
+            output += appointment + END_LINE;
         }
         return output;
     }
