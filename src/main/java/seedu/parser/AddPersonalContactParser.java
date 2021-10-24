72f60ae139fcd211e9751f716e7b9ae3d8c72eab
lezong <lezongmun@gmail.com>
2021-10-25 01:28:05 +0800

    Remove if branch in addPersonalContactParser

--- src/main/java/seedu/parser/AddPersonalContactParser.java
+++ src/main/java/seedu/parser/AddPersonalContactParser.java
@@ -23,27 +23,23 @@ public class AddPersonalContactParser extends RegexParser {
         TextUi.welcomeMessage();
         parsePersonalName(true);
         TextUi.greetingMessage(personalContact);
-        String userConfirmation = UserInputTextUi.getUserConfirmation();
-        if (userConfirmation.isBlank()) {
-            parsePersonalGithubUsername();
-            parsePersonalTelegramUsername();
-            parsePersonalTwitterUsername();
-            parsePersonalEmailAddress();
-            parsePersonalLinkedinUsername();
-            TextUi.finishSetUpMessage();
-        }
+        UserInputTextUi.getUserConfirmation();
+        parsePersonalGithubUsername();
+        parsePersonalTelegramUsername();
+        parsePersonalTwitterUsername();
+        parsePersonalEmailAddress();
+        parsePersonalLinkedinUsername();
+        TextUi.finishSetUpMessage();
     }
 
     public void recallPersonalDetails() {
         String userConfirmation = UserInputTextUi.getUserConfirmation();
-        if (userConfirmation.isBlank()) {
-            parsePersonalName(false);
-            parsePersonalGithubUsername();
-            parsePersonalTelegramUsername();
-            parsePersonalTwitterUsername();
-            parsePersonalEmailAddress();
-            parsePersonalLinkedinUsername();
-        }
+        parsePersonalName(false);
+        parsePersonalGithubUsername();
+        parsePersonalTelegramUsername();
+        parsePersonalTwitterUsername();
+        parsePersonalEmailAddress();
+        parsePersonalLinkedinUsername();
     }
 
     private void parsePersonalName(boolean isFirstTime) {
