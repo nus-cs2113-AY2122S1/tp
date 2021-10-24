7d5c4462aeef5f3bf0de199ff1145a0f71979f63
Tan Hui En <e0559373@u.nus.edu>
2021-10-24 19:01:39 +0800

    when update goal name, export to storage file

--- src/main/java/happybit/command/UpdateGoalNameCommand.java
+++ src/main/java/happybit/command/UpdateGoalNameCommand.java
@@ -1,6 +1,7 @@
 package happybit.command;
 
 import happybit.exception.HaBitCommandException;
+import happybit.exception.HaBitStorageException;
 import happybit.goal.GoalList;
 import happybit.storage.Storage;
 import happybit.ui.PrintManager;
@@ -18,5 +19,11 @@ public class UpdateGoalNameCommand extends UpdateCommand {
     @Override
     public void runCommand(GoalList goalList, PrintManager printManager, Storage storage) throws HaBitCommandException {
         goalList.updateGoalName(goalIndex, goalName, printManager);
+
+        try {
+            storage.export(goalList.getGoalList());
+        } catch (HaBitStorageException e) {
+            printManager.showError(e.getMessage());
+        }
     }
 }
