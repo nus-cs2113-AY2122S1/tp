7bc725f90f3bbe398199f6b0d6b5a04cd4ab58e5
Bobowoo2468 <woobt123@gmail.com>
2021-10-24 20:59:21 +0800

    Edit to clear Java CI tests

--- src/main/java/seedu/duke/ClientPackage.java
+++ src/main/java/seedu/duke/ClientPackage.java
@@ -6,12 +6,6 @@ public class ClientPackage {
     private final Flight flight;
 
     public ClientPackage(Client client, Tour tour, Flight flight, ClientPackageList clientPackages) {
-//        if (clientPackages.getPackageCount() > 0) {
-//            System.out.println(clientPackages.get(0));
-//        }
-//        if (clientPackages.getPackageCount() > 1) {
-//            System.out.println(clientPackages.get(1));
-//        }
         this.client = client;
         this.tour = tour;
         this.flight = flight;
