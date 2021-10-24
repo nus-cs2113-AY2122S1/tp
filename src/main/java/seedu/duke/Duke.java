40a5c27b0be4df977c2090b76f2e67dfb1d583d3
xuanlc113 <xuan.lc113@gmail.com>
2021-10-24 20:36:49 +0800

    add print for timetable info

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -24,7 +24,6 @@ public class Duke {
     private static Profile profileInUse;
 
     public static void main(String[] args) {
-        // assert false : "dummy";
         new Duke().setup();
     }
 
