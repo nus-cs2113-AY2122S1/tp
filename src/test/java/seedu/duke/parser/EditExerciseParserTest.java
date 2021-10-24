edd0991ea3e67168b738cccd01dd39cfc0285e5e
JmattChiam <joelmchiam@gmail.com>
2021-10-24 11:59:45 +0800

    Add JUnit test for EditExerciseParser

---
+++ src/test/java/seedu/duke/parser/EditExerciseParserTest.java
@@ -0,0 +1,49 @@
+package seedu.duke.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.command.Command;
+import seedu.duke.command.exercise.EditExerciseCommand;
+import seedu.duke.command.misc.IncorrectCommand;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class EditExerciseParserTest {
+    private EditExerciseParser parser;
+    private Command result;
+
+    @Test
+    void parseInput_validInput_returnsEditExerciseCommand() {
+        parser = new EditExerciseParser("edit 1, 2, testExercise, 5 20");
+        result = parser.parseInput();
+        assertTrue(result instanceof EditExerciseCommand);
+    }
+
+    @Test
+    void parseInput_invalidExerciseOrWorkoutIndex_returnsIncorrectCommand() {
+        parser = new EditExerciseParser("edit testExercise, 5 20");
+        result = parser.parseInput();
+        assertTrue(result instanceof IncorrectCommand);
+        parser = new EditExerciseParser("edit 1, testExercise, 5 20");
+        result = parser.parseInput();
+        assertTrue(result instanceof IncorrectCommand);
+        parser = new EditExerciseParser("edit , testExercise, 5 20");
+        result = parser.parseInput();
+        assertTrue(result instanceof IncorrectCommand);
+        parser = new EditExerciseParser("edit a, b, testExercise, 5 20");
+        result = parser.parseInput();
+        assertTrue(result instanceof IncorrectCommand);
+    }
+
+    @Test
+    void parseInput_invalidSetsAndReps_returnsIncorrectCommand() {
+        parser = new EditExerciseParser("edit 1, 2, testExercise,   ");
+        result = parser.parseInput();
+        assertTrue(result instanceof IncorrectCommand);
+        parser = new EditExerciseParser("edit 1, 2, testExercise, a b");
+        result = parser.parseInput();
+        assertTrue(result instanceof IncorrectCommand);
+        parser = new EditExerciseParser("edit 1, 2, testExercise, 5");
+        result = parser.parseInput();
+        assertTrue(result instanceof IncorrectCommand);
+    }
+}
