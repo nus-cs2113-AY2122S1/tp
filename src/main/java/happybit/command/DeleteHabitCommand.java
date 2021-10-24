92e9897516f66b3eb80f5e0ebcf1a69be9208086
Tan Hui En <e0559373@u.nus.edu>
2021-10-24 18:56:39 +0800

    update storage file when habit deleted

--- src/main/java/happybit/command/DeleteHabitCommand.java
+++ src/main/java/happybit/command/DeleteHabitCommand.java
@@ -1,6 +1,7 @@
 package happybit.command;
 
 import happybit.exception.HaBitCommandException;
+import happybit.exception.HaBitStorageException;
 import happybit.goal.GoalList;
 import happybit.storage.Storage;
 import happybit.ui.PrintManager;
@@ -18,6 +19,12 @@ public class DeleteHabitCommand extends DeleteCommand {
     @Override
     public void runCommand(GoalList goalList, PrintManager printManager, Storage storage) throws HaBitCommandException {
         goalList.deleteHabitFromGoal(goalIndex, habitIndex, printManager);
+
+        try {
+            storage.export(goalList.getGoalList());
+        } catch (HaBitStorageException e) {
+            printManager.showError(e.getMessage());
+        }
     }
 
 }
