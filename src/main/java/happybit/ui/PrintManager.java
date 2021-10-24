310f0b333ac78921bce008045c3221530f25cd70
STAung07 <swann1999@gmail.com>
2021-10-24 22:15:03 +0800

    standardised view command flags with Daren's

--- src/main/java/happybit/ui/PrintManager.java
+++ src/main/java/happybit/ui/PrintManager.java
@@ -3,28 +3,31 @@ package happybit.ui;
 import happybit.goal.Goal;
 import happybit.habit.Habit;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class PrintManager {
     private static final String COMMAND_LIST_GREETING =
             "Hello! These are all the possible commands for this habit tracker :)"
                     + " (flags within {} brackets are optional)";
     private static final String SET_GOAL_COMMAND =
-            "1. set a goal: set <goal name> {-<goal type> /<start date>} /<end date>";
+            "- set a goal: set n/<goal name> { t/<goal type> s/<start date> } e/<end date>";
     private static final String GOAL_TYPE_INFO =
-            "   -> Goal types include: default, sleep, food, exercise and study";
+            "   -> Goal types include: default[df], sleep[sl], food[fd], exercise[ex] and study[sd]";
     private static final String REMOVE_GOAL_COMMAND =
-            "2. remove a goal: remove <goal index>";
+            "- remove a goal: remove g/<goal index>";
     private static final String LIST_GOAL_COMMAND =
-            "3. list all goals for that habit: list";
+            "- list all goals for that habit: list";
     private static final String ADD_HABIT_COMMAND =
-            "4. add a habit to a goal: add <goal index> <habit name>";
+            "- add a habit to a goal: add g/<goal index> n/<habit name> i/<interval>";
     private static final String DELETE_HABIT_COMMAND =
-            "5. delete a habit from a goal: delete <goal index> <habit index>";
+            "- delete a habit from a goal: delete g/<goal index> h/<habit index>";
     private static final String DONE_HABIT_COMMAND =
-            "6. indicate a habit as done: done <goal index> <habit index>";
+            "- indicate a habit as done: done g/<goal index> h/<habit index>";
     private static final String LIST_HABIT_COMMAND =
-            "7. View all the habits user has under a goal: view <goal index>";
+            "- View all the habits user has under a goal: view g/<goal index>";
     private static final String BYE_COMMAND = "8. Exit habit tracker program: bye";
 
     private static final String NEWLINE = System.lineSeparator();
@@ -66,12 +69,15 @@ public class PrintManager {
         assert (numOfHabits > 0) : "List cannot be empty here";
         System.out.println("Here are your " + numOfHabits + " habit(s) under the goal \""
                 + goalDescription + "\".");
-        String prefix = "[ ]";
         for (Habit habit : habits) {
+            String prefix = "[ ]";
             System.out.print(index + ". ");
             String intervalPrint = "(every " + habit.getInterval() + " days)";
-            String lastHabitDatePrint = habit.getHabitDateString();
-            String nextHabitDatePrint = habit.getNextDateString();
+            Date lastHabitDate = habit.getHabitDate();
+            Date nextHabitDate = habit.getNextDate();
+            SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
+            String lastHabitDatePrint = dateFormatter.format(lastHabitDate);
+            String nextHabitDatePrint = dateFormatter.format(nextHabitDate);
             if (habit.getDone()) {
                 prefix = "[X]";
             }
