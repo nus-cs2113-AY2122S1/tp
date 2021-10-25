5012e1d751437e07f874429673a5d280e062ce63
yeezao <liangyuzhao1999@gmail.com>
2021-10-26 02:46:04 +0800

    fix checkstyle errors

--- src/test/java/seedu/duke/FileTest.java
+++ src/test/java/seedu/duke/FileTest.java
@@ -14,7 +14,10 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FileTest {
 
