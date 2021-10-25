24768d451fe9c8ddcfe55f0471f53e7a6c4e5cb0
Joshua Lee <joshualeeky@u.nus.edu>
2021-10-25 21:28:02 +0800

    Added more currencies

--- src/main/java/seedu/duke/Storage.java
+++ src/main/java/seedu/duke/Storage.java
@@ -39,16 +39,37 @@ public class Storage {
             put("SGD", new String[] {"$", "%.02f"});
             put("AUD", new String[] {"$", "%.02f"});
             put("CAD", new String[] {"$", "%.02f"});
+            put("NZD", new String[] {"$", "%.02f"});
             put("EUR", new String[] {"€", "%.02f"});
             put("GBP", new String[] {"£", "%.02f"});
             put("MYR", new String[] {"RM", "%.02f"});
             put("HKD", new String[] {"$", "%.02f"});
             put("THB", new String[] {"฿", "%.02f"});
+            put("RUB", new String[] {"₽", "%.02f"});
+            put("ZAR", new String[] {"R", "%.02f"});
+            put("TRY", new String[] {"₺", "%.02f"});
+            put("BRL", new String[] {"R$", "%.02f"});
+            put("DKK", new String[] {"Kr.", "%.02f"});
+            put("PLN", new String[] {"zł", "%.02f"});
+            put("ILS", new String[] {"₪", "%.02f"});
+            put("SAR", new String[] {"SR", "%.02f"});
             put("CNY", new String[] {"¥", "%.0f"});
             put("JPY", new String[] {"¥", "%.0f"});
             put("KRW", new String[] {"₩", "%.0f"});
             put("IDR", new String[] {"Rp", "%.0f"});
             put("INR", new String[] {"Rs", "%.0f"});
+            put("CHF", new String[] {"SFr.", "%.0f"});
+            put("SEK", new String[] {"kr", "%.0f"});
+            put("NOK", new String[] {"kr", "%.0f"});
+            put("MXN", new String[] {"$", "%.0f"});
+            put("TWD", new String[] {"NT$", "%.0f"});
+            put("HUF", new String[] {"Ft", "%.0f"});
+            put("CZK", new String[] {"Kč", "%.0f"});
+            put("CLP", new String[] {"$", "%.0f"});
+            put("PHP", new String[] {"₱", "%.0f"});
+            put("AED", new String[] {"د.إ", "%.0f"});
+            put("COP", new String[] {"$", "%.0f"});
+            put("RON", new String[] {"lei", "%.0f"});
         }};
 
     public static HashMap<String, String[]> getAvailableCurrency() {
