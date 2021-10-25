7dac6c03e2e77bbd760b14e3b14649e92a0347a3
Renzo Canare <renzo_canare@hotmail.com>
2021-10-26 02:31:33 +0800

    Refactor ClearScreen class code

--- src/main/java/seedu/duke/ClearScreen.java
+++ src/main/java/seedu/duke/ClearScreen.java
@@ -8,15 +8,17 @@ public class ClearScreen {
     public static void clearConsole() {
         try {
             // Get current operating system
-            String operatingSystem = System.getProperty("os.name");
+            String getOS = System.getProperty("os.name");
 
-            if (operatingSystem.contains("Windows")) {
+            if (getOS.contains("Windows")) {
                 // Try clear for Windows
+                // "/c" - execute string as command, "cls" -  Clear terminal
                 ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "cls");
                 Process startProcess = pb.inheritIO().start();
                 startProcess.waitFor();
             } else {
                 // Try clear for MacOS/Linux
+                // "clear" - Clear terminal
                 ProcessBuilder pb = new ProcessBuilder("clear");
                 Process startProcess = pb.inheritIO().start();
                 startProcess.waitFor();
