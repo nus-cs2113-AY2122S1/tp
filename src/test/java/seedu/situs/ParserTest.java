35cb7b35ea872a6055098ae610e2e71f3b823860
datn02 <ducanh02.work@gmail.com>
2021-10-26 10:20:11 +0800

    Update parser test

--- src/test/java/seedu/situs/ParserTest.java
+++ src/test/java/seedu/situs/ParserTest.java
@@ -82,11 +82,15 @@ public class ParserTest {
 
     @Test
     public void parseUpdateCommand_ingredientNotFound_success() throws SitusException {
-        String inputString1 = "add n/ apple a/ 300 e/ 12/10/2021";
-        String inputString2 = "update n/ go out have fun a/ 12.3 e/ 12/10/2021";
-        Parser.parse(inputString1);
-        String resultMsg = Parser.parse(inputString2);
-        assertEquals("Ingredient not found!", resultMsg);
+        try {
+            String inputString1 = "add n/ apple a/ 300 e/ 12/10/2021";
+            String inputString2 = "update n/ go out have fun a/ 12.3 e/ 12/10/2021";
+            Parser.parse(inputString1);
+            String resultMsg = Parser.parse(inputString2);
+            fail();
+        } catch (SitusException e) {
+            assertEquals("Ingredient not found!", e.getMessage());
+        }
     }
 
     @Test
