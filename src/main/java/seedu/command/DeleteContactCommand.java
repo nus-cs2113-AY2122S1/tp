fe3ac2dd4aece28e48caf7f7f258a94f14335d5e
Ng Andre <ng.andr3@gmail.com>
2021-10-26 18:49:20 +0800

    Implement delete fields for DeleteContactCommand

--- src/main/java/seedu/command/DeleteContactCommand.java
+++ src/main/java/seedu/command/DeleteContactCommand.java
@@ -19,9 +19,11 @@ public class DeleteContactCommand extends Command {
     public static final int EMAIL_INDEX = 5;
 
     private final int contactIndex;
+    private final boolean[] deleteDetails;
 
-    public DeleteContactCommand(int contactIndex) {
+    public DeleteContactCommand(int contactIndex, boolean[] deleteDetails) {
         this.contactIndex = contactIndex;
+        this.deleteDetails = deleteDetails;
     }
 
     public int getContactIndex() {
@@ -61,12 +63,42 @@ public class DeleteContactCommand extends Command {
         }
     }
 
+    private void deleteFields() {
+        Contact deletedContact = IndexParser.getContactFromIndex(contactIndex, contactList);
+        assert contactIndex >= 0 && contactIndex < contactList.getListSize();
+        TextUi.confirmDeleteFieldMessage();
+        String userConfirmation = UserInputTextUi.getUserConfirmation();
+        if (userConfirmation.equalsIgnoreCase(ACKNOWLEDGE_DELETE)) {
+            if (deleteDetails[GITHUB_INDEX]) {
+                deletedContact.setGithub(null);
+            }
+            if (deleteDetails[LINKEDIN_INDEX]) {
+                deletedContact.setLinkedin(null);
+            }
+            if (deleteDetails[TELEGRAM_INDEX]) {
+                deletedContact.setTelegram(null);
+            }
+            if (deleteDetails[TWITTER_INDEX]) {
+                deletedContact.setTwitter(null);
+            }
+            if (deleteDetails[EMAIL_INDEX]) {
+                deletedContact.setEmail(null);
+            }
+            TextUi.deleteFieldsMessage();
+        } else {
+            assert !userConfirmation.equalsIgnoreCase(ACKNOWLEDGE_DELETE);
+            TextUi.cancelDeleteContactMessage();
+        }
+    }
+
     public void execute() {
         try {
             if (contactIndex == DELETE_ALL_CONTACTS) {
                 deleteAllContacts();
-            } else {
+            } else if (deleteDetails[6]) { //delete entire contact
                 deleteSelectedContact();
+            } else {
+                deleteFields();
             }
         } catch (IndexOutOfBoundsException e) {
             ExceptionTextUi.numOutOfRangeMessage(contactList.getListSize());
