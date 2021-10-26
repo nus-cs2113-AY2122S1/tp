c19f7c0978092ae9ede397e81a8526c847df0376
dyahnafisah <59493341+dyahnafisah@users.noreply.github.com>
2021-10-27 07:55:01 +0800

    resolve merge conflict

--- src/main/java/seedu/duke/common/Messages.java
+++ src/main/java/seedu/duke/common/Messages.java
@@ -20,12 +20,24 @@ public class Messages {
     public static final String WELCOME_MESSAGE = "Welcome to libmgr";
     public static final String EXIT_MESSAGE = "See you soon!";
     public static final String UNKNOWN_MESSAGE = "Sorry, I don't understand what you mean";
+    public static final String INVALID_VALUES = "  (!) Invalid/missing values";
+
+    // Add Commands
+    public static final String WARN_ADDITIONAL_ARGS = "  (*) Additional arguments detected, ignoring them";
+    public static final String ADD_DUPLICATE_ID = "  (!) ID cannot be a duplicate!";
+
     // List
     public static final String LIST_ALL_MESSAGE = "  (+) Listing out all items in library";
     public static final String LIST_AVAILABLE_MESSAGE = "  (+) Listing out available items in library";
     public static final String LIST_LOANED_MESSAGE = "  (+) Listing out loaned items in library";
+    public static final String LIST_FORMAT_INCORRECT = " (!) Invalid listing command" + System.lineSeparator()
+            + " (!) Format: 'list all' OR 'list available' OR 'list loaned'";
     public static final String LIST_DEADLINE_TODAY = "  (+) Listing out loaned items that have to be returned today";
     public static final String LIST_DEADLINE_OVERDUE = "  (+) Listing out loaned items that are overdue";
+    // Search
+    public static final String SEARCH_MESSAGE = "  (+) Here are the searching results in library";
+    public static final String SEARCH_FORMAT_INCORRECT = " (!) Invalid searching format!" + System.lineSeparator()
+            + " (!) Format: 'search i/ID' OR 'search t/TITLE' OR 'search s/STATUS'";
     // Loan and Return
     public static final String LOAN_SUCCESS = "  (+) Item has been loaned out:";
     public static final String LOAN_SUCCESS_RESERVED = "  (+) The reserved item has been loaned out:";
@@ -37,12 +49,11 @@ public class Messages {
     // Remove
     public static final String RM_SUCCESS = "  (+) Removed the following item:";
     // Add
-    public static final String ADD_MESSAGE = "  (+) Added new item to catalogue:";
     public static final String INVALID_TITLE = "  (!) Please enter a valid title";
-    public static final String ADD_DUPLICATE_ID = "  (!) ID cannot be a duplicate!";
+
     // Invalid Formats
     public static final String ADD_INVALID_FORMAT = "  (!) Invalid/missing values!" + System.lineSeparator()
-            + "  (!) Format: add t/TITLE i/ID";
+            + "  (!) Format: add [a/b/m/v] [ARGUMENTS]";
     public static final String RM_INVALID_FORMAT = "  (!) Invalid/missing values" + System.lineSeparator()
             + "  (!) Format: rm ID";
     public static final String RESERVE_INVALID_FORMAT = "  (!) Invalid/missing values" + System.lineSeparator()
