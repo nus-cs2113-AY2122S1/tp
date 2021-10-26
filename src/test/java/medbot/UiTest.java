b8b92d26508a85522c77979a18ec3ffe034645d7
Tan <e0550425@u.nus.edu>
2021-10-26 21:02:11 +0800

    Add help command for GetCurrentViewCommand

--- src/test/java/medbot/UiTest.java
+++ src/test/java/medbot/UiTest.java
@@ -23,7 +23,7 @@ class UiTest {
             String actualOutput = Ui.getAddMessage(patientId, ViewType.PATIENT_INFO);
             assertEquals(actualOutput, expectedOutput);
         } catch (MedBotException e) {
-            assertEquals(e.getMessage(),"Cannot identify the current view type" + END_LINE);
+            assertEquals(e.getMessage(), "Cannot identify the current view type" + END_LINE);
         }
     }
 
@@ -35,7 +35,7 @@ class UiTest {
             String actualOutput = Ui.getDeleteMessage(patientId, ViewType.PATIENT_INFO);
             assertEquals(actualOutput, expectedOutput);
         } catch (MedBotException e) {
-            assertEquals(e.getMessage(),"Cannot identify the current view type" + END_LINE);
+            assertEquals(e.getMessage(), "Cannot identify the current view type" + END_LINE);
         }
     }
 
@@ -50,18 +50,18 @@ class UiTest {
         String patientInfo = patient.toString();
         String expectedOutput =
                 "The information of patient with ID 1 has been edited to:" + END_LINE + END_LINE
-                + "Patient ID: 1 " + END_LINE
-                + "IC: " + END_LINE
-                + "Name: Bob" + END_LINE
-                + "H/P: " + END_LINE
-                + "Email: John@gmail.com" + END_LINE
-                + "Address: " + END_LINE;
+                        + "Patient ID: 1 " + END_LINE
+                        + "IC: " + END_LINE
+                        + "Name: Bob" + END_LINE
+                        + "H/P: " + END_LINE
+                        + "Email: John@gmail.com" + END_LINE
+                        + "Address: " + END_LINE;
         int patientId = 1;
         try {
-            String actualOutput = Ui.getEditMessage(patientId,patientInfo, ViewType.PATIENT_INFO);
+            String actualOutput = Ui.getEditMessage(patientId, patientInfo, ViewType.PATIENT_INFO);
             assertEquals(actualOutput, expectedOutput);
         } catch (MedBotException e) {
-            assertEquals(e.getMessage(),"Cannot identify the current view type" + END_LINE);
+            assertEquals(e.getMessage(), "Cannot identify the current view type" + END_LINE);
         }
     }
 
@@ -95,20 +95,20 @@ class UiTest {
         patient2.setPhoneNumber("88889999");
         patientList.addPerson(patient2);
         String expectedOutput =
-            "Here is a list of all patients:" + END_LINE
-            + "For full details of each patient, please use the command \"view PATIENT_ID\"" + END_LINE
-            + " ----------------------------------------------------------------------------------------------------- "
-            + END_LINE
-            + " |  ID  | IC Number |         Name         | Phone No. |        Email         |       Address        | "
-            + END_LINE
-            + " ----------------------------------------------------------------------------------------------------- "
-            + END_LINE
-            + " | 1    |           | John Doe             |           | John@gmail.com       |                      | "
-            + END_LINE
-            + " | 2    |           | Bob                  | 88889999  |                      |                      | "
-            + END_LINE
-            + " ----------------------------------------------------------------------------------------------------- "
-            + END_LINE;
+                "Here is a list of all patients:" + END_LINE
+                        + "For full details of each patient, please use the command \"view PATIENT_ID\"" + END_LINE
+                        + " ----------------------------------------------------------------------------------------------------- "
+                        + END_LINE
+                        + " |  ID  | IC Number |         Name         | Phone No. |        Email         |       Address        | "
+                        + END_LINE
+                        + " ----------------------------------------------------------------------------------------------------- "
+                        + END_LINE
+                        + " | 1    |           | John Doe             |           | John@gmail.com       |                      | "
+                        + END_LINE
+                        + " | 2    |           | Bob                  | 88889999  |                      |                      | "
+                        + END_LINE
+                        + " ----------------------------------------------------------------------------------------------------- "
+                        + END_LINE;
 
         assertEquals(PatientUi.getAllPatientsString(patientList.listPersons(false)), expectedOutput);
     }
@@ -119,7 +119,7 @@ class UiTest {
         String expectedOutput = "Here are the list of commands:" + END_LINE + END_LINE
                 + "help" + END_LINE + "add" + END_LINE + "list" + END_LINE + "view" + END_LINE + "edit" + END_LINE
                 + "find" + END_LINE + "delete" + END_LINE + "exit" + END_LINE + "archive" + END_LINE + "unarchive"
-                + END_LINE + END_LINE
+                + END_LINE + "get view" + END_LINE + END_LINE
                 + "To obtain more information on each command and their respective required inputs, type:" + END_LINE
                 + "help [COMMAND]" + END_LINE + END_LINE
                 + "*Note that all commands will remove any '|' inputs for format parsing purposes" + END_LINE;
@@ -137,7 +137,7 @@ class UiTest {
         String expectedOutput = "Here are the list of commands:" + END_LINE + END_LINE
                 + "help" + END_LINE + "add" + END_LINE + "list" + END_LINE + "view" + END_LINE + "edit" + END_LINE
                 + "find" + END_LINE + "delete" + END_LINE + "exit" + END_LINE + "archive" + END_LINE + "unarchive"
-                + END_LINE + END_LINE
+                + END_LINE + "get view" + END_LINE + END_LINE
                 + "To obtain more information on each command and their respective required inputs, type:" + END_LINE
                 + "help [COMMAND]" + END_LINE + END_LINE
                 + "*Note that all commands will remove any '|' inputs for format parsing purposes" + END_LINE;
@@ -154,7 +154,7 @@ class UiTest {
 
         String expectedOutput = "Here are the list of commands:" + END_LINE + END_LINE
                 + "help" + END_LINE + "add" + END_LINE + "list" + END_LINE + "view" + END_LINE + "edit" + END_LINE
-                + "find" + END_LINE + "delete" + END_LINE + "exit" + END_LINE + END_LINE
+                + "find" + END_LINE + "delete" + END_LINE + "get view" + END_LINE + "exit" + END_LINE + END_LINE
                 + "To obtain more information on each command and their respective required inputs, type:" + END_LINE
                 + "help [COMMAND]" + END_LINE + END_LINE
                 + "*Note that all commands will remove any '|' inputs for format parsing purposes" + END_LINE;
