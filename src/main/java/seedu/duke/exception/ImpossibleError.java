c39b8dd38dcb86e0c2a8040c477a000345b65ef2
Hu Jialun <hujialun@comp.nus.edu.sg>
2021-10-27 00:59:48 +0800

    Add an impossible error class
    This is primarily used for programmatically (not logically) impossible situations that the compiler is unable to diagnose. For example, the default case after we exhaust all possible cases in an enum.

---
+++ src/main/java/seedu/duke/exception/ImpossibleError.java
@@ -0,0 +1,8 @@
+package seedu.duke.exception;
+
+/**
+ * For what should not happen
+ * e.g. To make the compiler happy with a switch of enumeration w/o default handling
+ */
+public class ImpossibleError extends Error {
+}
