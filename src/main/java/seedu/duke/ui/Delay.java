9a30e0b18edfd6afba43000ba20cca48652fb3ca
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2021-10-26 18:30:31 +0800

    format

--- src/main/java/seedu/duke/ui/Delay.java
+++ src/main/java/seedu/duke/ui/Delay.java
@@ -1,18 +1,18 @@
 package seedu.duke.ui;
 
 public class Delay {
-     /**
-      * Causes a delay in execution of code for specified duration
-      * in ms before returning control back to the caller of this
-      * method.
-      *
-      * @param ms The amount of millisecond to delay execution.
-      */
-     public static void wait(int ms) {
-         try {
-             Thread.sleep(ms);
-         } catch (InterruptedException ex) {
-             Thread.currentThread().interrupt();
-         }
-     }
+    /**
+     * Causes a delay in execution of code for specified duration
+     * in ms before returning control back to the caller of this
+     * method.
+     *
+     * @param ms The amount of millisecond to delay execution.
+     */
+    public static void wait(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
