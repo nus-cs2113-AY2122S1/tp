7478d366777720370ad5c14f4fe5b83657edff5e
ChuaXiongWei <e0543484@u.nus.edu>
2021-10-26 19:30:32 +0800

    Added some javadocs

--- src/main/java/gordon/util/UI.java
+++ src/main/java/gordon/util/UI.java
@@ -15,18 +15,17 @@ public class UI {
         System.out.println("1. Add a recipe: addRecipe \"recipe name\" \"/ingredients\" 1+2 \"/steps\" 1+2");
         System.out.println("2. Delete a recipe: deleteRecipe \"Index of recipe\"");
         System.out.println("3. List all your recipes: listRecipes");
-        System.out.println("4. Find a recipe: find \"Keyword\"");
+        System.out.println("4. Find a recipe: find \"keyword\" \"number/item name\", where keyword is either /calories, /difficulty, /ingredients, /price, /tags or /time");
         System.out.println("5. Check a specific recipe: check \"Name of Recipe\"");
         System.out.println("6. Add calories to recipe: set \"recipe name\" \"/calories\" numberOfCalories ");
         System.out.println("7. Add difficulty levels to recipe: set \"recipe name\" \"/difficulty\" difficultyLevel ");
         System.out.println("8. Add cooking and preparation time to recipe: "
-                + "set \"recipe name\" \"/time\" cookingTime, preparationTime");
+                + "set \"recipe name\" \"/time\" cookingTime \",\" preparationTime");
         System.out.println("9. Add price to recipe: set \"recipe name\" \"/price\" recipePrice ");
-        System.out.println("10. Tag a recipe: tag \"/ recipeName\" \"/ tagName1 + tagName2 + ...\"");
-        System.out.println("11. Untag a recipe: untag \"/ recipeName\" \"/ tagName1 + tagName2 + ...\"");
+        System.out.println("10. Tag a recipe: tag \"/ recipe name\" \"/ tagName1 + tagName2 + ...\"");
+        System.out.println("11. Untag a recipe: untag \"/ recipe name\" \"/ tagName1 + tagName2 + ...\"");
         System.out.println("12. Delete tags from Cookbook: deleteTag \"/ tagName1 + tagName2 + ...\"");
         System.out.println("13. List all tags: listTags");
         System.out.println("14. Help me: help");
-        System.out.println("");
     }
 }
