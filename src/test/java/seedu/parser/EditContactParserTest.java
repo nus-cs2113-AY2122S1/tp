985c28e087cf6f83bac2ffae397d076f82c526a5
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2021-10-27 01:56:29 +0800

    Add JUnit test for edit

--- src/test/java/seedu/parser/EditContactParserTest.java
+++ src/test/java/seedu/parser/EditContactParserTest.java
@@ -32,4 +32,17 @@ class EditContactParserTest {
         String[] actualResult = editContactParser.parseContactDetails(testInput);
         assertArrayEquals(expectedResult, actualResult);
     }
+
+    //@@author mayankp291
+    @Test
+    void parseEditCommand_inputAllFields_expectOutput() throws MissingDetailException,
+            MissingArgEditException, InvalidFlagException, ForbiddenDetailException, InvalidTelegramUsernameException,
+            InvalidNameException, InvalidLinkedinUsernameException, InvalidGithubUsernameException,
+            InvalidTwitterUsernameException, InvalidEmailException {
+        String[] expectedResult = {"akshay", "topcoder", "akshay123", "akshay123", "akshay123", "akshay@gmail.com"};
+        String testInput = "edit 1 -n akshay -g topcoder -l akshay123 -te akshay123 -tw akshay123 -e akshay@gmail.com";
+        String[] actualResult = editContactParser.parseContactDetails(testInput);
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
 }
