06e7de2e76a3fb42420d56c23180bb7054ba6aef
Yap Wei Xuan <e0533997@u.nus.edu>
2021-10-27 02:50:09 +0800

    Improve StatCategoryCommand
    fixed formatting issues
    added additional info. for user

--- src/main/java/seedu/duke/data/AllRecordList.java
+++ src/main/java/seedu/duke/data/AllRecordList.java
@@ -47,13 +47,14 @@ public class AllRecordList {
      * @param amount           amount spent
      * @param isLoadingStorage indicate if this command is called during loading or runtime
      */
-    public void addExpenditure(String description, double amount, LocalDate date, Category category,
+    public Expenditure addExpenditure(String description, double amount, LocalDate date, Category category,
                                boolean isLoadingStorage) {
         int month = date.getMonthValue();
-        allRecordList.get(month).addExpenditure(description, amount, date, category, isLoadingStorage);
+        Expenditure addedExpenditure = allRecordList.get(month).addExpenditure(description, amount, date, category, isLoadingStorage);
         if (!isLoadingStorage) {
             saveToStorage(storageDirectory);
         }
+        return addedExpenditure;
     }
 
     public void addLoan(String name, double amount, LocalDate date, boolean isLoadingStorage) {
@@ -149,5 +150,6 @@ public class AllRecordList {
     public double getCategorySpending(int month, String categoryString, double categorySpending) {
         return allRecordList.get(month).getCategorySpending(categoryString, categorySpending);
     }
+
 
 }
