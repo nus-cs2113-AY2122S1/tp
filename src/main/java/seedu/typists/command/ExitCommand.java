3af64f7a54166a2b53ce089091a51aa8b0449b7c
LimJiaJing <jiajinglim06.study@gmail.com>
2021-10-27 16:20:52 +0800

    Add a println at exit command to inform user the program is updating the records

--- src/main/java/seedu/typists/command/ExitCommand.java
+++ src/main/java/seedu/typists/command/ExitCommand.java
@@ -9,6 +9,7 @@ import static seedu.typists.Main.uiBot;
 public class ExitCommand implements Command {
     @Override
     public void run(ArrayList<String> args) {
+        System.out.println("Updating game records...");
         GameRecordsManager gameRecordsManager = GameRecordsManager.getGameRecordsManager();
         gameRecordsManager.updateGameRecords();
         uiBot.showBye();
