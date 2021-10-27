bb98561ea438617041ad0d0dc62ad86e42b1934b
APZH <amospzh@gmail.com>
2021-10-27 22:26:57 +0800

    add override for toString() method to print out modified exception message

--- src/main/java/seedu/duke/exception/InvalidPriorityException.java
+++ src/main/java/seedu/duke/exception/InvalidPriorityException.java
@@ -3,6 +3,14 @@ package seedu.duke.exception;
 public class InvalidPriorityException extends Exception {
     private static final String MESSAGE = "Invalid parameter for priority inserted, '%s'.";
 
+    private static final String INVALID_PRIORITY_MESSAGE = "[!] The priority you entered is not valid...\n"
+            + "The following are examples of valid priorities: {Low, Medium, High}";
+
+    @Override
+    public String toString() {
+        return INVALID_PRIORITY_MESSAGE;
+    }
+
     public InvalidPriorityException(String priority) {
         super(String.format(MESSAGE, priority));
     }
