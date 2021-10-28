058fecbce66fb32d9893928948794ab0e80d50f4
tngjunwei <tngjunwei@gmail.com>
2021-10-28 13:03:49 +0800

    Fix code test errors

--- src/test/java/command/expense/DeleteExpenseCommandTest.java
+++ src/test/java/command/expense/DeleteExpenseCommandTest.java
@@ -5,12 +5,23 @@ import entity.Expense;
 import entity.ExpenseList;
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
+import storage.DataManager;
+
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeleteExpenseCommandTest {
 
     @Test
     public void testDeleteExpenseByName() {
+        try {
+            DataManager dataMgr = DataManager.getDataMgr();
+            dataMgr.loadAllManagers();
+        } catch (IOException e) {
+
+        }
+
         Expense expense = new Expense("Eat Lunch", 10.99, "21-Nov-2021");
         ExpenseList.addExpense(expense);
         CommandLine cmd = CommandLineFactory.getCmd();
@@ -20,6 +31,12 @@ public class DeleteExpenseCommandTest {
 
     @Test
     public void testDeleteExpenseById() {
+        try {
+            DataManager dataMgr = DataManager.getDataMgr();
+            dataMgr.loadAllManagers();
+        } catch (IOException e) {
+
+        }
         Expense expense = new Expense("Eat Lunch", 10.99, "21-Nov-2021");
         ExpenseList.addExpense(expense);
         CommandLine cmd = CommandLineFactory.getCmd();
--- src/test/java/command/income/AddIncomeCommandTest.java
+++ src/test/java/command/income/AddIncomeCommandTest.java
@@ -3,12 +3,22 @@ package command.income;
 import command.CommandLineFactory;
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
+import storage.DataManager;
+
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddIncomeCommandTest {
 
     @Test
     public void testAddIncome() {
+        try {
+            DataManager dataMgr = DataManager.getDataMgr();
+            dataMgr.loadAllManagers();
+        } catch (IOException e) {
+
+        }
         CommandLine cmd = CommandLineFactory.getCmd();
         int exitCode = cmd.execute("income", "add", "Salary", "-v 1000");
 
