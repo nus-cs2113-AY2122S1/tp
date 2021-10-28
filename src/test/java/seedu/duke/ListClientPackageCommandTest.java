0e817015699ac94beed33d745681f381fc2eb3fb
YipWayne <79963329+YipWayne@users.noreply.github.com>
2021-10-28 14:48:09 +0800

    Update ListClientPackageCommandTest.java

--- src/test/java/seedu/duke/ListClientPackageCommandTest.java
+++ src/test/java/seedu/duke/ListClientPackageCommandTest.java
@@ -3,7 +3,14 @@ package seedu.duke;
 import org.junit.jupiter.api.Test;
 import seedu.duke.commands.Command;
 import seedu.duke.commands.clientpackages.ListClientPackageCommand;
-import seedu.duke.data.*;
+import seedu.duke.data.Client;
+import seedu.duke.data.Tour;
+import seedu.duke.data.Flight;
+import seedu.duke.data.ClientPackage;
+import seedu.duke.data.ClientList;
+import seedu.duke.data.TourList;
+import seedu.duke.data.FlightList;
+import seedu.duke.data.ClientPackageList;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
