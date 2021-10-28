004146754ee0da28424519b1a96d220eb2c1dde5
Leyond <leyondlee@users.noreply.github.com>
2021-10-28 19:22:20 +0800

    Add prompt to ask user to delete data

--- src/main/java/taa/command/ArchiveCommand.java
+++ src/main/java/taa/command/ArchiveCommand.java
@@ -24,6 +24,11 @@ public class ArchiveCommand extends Command {
 
     private static final String MESSAGE_FORMAT_ARCHIVE_USAGE = "%s [%s/<FILENAME>]";
     private static final String MESSAGE_FORMAT_ARCHIVE_SAVED = "Archive saved:\n  %s";
+    private static final String MESSAGE_FORMAT_CONFIRMATION_PROMPT = "Do you want to remove all data?\n"
+        + "WARNING: This action cannot be reversed.\n"
+        + "Type [%s] to continue: ";
+
+    private static final String CONFIRM_VALUE = "yes";
 
     public ArchiveCommand(String argument) {
         super(argument, ARCHIVE_ARGUMENT_KEYS);
@@ -58,7 +63,27 @@ public class ArchiveCommand extends Command {
         Storage archiveStorage = new Storage(filename);
         archiveStorage.save(moduleList);
 
-        ui.printMessage(String.format(MESSAGE_FORMAT_ARCHIVE_SAVED, Util.getAbsolutePath(filename)));
+        ui.printBorder();
+
+        String savedMessage = String.format(MESSAGE_FORMAT_ARCHIVE_SAVED, Util.getAbsolutePath(filename))
+            + "\n"
+            + "\n"
+            + String.format(MESSAGE_FORMAT_CONFIRMATION_PROMPT, CONFIRM_VALUE);
+        ui.printMessage(savedMessage, false);
+
+        String userInput = ui.getUserInput(PROMPT_WITHIN_COMMAND, true);
+        String message;
+        if (userInput.equals(CONFIRM_VALUE)) {
+            moduleList.deleteAllModules();
+            storage.save(moduleList);
+
+            message = MESSAGE_DATA_REMOVED;
+        } else {
+            message = MESSAGE_ABORT;
+        }
+
+        ui.printMessage(String.format("\n%s", message), false);
+        ui.printBorder();
     }
 
     private String getDefaultArchiveFilename() {
