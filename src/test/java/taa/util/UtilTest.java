6f7e93bd515b65b6691a62a382fdb6d6314b828a
Leyond <leyondlee@users.noreply.github.com>
2021-10-31 00:50:33 +0800

    Add UtilTest

---
+++ src/test/java/taa/util/UtilTest.java
@@ -0,0 +1,23 @@
+package taa.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilTest {
+
+    @Test
+    void isStringInteger_emptyString_expectFalse() {
+        assertFalse(Util.isStringInteger(""));
+    }
+
+    @Test
+    void isStringDouble_emptyString_expectFalse() {
+        assertFalse(Util.isStringDouble(""));
+    }
+
+    @Test
+    void isStringBoolean_emptyString_expectFalse() {
+        assertFalse(Util.isStringBoolean(""));
+    }
+}
