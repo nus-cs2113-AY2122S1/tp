c0f65d8f81eec6bc0aa8b9ca3d2cd726e21c96e0
dyahnafisah <59493341+dyahnafisah@users.noreply.github.com>
2021-10-30 23:32:08 +0800

    Create command deadline by specific date and add exceptions

--- src/main/java/seedu/duke/commands/DeadlineCommand.java
+++ src/main/java/seedu/duke/commands/DeadlineCommand.java
@@ -5,8 +5,15 @@ import seedu.duke.data.Item;
 import seedu.duke.ui.TextUI;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 import static seedu.duke.common.Messages.DIVIDER;
+import static seedu.duke.common.Messages.EMPTY_DATE;
+import static seedu.duke.common.Messages.EMPTY_DEADLINE_COMMAND;
+import static seedu.duke.common.Messages.INVALID_DATE;
+import static seedu.duke.common.Messages.INVALID_DEADLINE_COMMAND;
+import static seedu.duke.common.Messages.LIST_DEADLINE_DATE;
 import static seedu.duke.common.Messages.LIST_DEADLINE_OVERDUE;
 import static seedu.duke.common.Messages.LIST_DEADLINE_TODAY;
 
@@ -17,6 +24,8 @@ public class DeadlineCommand extends Command {
     public static final String COMMAND_WORD = "deadline";
     public static final String DEADLINE_TODAY_COMMAND = "deadline today";
     public static final String DEADLINE_OVERDUE_COMMAND = "deadline overdue";
+    public static final String dateFormat = "dd-MM-yyyy";
+    protected DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern(dateFormat);
     public String input = "";
 
     /**
@@ -27,13 +36,11 @@ public class DeadlineCommand extends Command {
     }
 
     /**
-     * Prints out items in the list according to their return deadline.
-     * Overrides method from parent class.
+     * Processes <b>deadline</b> Command, including handle exceptions.
      * @param ui Object that handles user IO
      * @param catalogue Object that stores the list of all items
      */
-    @Override
-    public void execute(TextUI ui, Catalogue catalogue) {
+    public void handleDeadlineCommand(TextUI ui, Catalogue catalogue) {
         LocalDate today = LocalDate.now();
         if (input.equals(DEADLINE_TODAY_COMMAND)) {
             ui.print(LIST_DEADLINE_TODAY);
@@ -51,7 +58,39 @@ public class DeadlineCommand extends Command {
                     ui.print(temp);
                 }
             }
+        } else if (input.contains("d/")) {
+            LocalDate dueDate = LocalDate.parse(input.split("d/")[1].substring(0,10), dtFormatter);
+            ui.print(LIST_DEADLINE_DATE + dueDate.format(dtFormatter));
+            ui.print(DIVIDER);
+            for (Item temp : catalogue.getAllItems()) {
+                if (temp.getDueDate() != null && temp.getDueDate().equals(dueDate)) {
+                    ui.print(temp);
+                }
+            }
+        } else if (input.equals(COMMAND_WORD)) {
+            ui.print(EMPTY_DEADLINE_COMMAND);
+        } else {
+            ui.print(INVALID_DEADLINE_COMMAND);
         }
         ui.print(DIVIDER);
+    }
+
+    /**
+     * Prints out items in the list according to their return deadline.
+     * Overrides method from parent class.
+     * @param ui Object that handles user IO
+     * @param catalogue Object that stores the list of all items
+     */
+    @Override
+    public void execute(TextUI ui, Catalogue catalogue) {
+        try {
+            handleDeadlineCommand(ui, catalogue);
+        } catch (DateTimeParseException e) {
+            ui.print(INVALID_DATE);
+        } catch (StringIndexOutOfBoundsException e) {
+            ui.print(INVALID_DATE);
+        } catch (IndexOutOfBoundsException e) {
+            ui.print(EMPTY_DATE);
+        }
     }
 }
