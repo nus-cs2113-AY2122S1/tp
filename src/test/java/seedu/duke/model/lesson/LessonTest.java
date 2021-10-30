8e1da4e6469942a48d72cb384941b6f6da37c59d
richwill28 <e0550368@u.nus.edu>
2021-10-30 22:51:07 +0800

    Add JUnit for launchUrl

--- src/test/java/seedu/duke/model/lesson/LessonTest.java
+++ src/test/java/seedu/duke/model/lesson/LessonTest.java
@@ -1,4 +1,20 @@
 package seedu.duke.model.lesson;
 
+import org.junit.jupiter.api.Test;
+import seedu.duke.DukeException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+//@@author richwill28
 public class LessonTest {
+    @Test
+    public void launchUrl_emptyUrl_exceptionThrown() {
+        assertThrows(DukeException.class, () -> {
+            LessonList lessonList = new LessonList();
+            lessonList.addLesson(new Lesson("CS3243 Lecture", "Monday", "10:00 PM", "12:00 PM", " "));
+            lessonList.addLesson(new Lesson("CS1101S Lecture", "Wednesday", "10:00 PM", "12:00 PM", "-"));
+            lessonList.getLesson(1).launchUrl();
+            lessonList.getLesson(2).launchUrl();
+        });
+    }
 }
