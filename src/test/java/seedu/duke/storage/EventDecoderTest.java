8456663e8caeee667bbd742e0900f8b741213c04
Alvin.Avocados <alvinpang321@gmail.com>
2021-10-31 13:49:12 +0800

    Update EventDecoder to handle new convertDateTime thrown exception

--- src/test/java/seedu/duke/storage/EventDecoderTest.java
+++ src/test/java/seedu/duke/storage/EventDecoderTest.java
@@ -1,6 +1,7 @@
 package seedu.duke.storage;
 
 import org.junit.jupiter.api.Test;
+import seedu.duke.exceptions.DukeException;
 import seedu.duke.items.Event;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class EventDecoderTest {
 
     @Test
-    void decodeEventFromString_stringValidFormat_expectOneEvent() {
+    void decodeEventFromString_stringValidFormat_expectOneEvent() throws DukeException {
         // Setting up
         String encodedEvent = "e | Peppa Pig's Concert | X | Asia world tour | 19-02-2022 2000 "
                 + "| Indoor Stadium | 1000.9";
