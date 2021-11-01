3abfcdbd01cdb1240ab85536b4b9e81187f5e910
APZH <amospzh@gmail.com>
2021-11-02 01:22:50 +0800

    add JavaDoc summary for ListFlag class

--- src/main/java/seedu/duke/command/flags/ListFlag.java
+++ src/main/java/seedu/duke/command/flags/ListFlag.java
@@ -1,6 +1,9 @@
 package seedu.duke.command.flags;
 
 //@@author APZH
+/**
+ * Flags and arguments used by the 'list' command.
+ */
 public class ListFlag {
     public static final String TASK_TYPE = "type";
     public static final String PRIORITY = "priority";
