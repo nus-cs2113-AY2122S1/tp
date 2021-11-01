2dcc009615c449356688bd016e724f63b3cbacaf
APZH <amospzh@gmail.com>
2021-11-02 01:23:13 +0800

    add JavaDoc for SortFlag class

--- src/main/java/seedu/duke/command/flags/SortFlag.java
+++ src/main/java/seedu/duke/command/flags/SortFlag.java
@@ -1,6 +1,9 @@
 package seedu.duke.command.flags;
 
 //@@author APZH
+/**
+ * Flags and arguments used by the 'sort' command.
+ */
 public class SortFlag {
     public static final String SORT_BY = "by";
     public static final String SORT_BY_TYPE_ARGUMENT = "type";
