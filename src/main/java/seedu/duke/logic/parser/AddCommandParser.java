5c4e3d608864078a49ee7ebb27a7f4c141088c5e
Roycius <roycius@gmail.com>
2021-11-02 18:30:37 +0800

    Extract flags to be constants in parser

--- src/main/java/seedu/duke/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/duke/logic/parser/AddCommandParser.java
@@ -1,8 +1,5 @@
 package seedu.duke.logic.parser;
 
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.HashMap;
 
 import seedu.duke.commons.core.CommandType;
@@ -29,12 +26,19 @@ import static seedu.duke.logic.parser.ParserUtil.removeFirstParam;
 public class AddCommandParser {
     public static final String LESSON_FLAGS = "-d |-s |-e |-l ";
     public static final String TASK_FLAGS = "-d |-p |-i ";
+    public static final String DAY_FLAG = "-d";
+    public static final String START_FLAG = "-s";
+    public static final String END_FLAG = "-e";
+    public static final String LINK_FLAG = "-l";
+    public static final String PRIORITY_FLAG = "-p";
+    public static final String INFORMATION_FLAG = "-i";
     public static final String DEFAULT_PRIORITY = LOW.toString();
     public static final String EMPTY_INFORMATION = "-";
 
+
     public static Command parse(String userResponse) throws ParseException {
         if (userResponse.contains("|")) {
-            userResponse = userResponse.replace("|","/");
+            userResponse = userResponse.replace("|", "/");
         }
 
         CommandType commandType = parseCommandType(userResponse);
@@ -59,39 +63,41 @@ public class AddCommandParser {
 
     //@@author Roycius
     private static Command parseAddLessonCommand(String userResponse) throws ParseException {
-        HashMap<String, String> flagMap = ParserUtil.getFlagMap(userResponse, "-d", "-s", "-e", "-l");
+        HashMap<String, String> flagMap =
+                ParserUtil.getFlagMap(userResponse, DAY_FLAG, START_FLAG, END_FLAG, LINK_FLAG);
         String[] params = userResponse.split(LESSON_FLAGS);
-        if (!flagMap.containsKey("-d") || !flagMap.containsKey("-s") || !flagMap.containsKey("-e")) {
+        if (!flagMap.containsKey(DAY_FLAG) || !flagMap.containsKey(START_FLAG) || !flagMap.containsKey(END_FLAG)) {
             throw new ParseException(Messages.ERROR_MISSING_FLAGS);
         }
         String title = parseTitle(params[0]);
-        String dayOfTheWeek = parseDayOfTheWeek(flagMap.get("-d"));
-        String startTime = parseTime(flagMap.get("-s"));
-        String endTime = parseTime(flagMap.get("-e"));
+        String dayOfTheWeek = parseDayOfTheWeek(flagMap.get(DAY_FLAG));
+        String startTime = parseTime(flagMap.get(START_FLAG));
+        String endTime = parseTime(flagMap.get(END_FLAG));
         String meetingUrl = EMPTY_INFORMATION;
 
-        if (flagMap.containsKey("-l")) {
-            meetingUrl = flagMap.get("-l");
+        if (flagMap.containsKey(LINK_FLAG)) {
+            meetingUrl = flagMap.get(LINK_FLAG);
         }
         return new AddLessonCommand(title, dayOfTheWeek, startTime, endTime, meetingUrl);
     }
 
     private static Command parseAddTaskCommand(String userResponse) throws ParseException {
-        HashMap<String, String> flagMap = ParserUtil.getFlagMap(userResponse, "-d", "-p", "-i");
+        HashMap<String, String> flagMap =
+                ParserUtil.getFlagMap(userResponse, DAY_FLAG, PRIORITY_FLAG, INFORMATION_FLAG);
         String[] params = userResponse.split(TASK_FLAGS);
-        if (!flagMap.containsKey("-d")) {
+        if (!flagMap.containsKey(DAY_FLAG)) {
             throw new ParseException(Messages.ERROR_MISSING_FLAGS);
         }
         String title = parseTitle(params[0]);
-        String dayOfTheWeek = parseDayOfTheWeek(flagMap.get("-d"));
+        String dayOfTheWeek = parseDayOfTheWeek(flagMap.get(DAY_FLAG));
         String priority = DEFAULT_PRIORITY;
         String information = EMPTY_INFORMATION;
 
-        if (flagMap.containsKey("-p")) {
-            priority = parsePriority(flagMap.get("-p"));
+        if (flagMap.containsKey(PRIORITY_FLAG)) {
+            priority = parsePriority(flagMap.get(PRIORITY_FLAG));
         }
-        if (flagMap.containsKey("-i")) {
-            information = flagMap.get("-i");
+        if (flagMap.containsKey(INFORMATION_FLAG)) {
+            information = flagMap.get(INFORMATION_FLAG);
         }
         return new AddTaskCommand(title, dayOfTheWeek, priority, information);
     }
