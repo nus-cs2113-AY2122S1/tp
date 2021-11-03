9a66000225a7a479c8441e03647f469e73c9914c
alvintan01 <35805635+alvintan01@users.noreply.github.com>
2021-11-03 22:47:13 +0800

    Refactor test data generator

--- src/test/java/command/Data.java
+++ src/test/java/command/Data.java
@@ -11,6 +11,8 @@ import java.text.ParseException;
 import java.util.ArrayList;
 
 public class Data {
+    private static Ui ui = Ui.getInstance();
+
     /**
      * Function to clear all the data and reset the IDs.
      */
@@ -30,7 +32,17 @@ public class Data {
     public static void generateTestData() {
         clearTestData();
         ArrayList<Medicine> medicines = Medicine.getInstance();
+        generateTestStocks(medicines);
+        generateTestPrescriptions(medicines);
+        generateTestOrders(medicines);
+    }
 
+    /**
+     * Function to generate stock data to test list commands.
+     *
+     * @param medicines Arraylist of medicines.
+     */
+    public static void generateTestStocks(ArrayList<Medicine> medicines) {
         try {
             medicines.add(new Stock("PANADOL", 20, 20, DateParser.stringToDate("13-9-2022"),
                     "HEADACHES", 1000));
@@ -45,15 +57,19 @@ public class Data {
                     "HYPOTHYROIDISM", 800));
             medicines.add(new Stock("AZITHROMYCIN", 20, 35, DateParser.stringToDate("15-10-2023"),
                     "INFECTIONS", 100));
-            medicines.add(new Order("PANADOL", 100, DateParser.stringToDate("9-10-2021")));
-            medicines.add(new Order("VICODIN", 30, DateParser.stringToDate("10-10-2021")));
-            Order order = new Order("VICODIN", 50, DateParser.stringToDate("11-10-2021"));
-            order.setDelivered();
-            medicines.add(order);
-            medicines.add(new Order("SIMVASTATIN", 20, DateParser.stringToDate("11-10-2021")));
-            medicines.add(new Order("LISINOPRIL", 200, DateParser.stringToDate("12-12-2021")));
-            medicines.add(new Order("AZITHROMYCIN", 100, DateParser.stringToDate("13-12-2021")));
-            medicines.add(new Order("PANADOL", 50, DateParser.stringToDate("13-12-2021")));
+        } catch (ParseException e) {
+            ui.print("Unable to parse date!");
+        }
+
+    }
+
+    /**
+     * Function to generate prescription data to test list commands.
+     *
+     * @param medicines Arraylist of medicines.
+     */
+    public static void generateTestPrescriptions(ArrayList<Medicine> medicines) {
+        try {
             medicines.add(new Prescription("PANADOL", 10, "S1234567A",
                     DateParser.stringToDate("9-10-2021"), "Jane", 1));
             medicines.add(new Prescription("VICODIN", 15, "S2345678B",
@@ -65,7 +81,27 @@ public class Data {
             medicines.add(new Prescription("AZITHROMYCIN", 5, "S2345678B",
                     DateParser.stringToDate("13-10-2021"), "Peter", 6));
         } catch (ParseException e) {
-            Ui ui = Ui.getInstance();
+            ui.print("Unable to parse date!");
+        }
+    }
+
+    /**
+     * Function to generate order data to test list commands.
+     *
+     * @param medicines Arraylist of medicines.
+     */
+    public static void generateTestOrders(ArrayList<Medicine> medicines) {
+        try {
+            medicines.add(new Order("PANADOL", 100, DateParser.stringToDate("9-10-2021")));
+            medicines.add(new Order("VICODIN", 30, DateParser.stringToDate("10-10-2021")));
+            Order order = new Order("VICODIN", 50, DateParser.stringToDate("11-10-2021"));
+            order.setDelivered();
+            medicines.add(order);
+            medicines.add(new Order("SIMVASTATIN", 20, DateParser.stringToDate("11-10-2021")));
+            medicines.add(new Order("LISINOPRIL", 200, DateParser.stringToDate("12-12-2021")));
+            medicines.add(new Order("AZITHROMYCIN", 100, DateParser.stringToDate("13-12-2021")));
+            medicines.add(new Order("PANADOL", 50, DateParser.stringToDate("13-12-2021")));
+        } catch (ParseException e) {
             ui.print("Unable to parse date!");
         }
     }
