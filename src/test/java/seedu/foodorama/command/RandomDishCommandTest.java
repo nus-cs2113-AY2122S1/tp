3b287e8a678cfbae8e6089eff8b09de0549b6a48
jhsee5 <70202843+jhsee5@users.noreply.github.com>
2021-11-03 21:58:28 +0800

    Add comments to RandomDishCommandTest

--- src/test/java/seedu/foodorama/command/RandomDishCommandTest.java
+++ src/test/java/seedu/foodorama/command/RandomDishCommandTest.java
@@ -13,20 +13,25 @@ public class RandomDishCommandTest {
     @Test
     void execute() throws FoodoramaException {
 
+        //initialise RandomDishCommand
         RandomDishCommand testCommand = new RandomDishCommand();
 
+        //implement testing Carbohydrates ArrayList, execute test
         ArrayList<String> testCarbohydrates;
         testCarbohydrates = testCommand.getCarbohydrates();
         assertEquals(testCommand.getCarbohydrates().size(), testCarbohydrates.size());
 
+        //implement testing Proteins ArrayList, execute test
         ArrayList<String> testProteins;
         testProteins = testCommand.getProteins();
         assertEquals(testCommand.getProteins().size(), testProteins.size());
 
+        //implement testingSauces ArrayList, execute test
         ArrayList<String> testSauces;
         testSauces = testCommand.getSauces();
         assertEquals(testCommand.getSauces().size(), testSauces.size());
 
+        //implement testing CookingMethods ArrayList, execute test
         ArrayList<String> testCookingMethods;
         testCookingMethods = testCommand.getCookingMethods();
         assertEquals(testCommand.getCookingMethods().size(), testCookingMethods.size());
