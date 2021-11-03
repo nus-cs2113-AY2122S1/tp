70f65dd9019f388b54facc5183817bf9a9562d06
silinche <chensilin011025@gmail.com>
2021-11-04 00:14:23 +0800

    updated UG for search and JUnit test for search

--- src/test/java/seedu/duke/commands/SearchCommandTest.java
+++ src/test/java/seedu/duke/commands/SearchCommandTest.java
@@ -16,7 +16,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+//@@author silinche
 class SearchCommandTest {
     SearchCommand searchCommand;
     ArrayList<Item> items;
@@ -33,7 +36,8 @@ class SearchCommandTest {
         audio2 = new Audio("Thriller", "4192", Status.RESERVED, null, null, "Michael Jackson", "4:00");
         book1 = new Book("The three body problem", "7234", Status.AVAILABLE, null, null,  "Liu Cixin");
         book2 = new Book("Dune", "3457345", Status.LOANED, null, null, "Frank Herbert");
-        magazine1 = new Magazine("Economist", "01230", Status.AVAILABLE, null, null, "Some random publisher", "30Oct2021");
+        magazine1 = new Magazine("Economist", "01230", Status.AVAILABLE, null, null,
+                "Some random publisher", "30Oct2021");
         items = new ArrayList<Item>();
         items.add(audio1);
         items.add(audio2);
@@ -41,15 +45,37 @@ class SearchCommandTest {
         items.add(book2);
         catalogue = new Catalogue(items);
 
+    }
+
+    @Test
+    void testCheckValidArgs() {
+        HashMap<String, String> args = new HashMap<>();
+        args.put(null, "search");
+        searchCommand = new SearchCommand(args);
+        assertFalse(searchCommand.checkValidArgs());
+        args.put("c", "Magazine");
+        assertTrue(searchCommand.checkValidArgs());
+    }
+
+    @Test
+    void testCheckAdditionalArgs() {
+        HashMap<String, String> args = new HashMap<>();
+        args.put(null, "search");
+        args.put("c", "Magazine");
+        args.put("q", "Something");
+        searchCommand = new SearchCommand(args);
+        assertTrue(searchCommand.checkAdditionalArgs());
+        args.remove("q");
+        assertFalse(searchCommand.checkAdditionalArgs());
+    }
+
+    @Test
+    void testCheckMatches() {
         HashMap<String, String> args = new HashMap<>();
         args.put(null, "search");
         args.put("c", "Magazine");
         searchCommand = new SearchCommand(args);
-    }
 
-
-    @Test
-    void testCheckMatches() {
         assertEquals(0, searchCommand.checkMatches(audio1));
         assertEquals(0, searchCommand.checkMatches(audio2));
         assertEquals(0, searchCommand.checkMatches(book1));
