a506a10b2ef502e96bfc7b36ab92b376f2529b50
Jiale-Sun <69443909+Jiale-Sun@users.noreply.github.com>
2021-11-04 14:54:26 +0800

    add java doc to add uni

--- src/main/java/seplanner/commands/AddUniCommand.java
+++ src/main/java/seplanner/commands/AddUniCommand.java
@@ -6,10 +6,21 @@ import seplanner.universities.UniversityList;
 
 import java.io.IOException;
 
+/**
+ * Command to add a university to the selected university list.
+*/
 public class AddUniCommand extends Command {
     private final University universityToAdd;
     private final int universityIndexToAdd;
-
+    
+    /**
+     * Add a university to the selected university list.
+     *
+     * @param universityToAdd university to add to the selected university list.
+     * @param universityMasterList The master university list which contains all universities.
+     * @param universitySelectedList The selected university list which contains only the universities selected by the user.
+     * @throws IOException If input-output operation failed.
+    */
     public AddUniCommand(University universityToAdd, UniversityList universityMasterList,
                          UniversityList universitySelectedList) throws IOException {
         this.universityToAdd = universityToAdd;
