b430f7cc29fa6d7c9afe6709e83e2181532c63d6
Jiale-Sun <69443909+Jiale-Sun@users.noreply.github.com>
2021-11-04 16:21:25 +0800

    fix length

--- src/main/java/seplanner/commands/RemoveMapCommand.java
+++ src/main/java/seplanner/commands/RemoveMapCommand.java
@@ -22,7 +22,7 @@ public class RemoveMapCommand extends Command {
      * @param selectedMappingIndex index of the mapping to remove under the selected university.
      * @param universityMasterList The master university list which contains all universities.
      * @param moduleMasterList The master module list which contains all modules.
-     * @param universitySelectedList The selected university list which contains only the university selected by the user.
+     * @param universitySelectedList The selected university list which contains only the university selected.
      * @param moduleSelectedList The selected module list which contains only the module selected by the user.
      * @throws IOException If input-output operation failed.
     */
