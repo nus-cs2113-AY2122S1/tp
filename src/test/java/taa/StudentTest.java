cc2c581227c49cf615ec85bc9617d63754b04f7e
Jon-the-MELON <limyongkiat@yahoo.com.sg>
2021-11-06 03:40:07 +0800

    add junit testing for marks

--- src/test/java/taa/StudentTest.java
+++ src/test/java/taa/StudentTest.java
@@ -1,7 +1,5 @@
 package taa;
 
-import taa.assessment.Assessment;
-import taa.assessment.AssessmentList;
 import taa.student.Student;
 import org.junit.jupiter.api.Test;
 
@@ -11,33 +9,5 @@ public class StudentTest {
     @Test
     public void testStringConversion() {
         assertEquals("A1234567B - Jon Lim", new Student("A1234567B", "Jon Lim").toString());
-    }
-
-    @Test
-    void setMarks_newMarksArguments_marksAddedCorrectly() {
-        Student student = new Student("A0123456X", "Jim");
-        AssessmentList assessments = new AssessmentList();
-        assessments.addAssessment(new Assessment("Midterms", 100, 20));
-        student.setMarks("Midterms", 99.9);
-        assertEquals(99.9, student.getMarks("Midterms"));
-    }
-
-    @Test
-    void averageMarks_averageMarksArguments_marksAveragedCorrectly() {
-        Student student1 = new Student("A0123456X", "Jim Ho");
-        Student student2 = new Student("A0123456Y", "Kim Ho");
-        Student student3 = new Student("A0123456Z", "Lim Ho");
-        Student student4 = new Student("A0123456A", "Mim Ho");
-        Student student5 = new Student("A0123456B", "Nim Ho");
-        AssessmentList assessments = new AssessmentList();
-        assessments.addAssessment(new Assessment("Midterms", 100, 20));
-        student1.setMarks("Midterms", 50);
-        student2.setMarks("Midterms", 60);
-        student3.setMarks("Midterms", 70);
-        student4.setMarks("Midterms", 80);
-        student5.setMarks("Midterms", 90);
-        assertEquals(70, );
-
-
     }
 }
---
+++ src/test/java/taa/mark/SetMarkTest.java
@@ -0,0 +1,232 @@
+package taa.mark;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import taa.Parser;
+import taa.Ui;
+import taa.command.Command;
+import taa.exception.TaaException;
+import taa.storage.Storage;
+import taa.teachingclass.ClassList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SetMarkTest {
+    @Test
+    void setMark_userMarkTwoStudents_marksSetCorrectly() throws TaaException {
+        Ui ui = new Ui();
+        Storage storage = new Storage("./data/taa_data.json");
+        ClassList classList = new ClassList();
+        String classInput = "add_class i/cs2102";
+        Command command1 = Parser.parseUserInput(classInput);
+        command1.parseArgument();
+        command1.checkArgument();
+        command1.execute(classList, ui, storage);
+
+        String assessmentInput = "add_assessment c/cs2102 n/Midterms m/50 w/20";
+        Command command2 = Parser.parseUserInput(assessmentInput);
+        command2.parseArgument();
+        command2.checkArgument();
+        command2.execute(classList, ui, storage);
+
+        String studentInput = "add_student c/cs2102 i/12345678 n/Jim Ho";
+        Command command3 = Parser.parseUserInput(studentInput);
+        command3.parseArgument();
+        command3.checkArgument();
+        command3.execute(classList, ui, storage);
+
+        String studentInput2 = "add_student c/cs2102 i/12345677 n/Tim Ho";
+        Command command4 = Parser.parseUserInput(studentInput2);
+        command4.parseArgument();
+        command4.checkArgument();
+        command4.execute(classList, ui, storage);
+
+        String markInput = "set_mark c/cs2102 s/1 a/Midterms m/48";
+        Command command5 = Parser.parseUserInput(markInput);
+        command5.parseArgument();
+        command5.checkArgument();
+        command5.execute(classList, ui, storage);
+
+        String markInput2 = "set_mark c/cs2102 s/2 a/Midterms m/12";
+        Command command6 = Parser.parseUserInput(markInput2);
+        command6.parseArgument();
+        command6.checkArgument();
+        command6.execute(classList, ui, storage);
+
+        assertEquals(48.00, classList.getClassWithId("cs2102").getStudentList().getStudentAt(0)
+                .getMarks("Midterms"));
+        assertEquals(12.00, classList.getClassWithId("cs2102").getStudentList().getStudentAt(1)
+                .getMarks("Midterms"));
+    }
+
+    @Test
+    void setMark_marksExceedMaximumMarks_taaExceptionThrown() throws TaaException {
+        Ui ui = new Ui();
+        Storage storage = new Storage("./data/taa_data.json");
+        ClassList classList = new ClassList();
+        String classInput = "add_class i/cs2102";
+        Command command1 = Parser.parseUserInput(classInput);
+        command1.parseArgument();
+        command1.checkArgument();
+        command1.execute(classList, ui, storage);
+
+        String assessmentInput = "add_assessment c/cs2102 n/Midterms m/50 w/20";
+        Command command2 = Parser.parseUserInput(assessmentInput);
+        command2.parseArgument();
+        command2.checkArgument();
+        command2.execute(classList, ui, storage);
+
+        String studentInput = "add_student c/cs2102 i/12345678 n/Jim Ho";
+        Command command3 = Parser.parseUserInput(studentInput);
+        command3.parseArgument();
+        command3.checkArgument();
+        command3.execute(classList, ui, storage);
+
+        TaaException thrown = Assertions.assertThrows(TaaException.class, () -> {
+            String markInput = "set_mark c/cs2102 s/1 a/Midterms m/51";
+            Command command4 = Parser.parseUserInput(markInput);
+            command4.parseArgument();
+            command4.checkArgument();
+            command4.execute(classList, ui, storage);
+        }, "TaaException error was expected");
+        Assertions.assertEquals("Invalid Marks. Marks must be between 0 and 50.00 (inclusive)",
+                thrown.getMessage());
+    }
+    @Test
+    void setMark_studentDoesNotExist_taaExceptionThrown() throws TaaException {
+        Ui ui = new Ui();
+        Storage storage = new Storage("./data/taa_data.json");
+        ClassList classList = new ClassList();
+        String classInput = "add_class i/cs2102";
+        Command command1 = Parser.parseUserInput(classInput);
+        command1.parseArgument();
+        command1.checkArgument();
+        command1.execute(classList, ui, storage);
+
+        String assessmentInput = "add_assessment c/cs2102 n/Midterms m/50 w/20";
+        Command command2 = Parser.parseUserInput(assessmentInput);
+        command2.parseArgument();
+        command2.checkArgument();
+        command2.execute(classList, ui, storage);
+
+        String studentInput = "add_student c/cs2102 i/12345678 n/Jim Ho";
+        Command command3 = Parser.parseUserInput(studentInput);
+        command3.parseArgument();
+        command3.checkArgument();
+        command3.execute(classList, ui, storage);
+
+        TaaException thrown = Assertions.assertThrows(TaaException.class, () -> {
+            String markInput = "set_mark c/cs2102 s/2 a/Midterms m/48";
+            Command command4 = Parser.parseUserInput(markInput);
+            command4.parseArgument();
+            command4.checkArgument();
+            command4.execute(classList, ui, storage);
+        }, "TaaException error was expected");
+        Assertions.assertEquals("Invalid student index.", thrown.getMessage());
+    }
+
+    @Test
+    void setMark_classDoesNotExist_taaExceptionThrown() throws TaaException {
+        Ui ui = new Ui();
+        Storage storage = new Storage("./data/taa_data.json");
+        ClassList classList = new ClassList();
+        String classInput = "add_class i/cs2102";
+        Command command1 = Parser.parseUserInput(classInput);
+        command1.parseArgument();
+        command1.checkArgument();
+        command1.execute(classList, ui, storage);
+
+        String assessmentInput = "add_assessment c/cs2102 n/Midterms m/50 w/20";
+        Command command2 = Parser.parseUserInput(assessmentInput);
+        command2.parseArgument();
+        command2.checkArgument();
+        command2.execute(classList, ui, storage);
+
+        String studentInput = "add_student c/cs2102 i/12345678 n/Jim Ho";
+        Command command3 = Parser.parseUserInput(studentInput);
+        command3.parseArgument();
+        command3.checkArgument();
+        command3.execute(classList, ui, storage);
+
+        TaaException thrown = Assertions.assertThrows(TaaException.class, () -> {
+            String markInput = "set_mark c/cs2112 s/1 a/Midterms m/48";
+            Command command4 = Parser.parseUserInput(markInput);
+            command4.parseArgument();
+            command4.checkArgument();
+            command4.execute(classList, ui, storage);
+        }, "TaaException error was expected");
+        Assertions.assertEquals("Class not found.", thrown.getMessage());
+    }
+
+    @Test
+    void setMark_assessmentDoesNotExist_taaExceptionThrown() throws TaaException {
+        Ui ui = new Ui();
+        Storage storage = new Storage("./data/taa_data.json");
+        ClassList classList = new ClassList();
+        String classInput = "add_class i/cs2102";
+        Command command1 = Parser.parseUserInput(classInput);
+        command1.parseArgument();
+        command1.checkArgument();
+        command1.execute(classList, ui, storage);
+
+        String assessmentInput = "add_assessment c/cs2102 n/Midterms m/50 w/20";
+        Command command2 = Parser.parseUserInput(assessmentInput);
+        command2.parseArgument();
+        command2.checkArgument();
+        command2.execute(classList, ui, storage);
+
+        String studentInput = "add_student c/cs2102 i/12345678 n/Jim Ho";
+        Command command3 = Parser.parseUserInput(studentInput);
+        command3.parseArgument();
+        command3.checkArgument();
+        command3.execute(classList, ui, storage);
+
+        TaaException thrown = Assertions.assertThrows(TaaException.class, () -> {
+            String markInput = "set_mark c/cs2102 s/1 a/Middle terms m/48";
+            Command command4 = Parser.parseUserInput(markInput);
+            command4.parseArgument();
+            command4.checkArgument();
+            command4.execute(classList, ui, storage);
+        }, "TaaException error was expected");
+        Assertions.assertEquals("Invalid assessment name.", thrown.getMessage());
+    }
+
+    @Test
+    void setMark_doubleMarking_taaExceptionThrown() throws TaaException {
+        Ui ui = new Ui();
+        Storage storage = new Storage("./data/taa_data.json");
+        ClassList classList = new ClassList();
+        String classInput = "add_class i/cs2102";
+        Command command1 = Parser.parseUserInput(classInput);
+        command1.parseArgument();
+        command1.checkArgument();
+        command1.execute(classList, ui, storage);
+
+        String assessmentInput = "add_assessment c/cs2102 n/Midterms m/50 w/20";
+        Command command2 = Parser.parseUserInput(assessmentInput);
+        command2.parseArgument();
+        command2.checkArgument();
+        command2.execute(classList, ui, storage);
+
+        String studentInput = "add_student c/cs2102 i/12345678 n/Jim Ho";
+        Command command3 = Parser.parseUserInput(studentInput);
+        command3.parseArgument();
+        command3.checkArgument();
+        command3.execute(classList, ui, storage);
+
+        String markInput = "set_mark c/cs2102 s/1 a/Midterms m/48";
+        Command command4 = Parser.parseUserInput(markInput);
+        command4.parseArgument();
+        command4.checkArgument();
+        command4.execute(classList, ui, storage);
+
+        TaaException thrown = Assertions.assertThrows(TaaException.class, () -> {
+            String markInput2 = "set_mark c/cs2102 s/1 a/Midterms m/0";
+            Command command5 = Parser.parseUserInput(markInput2);
+            command5.parseArgument();
+            command5.checkArgument();
+            command5.execute(classList, ui, storage);
+        }, "TaaException error was expected");
+        Assertions.assertEquals("This student has already been marked.", thrown.getMessage());
+    }
+}
