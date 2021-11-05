0a2925bd1920628b72928c92f81f59ee8a45f783
Leyond <leyondlee@users.noreply.github.com>
2021-11-05 16:20:07 +0800

    Remove result check for Util.createFile method

--- src/main/java/taa/storage/Storage.java
+++ src/main/java/taa/storage/Storage.java
@@ -18,7 +18,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Storage {
-    private static final String MESSAGE_FORMAT_FAIL_CREATE_FILE = "Fail to create file/folder - %s";
     private static final String MESSAGE_FORMAT_UNABLE_TO_OPEN_READ = "Unable to open file to read - %s";
     private static final String MESSAGE_FORMAT_UNABLE_TO_OPEN_WRITE = "Unable to open file to write - %s";
     private static final String MESSAGE_FORMAT_UNABLE_TO_READ_JSON = "Unable to read from JSON file - %s";
@@ -70,9 +69,7 @@ public class Storage {
      * @throws TaaException if fail to create, open, or write to the file.
      */
     public void save(ClassList classList) throws TaaException {
-        if (!Util.createFile(filename)) {
-            throw new TaaException(String.format(MESSAGE_FORMAT_FAIL_CREATE_FILE, Util.getAbsolutePath(filename)));
-        }
+        Util.createFile(filename);
 
         assert Util.fileExists(filename);
 
