39d96e0ac0cf7248787d1ecdac62400aafafc14c
Leyond <leyondlee@users.noreply.github.com>
2021-11-05 16:15:28 +0800

    Use StringBuilder to create string for duplicated keys

--- src/main/java/taa/exception/DuplicatedArgumentException.java
+++ src/main/java/taa/exception/DuplicatedArgumentException.java
@@ -10,16 +10,16 @@ public class DuplicatedArgumentException extends TaaException {
     }
 
     private static String generateMessage(ArrayList<String> duplicatedKeys) {
-        String duplicatedKeysStr = "";
+        StringBuilder duplicatedKeysStr = new StringBuilder();
         for (int i = 0; i < duplicatedKeys.size(); i += 1) {
             String key = duplicatedKeys.get(i);
 
             if (i > 0) {
-                duplicatedKeysStr += ", ";
+                duplicatedKeysStr.append(", ");
             }
-            duplicatedKeysStr += key;
+            duplicatedKeysStr.append(key);
         }
 
-        return String.format(MESSAGE_FORMAT_DUPLICATED_KEYS_FOUND, duplicatedKeysStr);
+        return String.format(MESSAGE_FORMAT_DUPLICATED_KEYS_FOUND, duplicatedKeysStr.toString());
     }
 }
