fff04f7725a9e126d9d2097e2d8e1296df04f448
Cuiminjing <e0492441@u.nus.edu>
2021-11-06 19:21:09 +0800

    ...

--- src/main/java/seedu/traveller/exceptions/InvalidDeleteItemFormatCommand.java
+++
@@ -1,9 +0,0 @@
-package seedu.traveller.exceptions;
-
-
-public class InvalidDeleteItemFormatCommand extends TravellerException {
-    public InvalidDeleteItemFormatCommand() {
-        message = "\tWrong format for DeleteItem!"
-                + "\n\tCorrect format: delete-item TRIP_NAME /day DAY_INDEX /item ITEM_INDEX";
-    }
-}
