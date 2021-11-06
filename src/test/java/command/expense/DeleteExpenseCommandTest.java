477bc08025f736e04bca9bef9d1818a314785ec4
tngjunwei <tngjunwei@gmail.com>
2021-11-06 23:03:32 +0800

    Revert "Delete tests for Commands - causing bugs"
    This reverts commit 4a283f39a2f7b5ec0389b259757f5ffc4b6c1fa7.

---
+++ src/test/java/command/expense/DeleteExpenseCommandTest.java
@@ -0,0 +1,46 @@
+package command.expense;
+
+import command.CommandLineFactory;
+import entity.expense.Expense;
+import entity.expense.ExpenseList;
+import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
+import storage.DataManager;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeleteExpenseCommandTest {
+
+    @Test
+    public void testDeleteExpenseByName() {
+        try {
+            DataManager dataMgr = DataManager.getDataMgr();
+            dataMgr.loadAllManagers();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Expense expense = new Expense("Eat Lunch", 10.99, "21-Nov-2021");
+        ExpenseList.addExpense(expense);
+        CommandLine cmd = CommandLineFactory.getCmd();
+        int exitCode = cmd.execute("expense", "delete", "-n Eat Lunch");
+        assertEquals(0, exitCode);
+    }
+
+    @Test
+    public void testDeleteExpenseById() {
+        try {
+            DataManager dataMgr = DataManager.getDataMgr();
+            dataMgr.loadAllManagers();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        Expense expense = new Expense("Eat Lunch", 10.99, "21-Nov-2021");
+        ExpenseList.addExpense(expense);
+        CommandLine cmd = CommandLineFactory.getCmd();
+        int exitCode = cmd.execute("expense", "delete", "-i=1");
+        assertEquals(0, exitCode);
+    }
+}
