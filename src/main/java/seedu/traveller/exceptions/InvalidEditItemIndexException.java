088fc1e209c23d643ff749e9fb13708aad8e77c4
JaCh23 <jach2355@gmail.com>
2021-11-07 01:55:48 +0800

    gradle syntax

--- src/main/java/seedu/traveller/exceptions/InvalidEditItemIndexException.java
+++ src/main/java/seedu/traveller/exceptions/InvalidEditItemIndexException.java
@@ -5,7 +5,7 @@ package seedu.traveller.exceptions;
 public class InvalidEditItemIndexException extends TravellerException {
     public InvalidEditItemIndexException(String rawIndex) {
         message = "\tRemember edit item index " + rawIndex
-                + "must be followed by a time " +
-                "\n\tor date field and needs to be a valid integer 0 or above!";
+                + "must be followed by a time "
+                + "\n\tor date field and needs to be a valid integer 0 or above!";
     }
 }
