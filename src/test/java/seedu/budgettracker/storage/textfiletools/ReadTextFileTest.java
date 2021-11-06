13f65fab3216c6d0b7177f03c6baef190040befd
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2021-11-06 23:39:25 +0800

    test

--- src/test/java/seedu/budgettracker/storage/textfiletools/ReadTextFileTest.java
+++ src/test/java/seedu/budgettracker/storage/textfiletools/ReadTextFileTest.java
@@ -1,11 +1,13 @@
 package seedu.budgettracker.storage.textfiletools;
 
 import org.junit.jupiter.api.Test;
+import seedu.budgettracker.data.records.exceptions.DuplicateBudgetException;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ReadTextFileTest {
     ReadTextFile textFileReader = new ReadTextFile("./data/wrong.txt");
@@ -13,6 +15,9 @@ public class ReadTextFileTest {
     @Test
     void readTextFileToString_invalidFile_expectNoRawCommand() throws IOException {
         ArrayList<String> arrayListOfRawCommands = textFileReader.readTextFileToString();
-        assertEquals(0, arrayListOfRawCommands.size());
+        // assertEquals(0, arrayListOfRawCommands.size());
+
+        assertThrows(NullPointerException.class,
+                ()-> arrayListOfRawCommands.size());
     }
 }
