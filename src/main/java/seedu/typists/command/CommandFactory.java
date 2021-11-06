744c05dde962043787cd5b0328bb919c8419a9c0
Isabella-L <isabellalu01@gmail.com>
2021-11-06 17:32:30 +0800

    checkstyle update

--- src/main/java/seedu/typists/command/CommandFactory.java
+++ src/main/java/seedu/typists/command/CommandFactory.java
@@ -1,6 +1,13 @@
 package seedu.typists.command;
 
-import seedu.typists.command.commands.*;
+import seedu.typists.command.commands.Command;
+import seedu.typists.command.commands.ContentCommand;
+import seedu.typists.command.commands.HistoryCommand;
+import seedu.typists.command.commands.ClearCommand;
+import seedu.typists.command.commands.ExitCommand;
+import seedu.typists.command.commands.GameCommand;
+import seedu.typists.command.commands.TimeGameCommand;
+import seedu.typists.command.commands.WordGameCommand;
 import seedu.typists.ui.TextUi;
 
 import java.util.ArrayList;
