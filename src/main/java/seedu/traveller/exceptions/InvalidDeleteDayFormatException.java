162651eee0f0543fe3a934f5a49bc42798c09066
Cuiminjing <e0492441@u.nus.edu>
2021-11-06 19:22:00 +0800

    deleteday

--- src/main/java/seedu/traveller/exceptions/InvalidDeleteDayFormatException.java
+++ src/main/java/seedu/traveller/exceptions/InvalidDeleteDayFormatException.java
@@ -1,1 +1,9 @@
+package seedu.traveller.exceptions;
 
+
+public class InvalidDeleteDayFormatException extends TravellerException {
+    public InvalidDeleteDayFormatException() {
+        message = "\tWrong format for DeleteDay!"
+                + "\n\tCorrect format: delete-day TRIP_NAME /day DAY_INDEX";
+    }
+}
