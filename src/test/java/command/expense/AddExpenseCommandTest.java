95022f87407c521c14bbdb6726941564f34193f2
Mohamed Irfan <irfancuber@gmail.com>
2021-11-06 22:54:21 +0800

    Fix add expense command test

--- src/test/java/command/expense/AddExpenseCommandTest.java
+++ src/test/java/command/expense/AddExpenseCommandTest.java
@@ -9,9 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AddExpenseCommandTest {
 
     @Test
-    public void testAddExpense() {
+    public void addExpenseCommand_addExpense_success() {
         CommandLine cmd = CommandLineFactory.getCmd();
         int exitCode = cmd.execute("expense", "add", "Eat Lunch", "-v=20.99");
-        assertEquals(1, exitCode);
+        assertEquals(0, exitCode);
     }
 }
