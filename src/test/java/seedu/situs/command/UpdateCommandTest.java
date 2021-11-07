57e0ee286572ebf18e95634fc61f43d61380ba78
AayushMathur7 <aayushmathur7@gmail.com>
2021-11-07 20:25:22 +0800

    updated ppp

--- src/test/java/seedu/situs/command/UpdateCommandTest.java
+++ src/test/java/seedu/situs/command/UpdateCommandTest.java
@@ -4,29 +4,29 @@ import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class UpdateCommandTest {
+//class UpdateCommandTest {
 
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-    /*@Test
-    public void updateCommandTest_dukeExceptionThrown() throws DukeException, IOException {
-        LocalDate expiryDate1 = LocalDate.parse("21/10/2021", DATE_FORMATTER);
-        LocalDate expiryDate2 = LocalDate.parse("01/11/2021", DATE_FORMATTER);
-        LocalDate expiryDate3 = LocalDate.parse("02/10/2021", DATE_FORMATTER);
-        LocalDate expiryDate4 = LocalDate.parse("04/11/2021", DATE_FORMATTER);
-
-        Ingredient ingredient1 = new Ingredient("Carrot", 300, "grams", expiryDate1);
-        Ingredient ingredient2 = new Ingredient("Tomato", 200, "ounces", expiryDate2);
-        Ingredient ingredient3 = new Ingredient("Carrot", 1.5, "kilograms", expiryDate3);
-
-        IngredientGroup.getInstance().add(ingredient1);
-        IngredientGroup.getInstance().add(ingredient2);
-        IngredientGroup.getInstance().add(ingredient3);
-
-        Ingredient updatedIngredient = new Ingredient("Carrot", 450.0, "grams", expiryDate4);
-        String resultMsg = new UpdateCommand(updatedIngredient).run();
-        String expected = "Got it. This ingredient has been updated:\n" + "\t" + updatedIngredient.toString();
-
-        assertEquals(expected, resultMsg);
-    }*/
-}
+//    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//
+//    @Test
+//    public void updateCommandTest_dukeExceptionThrown() throws DukeException, IOException {
+//        LocalDate expiryDate1 = LocalDate.parse("21/11/2021", DATE_FORMATTER);
+//        LocalDate expiryDate2 = LocalDate.parse("01/12/2021", DATE_FORMATTER);
+//        LocalDate expiryDate3 = LocalDate.parse("02/01/2022", DATE_FORMATTER);
+//        LocalDate expiryDate4 = LocalDate.parse("02/03/2022", DATE_FORMATTER);
+//
+//        Ingredient ingredient1 = new Ingredient("Carrot", 300, expiryDate1);
+//        Ingredient ingredient2 = new Ingredient("Tomato", 200, expiryDate2);
+//        Ingredient ingredient3 = new Ingredient("Carrot", 1.5, expiryDate3);
+//
+//        IngredientGroup.getInstance().add(ingredient1);
+//        IngredientGroup.getInstance().add(ingredient2);
+//        IngredientGroup.getInstance().add(ingredient3);
+//
+//        Ingredient updatedIngredient = new Ingredient("Carrot", 450.0, expiryDate4);
+//        String resultMsg = new UpdateCommand(updatedIngredient).run();
+//        String expected = "Got it. This ingredient has been updated:\n" + "\t" + updatedIngredient.toString();
+//
+//        assertEquals(expected, resultMsg);
+//    }
+//}
