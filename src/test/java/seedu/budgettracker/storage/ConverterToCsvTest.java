74cecd3dee98a60dee4222f27ff68a2e22661478
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2021-11-07 13:29:10 +0800

    storage test

---
+++ src/test/java/seedu/budgettracker/storage/ConverterToCsvTest.java
@@ -0,0 +1,43 @@
+package seedu.budgettracker.storage;
+
+import org.junit.jupiter.api.Test;
+import seedu.budgettracker.data.AllRecordList;
+import seedu.budgettracker.data.RecordList;
+import seedu.budgettracker.data.records.exceptions.DuplicateBudgetException;
+
+import java.io.File;
+import java.time.DateTimeException;
+import java.util.Hashtable;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ConverterToCsvTest {
+    @Test
+    void convertToCsvFile_convertExistingFile_expectNewCsvFile() {
+        Hashtable<Integer, RecordList> allRecordList = new Hashtable<>();
+        Storage loader = new Storage();
+        AllRecordList recordListNew = new AllRecordList();
+
+        for (int i = 1; i <= 12; i++) {
+            allRecordList.put(i, new RecordList(i));
+        }
+
+        loader.loadStorage(recordListNew, "./data/2018.txt");
+
+        ConverterToCsv csvCreator = new ConverterToCsv();
+        boolean isExist = false;
+
+        csvCreator.convertToCsvFile(recordListNew, "./data/2018.csv");
+
+        File dataDirectory = new File("./data/");
+        File[] dataDirectoryList = dataDirectory.listFiles();
+
+        for (int i = 0; i < dataDirectoryList.length; i++) {
+            if (dataDirectoryList[i].isFile() && dataDirectoryList[i].getName().equals("2018.csv")) {
+                isExist = true;
+            }
+        }
+
+        assertTrue(isExist);
+    }
+}
