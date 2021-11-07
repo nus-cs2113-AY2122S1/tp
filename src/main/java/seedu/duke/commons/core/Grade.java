2373ab5a245ed7821e132efc755153532874cb1e
richwill28 <e0550368@u.nus.edu>
2021-11-08 00:20:12 +0800

    Add Grade class

--- src/main/java/seedu/duke/commons/core/Grade.java
+++ src/main/java/seedu/duke/commons/core/Grade.java
@@ -1,5 +1,38 @@
 package seedu.duke.commons.core;
 
-public enum Grade {
+//@@author richwill28
+public class Grade {
+    public static final String[] GRADES = {
+            "A+",   // Point = 5.0
+            "A",    // Point = 5.0
+            "A-",   // Point = 4.5
+            "B+",   // Point = 4.0
+            "B",    // Point = 3.5
+            "B-",   // Point = 3.0
+            "C+",   // Point = 2.5
+            "C",    // Point = 2.0
+            "D+",   // Point = 1.5
+            "D",    // Point = 1.0
+            "F",    // Point = 0.0
+            "S",    // Satisfactory
+            "U",    // Unsatisfactory
+            "CS",   // Completed (Satisfactory)
+            "CU",   // Completed (Unsatisfactory)
+            "IC",   // In Complete
+            "IP",   // In Progress
+            "AUD",  // Audit
+            "EXE",  // Exempted
+            "W",    // Withdrawn
+            "WU",   // Withdrawal from University
+            "NONE"  // Custom default grade
+    };
 
+    public static boolean isValidGrade(String param) {
+        for (String grade : GRADES) {
+            if (param.equals(grade)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
