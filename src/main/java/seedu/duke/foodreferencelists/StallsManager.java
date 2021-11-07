384d197128b598a87c3f4357b2420b0375ac0662
ngnigel99 <ngnigel99@gmail.com>
2021-11-07 21:54:12 +0800

    Update user guide with updated commands

--- src/main/java/seedu/duke/foodreferencelists/StallsManager.java
+++ src/main/java/seedu/duke/foodreferencelists/StallsManager.java
@@ -155,7 +155,8 @@ public class StallsManager {
         }
         sortListByCalorieCount(filterLowerThanCalories);
         sortListByName(filterLowerThanCalories);
-        Ui.printMessage("I've found the following items with " + calories + " calories!");
+        Ui.printMessage("I've found the following items with less than or equal to, "
+            + calories + " calories!");
         filterLowerThanCalories.printList(false);
         Ui.printMessage("done printing!");
         Ui.printLine();
