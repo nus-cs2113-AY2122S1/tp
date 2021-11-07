00cc14e6aaa044e85a46747a74f6a3c6f9cb10b6
Leyond <leyondlee@users.noreply.github.com>
2021-11-07 13:58:20 +0800

    SLAP assessment deserializers

--- src/main/java/taa/storage/deserializer/AssessmentDeserializer.java
+++ src/main/java/taa/storage/deserializer/AssessmentDeserializer.java
@@ -1,6 +1,7 @@
 package taa.storage.deserializer;
 
 //@@author leyondlee
+
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
