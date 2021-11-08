2acccf491bdc2a12494f9a2cea76727e466efa75
leeyikai <e0544550@u.nus.edu>
2021-11-08 21:18:46 +0800

    Removed magic number and string for executeView

--- src/main/java/seedu/duke/parser/CommandExecutor.java
+++ src/main/java/seedu/duke/parser/CommandExecutor.java
@@ -24,6 +24,12 @@ abstract class CommandExecutor extends PaymentOptimizer implements ExpenseSummar
     private static final String EDIT_FORCUR = "-forcur";
     private static final String EDIT_HOMECUR = "-homecur";
     private static final int NEW_TRIP_ATTRIBUTES_COUNT = 6;
+    private static final int NUMBER_OF_PARAMETERS = 3;
+    private static final int INDEX_OF_SECOND_COMMAND = 0;
+    private static final int INDEX_OF_CATEGORY = 1;
+    private static final int INDEX_OF_EXPENSE_ATTRIBUTE = 2;
+    private static final String LAST = "last";
+    private static final String FILTER = "filter";
 
     //@@author yeezao
     /**
@@ -195,22 +201,22 @@ abstract class CommandExecutor extends PaymentOptimizer implements ExpenseSummar
         Trip openTrip = Storage.getOpenTrip();
         if (inputParams == null) {
             openTrip.viewAllExpenses();
-        } else if (inputParams.equalsIgnoreCase("last")) {
+        } else if (inputParams.equalsIgnoreCase(LAST)) {
             if (openTrip.getLastExpense() == null) {
                 Ui.noRecentExpenseError();
             } else {
                 System.out.println(openTrip.getLastExpense());
             }
         } else {
-            String[] paramString = inputParams.split(" ", 3);
-            String secondCommand = paramString[0];
+            String[] paramString = inputParams.split(" ", NUMBER_OF_PARAMETERS);
+            String secondCommand = paramString[INDEX_OF_SECOND_COMMAND];
             String expenseCategory = null;
             String expenseAttribute = null;
             if (!isNumeric(secondCommand)) {
-                expenseCategory = paramString[1];
-                expenseAttribute = paramString[2];
+                expenseCategory = paramString[INDEX_OF_CATEGORY];
+                expenseAttribute = paramString[INDEX_OF_EXPENSE_ATTRIBUTE];
             }
-            if (secondCommand.equalsIgnoreCase("filter")) {
+            if (secondCommand.equalsIgnoreCase(FILTER)) {
                 try {
                     openTrip.getFilteredExpenses(expenseCategory, expenseAttribute);
                 } catch (IndexOutOfBoundsException e) {
