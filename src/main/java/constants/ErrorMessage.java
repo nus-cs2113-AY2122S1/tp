6326849925c7ac62ab866e26af0e60d2f4b74b07
tngjunwei <tngjunwei@gmail.com>
2021-11-08 15:26:37 +0800

    Fix style errors

--- src/main/java/constants/ErrorMessage.java
+++ src/main/java/constants/ErrorMessage.java
@@ -49,7 +49,9 @@ public final class ErrorMessage {
 
     public static final String invalidIndexMsg = "Invalid index";
 
-    public static final String negativeValueMsg = "Your value cannot be a negative amount. Check that you have entered the correct value and try again!";
+    public static final String negativeValueMsg = "Your value cannot be a negative amount. Check that you have "
+            + "entered the correct value and try again!";
 
-    public static final String invalidShareMsg = "Your number of shares cannot be 0 or negative. Check that you have entered the correct value and try again!";
+    public static final String invalidShareMsg = "Your number of shares cannot be 0 or negative. Check that "
+            + "you have entered the correct value and try again!";
 }
