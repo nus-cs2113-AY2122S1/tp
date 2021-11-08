364c7d95815a08fe359fda1da00217e031808825
leeyikai <e0544550@u.nus.edu>
2021-11-08 11:56:40 +0800

    Edited checkstyle

--- src/test/java/seedu/duke/parser/PaymentOptimizerTest.java
+++ src/test/java/seedu/duke/parser/PaymentOptimizerTest.java
@@ -11,10 +11,12 @@ import seedu.duke.exceptions.ForceCancelException;
 import seedu.duke.exceptions.SameNameException;
 import seedu.duke.trip.Trip;
 import seedu.duke.expense.Expense;
+import java.time.LocalDate;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.lang.reflect.Array;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -25,6 +27,7 @@ class PaymentOptimizerTest {
     static Expense exp1;
     static Expense exp2;
     static Expense exp3;
+    private static final DateTimeFormatter inputPattern = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
     @BeforeAll
     static void setUp() throws SameNameException, ForceCancelException {
@@ -37,27 +40,28 @@ class PaymentOptimizerTest {
         ArrayList<Person> listOfPersons = trip.getListOfPersons();
         Person person1 = listOfPersons.get(0);
         Person person2 = listOfPersons.get(1);
-        Person person3 = listOfPersons.get(2);
-        Expense exp1 = new Expense(8.00, "food", listOfPersons, "chicken nuggers");
+        HashMap<String, Double> amountSplit1 = new HashMap<>();
+        Expense exp1 = new Expense(8.00, "chicken nuggers", listOfPersons, "food",
+                LocalDate.parse("11-02-2021", inputPattern), person1, amountSplit1);
         exp1.setPayer(person1);
         person1.setMoneyOwed(person1, -8.0);
         person1.setMoneyOwed(person2, 5.0);
         person2.setMoneyOwed(person1, -5.0);
+        Person person3 = listOfPersons.get(2);
         person1.setMoneyOwed(person3, 3.0);
         person3.setMoneyOwed(person1, -3.0);
         exp1.setAmountSplit(person2, 4.0);
         exp1.setAmountSplit(person3, 4.0);
-        Expense exp2 = new Expense(16.00, "food", listOfPersons, "chicken");
-        exp1.setPayer(person2);
+        HashMap<String, Double> amountSplit2 = new HashMap<>();
+        Expense exp2 = new Expense(16.00, "chicken", listOfPersons, "food",
+                LocalDate.parse("11-02-2021", inputPattern), person2, amountSplit2);
+        exp2.setAmountSplit(person2, 10.0);
+        exp2.setAmountSplit(person3, 6.0);
         person2.setMoneyOwed(person2, -16.0);
         person2.setMoneyOwed(person1, 10.0);
         person1.setMoneyOwed(person2, -10.0);
         person2.setMoneyOwed(person3, 6.0);
         person3.setMoneyOwed(person2, -6.0);
-        exp2.setAmountSplit(person2, 10.0);
-        exp1.setDate("11-02-2021");
-        exp2.setAmountSplit(person3, 6.0);
-        exp2.setDate("11-02-2021");
         trip.addExpense(exp1);
         trip.addExpense(exp2);
     }
@@ -66,16 +70,17 @@ class PaymentOptimizerTest {
     public void testOptimizePayments() throws ForceCancelException {
         Trip openTrip = Storage.getOpenTrip();
         Person person1 = openTrip.getListOfPersons().get(0);
-        Person person2 = openTrip.getListOfPersons().get(1);
-        Person person3 = openTrip.getListOfPersons().get(2);
-        HashMap<String, Double> hashMapBen = person1.getOptimizedMoneyOwed();
-        HashMap<String, Double> hashMapJerry = person2.getOptimizedMoneyOwed();
-        HashMap<String, Double> hashMapTom = person3.getOptimizedMoneyOwed();
         PaymentOptimizer.optimizePayments(openTrip);
+        HashMap<String, Double> hashMapBen = person1.getOptimizedMoneyOwed();
         assertEquals(hashMapBen.get("jerry"), -2.0);
+        Person person2 = openTrip.getListOfPersons().get(1);
+        HashMap<String, Double> hashMapJerry = person2.getOptimizedMoneyOwed();
         assertEquals(hashMapJerry.get("ben"), 2.0);
-        assertEquals(hashMapTom.get("jerry"), -9.0);
         assertEquals(hashMapJerry.get("tom"), 9.0);
+        Person person3 = openTrip.getListOfPersons().get(2);
+        HashMap<String, Double> hashMapTom = person3.getOptimizedMoneyOwed();
+        assertEquals(hashMapTom.get("jerry"), -9.0);
+
 
     }
 
