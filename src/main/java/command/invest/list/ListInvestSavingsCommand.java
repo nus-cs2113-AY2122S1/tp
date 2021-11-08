c08073afebef5a966b247caa21c33b373c62ccf4
tngjunwei <tngjunwei@gmail.com>
2021-11-08 16:25:48 +0800

    Refactor the name of command class slightly

--- src/main/java/command/invest/list/ListInvestSavingsCommand.java
+++ src/main/java/command/invest/list/ListInvestSavingsCommand.java
@@ -7,7 +7,7 @@ import terminal.Ui;
 import java.util.concurrent.Callable;
 
 @Command(name = "savings", description = "Shows a list of all savings.")
-public class ListInvestSavingCommand implements Callable<Integer> {
+public class ListInvestSavingsCommand implements Callable<Integer> {
 
     public Integer call() throws Exception {
         Ui ui = Ui.getUi();
