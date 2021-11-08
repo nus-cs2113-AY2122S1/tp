6714b11b048f726807c3487656d9ec0eb4365261
yipwayyne <E0533998@u.nus.edu>
2021-11-08 16:43:16 +0800

    Commit Javadoc for some Ui functions, change to findclient UML

--- src/main/java/seedu/duke/commands/flights/FindFlightCommand.java
+++ src/main/java/seedu/duke/commands/flights/FindFlightCommand.java
@@ -3,16 +3,28 @@ package seedu.duke.commands.flights;
 import seedu.duke.TourPlannerException;
 import seedu.duke.commands.Command;
 
+/**
+ * Finds a flight based on a certain id.
+ */
 public class FindFlightCommand extends Command {
-    private final String code;
+    private final String id;
 
-    public FindFlightCommand(String code) {
-        this.code = code;
+    /**
+     * Class constructor for FindFlightCommand.
+     *
+     * @param id id used to determine which flight to find.
+     */
+    public FindFlightCommand(String id) {
+        this.id = id;
     }
 
+    /**
+     * Executes the finding of flight, as well as finding said flight's passengers.
+     * If there are no found flights, it will be shown to the user.
+     */
     public void execute() {
         try {
-            ui.showFindFlight(flights, clientPackages, code);
+            ui.showFindFlight(flights, clientPackages, id);
         } catch (TourPlannerException e) {
             System.out.println(e.getMessage());
         }
