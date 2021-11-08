cabc43b9fe5c46b8dac59a3dcaedb44ac55de9d2
Kureans <leonardtengys@gmail.com>
2021-11-08 17:11:00 +0800

    Fix UiTest for FindPatient/Staff

--- src/main/java/medbot/parser/SchedulerCommandParser.java
+++ src/main/java/medbot/parser/SchedulerCommandParser.java
@@ -90,8 +90,9 @@ public abstract class SchedulerCommandParser {
      *
      * @param userInput String containing the full user input.
      * @return EditAppointmentCommand with the specified appointment ID and appointment information.
-     * @throws MedBotParserException if the appointment ID is not specified or not a positive number, or when the appointment
-     *                               information given is invalid
+     * @throws MedBotParserException if the appointment ID is not specified or not a positive number,
+     *                               or when the appointment  information given is invalid.
+     *
      */
     private static EditAppointmentCommand parseEditAppointmentCommand(String userInput) throws MedBotParserException {
         int appointmentId = ParserUtils.parseId(userInput.substring(4));
